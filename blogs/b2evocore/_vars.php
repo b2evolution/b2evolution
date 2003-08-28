e386fd730fe06a6fc748621ff2c965d00b8eab1c
jupiterx <jupiterx@users.sourceforge.net>
2003-08-28 01:59:34 +0000

    Misc. code cleanup; replaced simple preg_match()es with stropos() for speed; new smiliescmp() just because there was room for improvement; Changed perl-style (#) comments to //

--- blogs/b2evocore/_vars.php
+++ blogs/b2evocore/_vars.php
@@ -23,34 +23,45 @@ if( ($use_l10n==1) && function_exists( 'bindtextdomain' ) )
 locale_activate( $default_locale );
 
 
-# on which page are we ?
-$PHP_SELF = $HTTP_SERVER_VARS['PHP_SELF'];
+// on which page are we ?
+$PHP_SELF = $_SERVER['PHP_SELF'];
 $pagenow = explode('/', $PHP_SELF);
 $pagenow = trim($pagenow[(sizeof($pagenow)-1)]);
 $pagenow = explode('?', $pagenow);
 $pagenow = $pagenow[0];
 
-# browser detection
+// browser detection
 $is_lynx = 0; $is_gecko = 0; $is_winIE = 0; $is_macIE = 0; $is_opera = 0; $is_NS4 = 0;
 if (!isset($HTTP_USER_AGENT)) {
-	$HTTP_USER_AGENT = $HTTP_SERVER_VARS['HTTP_USER_AGENT'];
+	$HTTP_USER_AGENT = $_SERVER['HTTP_USER_AGENT'];
 }
-if (preg_match('/Lynx/', $HTTP_USER_AGENT)) {
+if(strpos($HTTP_USER_AGENT, 'Lynx') !== false)
+{
 	$is_lynx = 1;
-} elseif (preg_match('/Gecko/', $HTTP_USER_AGENT)) {
+}
+elseif(strpos($HTTP_USER_AGENT, 'Gecko') !== false)
+{
 	$is_gecko = 1;
-} elseif ((preg_match('/MSIE/', $HTTP_USER_AGENT)) && (preg_match('/Win/', $HTTP_USER_AGENT))) {
+}
+elseif(strpos($HTTP_USER_AGENT, 'MSIE') !== false && strpos($HTTP_USER_AGENT, 'Win') !== false)
+{
 	$is_winIE = 1;
-} elseif ((preg_match('/MSIE/', $HTTP_USER_AGENT)) && (preg_match('/Mac/', $HTTP_USER_AGENT))) {
+}
+elseif(strpos($HTTP_USER_AGENT, 'MSIE') !== false && strpos($HTTP_USER_AGENT, 'Mac') !== false)
+{
 	$is_macIE = 1;
-} elseif (preg_match('/Opera/', $HTTP_USER_AGENT)) {
+}
+elseif(strpos($HTTP_USER_AGENT, 'Opera') !== false)
+{
 	$is_opera = 1;
-} elseif ((preg_match('/Nav/', $HTTP_USER_AGENT) ) || (preg_match('/Mozilla\/4\./', $HTTP_USER_AGENT))) {
+}
+elseif(strpos($HTTP_USER_AGENT, 'Nav') !== false || preg_match('/Mozilla\/4\./', $HTTP_USER_AGENT))
+{
 	$is_NS4 = 1;
 }
-$is_IE    = (($is_macIE) || ($is_winIE));
+$is_IE = (($is_macIE) || ($is_winIE));
 
-# browser-specific javascript corrections
+// browser-specific javascript corrections
 $b2_macIE_correction['in'] = array(
 	'/\%uFFD4/', '/\%uFFD5/', '/\%uFFD2/', '/\%uFFD3/',
 	'/\%uFFA5/', '/\%uFFD0/', '/\%uFFD1/', '/\%uFFBD/',
@@ -60,49 +71,49 @@ $b2_macIE_correction['in'] = array(
 );
 $b2_macIE_correction['out'] = array(
 	'&lsquo;', '&rsquo;', '&ldquo;', '&rdquo;',
-	'&bull;', '&ndash;', '&mdash;', '&Omega;',
-	'&beta;', '&gamma;', '&theta;', '&lambda;',
-	'&pi;', '&prime;', '&Prime;', '&ang;',
+	'&bull;',  '&ndash;', '&mdash;', '&Omega;',
+	'&beta;',  '&gamma;', '&theta;', '&lambda;',
+	'&pi;',    '&prime;', '&Prime;', '&ang;',
 	'&euro;'
 );
 $b2_gecko_correction['in'] = array(
 	'/\‘/', '/\’/', '/\“/', '/\”/',
 	'/\•/', '/\–/', '/\—/', '/\Ω/',
-	'/\β/', '/\γ/', '/\θ/', '/\λ/',
-	'/\π/', '/\′/', '/\″/', '/\�/',
+	'/\β/',  '/\γ/',  '/\θ/',  '/\λ/',
+	'/\π/',  '/\′/', '/\″/', '/\�/',
 	'/\€/', '/\ /'
 );
 $b2_gecko_correction['out'] = array(
 	'&8216;', '&rsquo;', '&ldquo;', '&rdquo;',
 	'&bull;', '&ndash;', '&mdash;', '&Omega;',
 	'&beta;', '&gamma;', '&theta;', '&lambda;',
-	'&pi;', '&prime;', '&Prime;', '&ang;',
+	'&pi;',   '&prime;', '&Prime;', '&ang;',
 	'&euro;', '&#8201;'
 );
 
-# server detection
-$is_Apache = strstr($HTTP_SERVER_VARS['SERVER_SOFTWARE'], 'Apache') ? 1 : 0;
-$is_IIS = strstr($HTTP_SERVER_VARS['SERVER_SOFTWARE'], 'Microsoft-IIS') ? 1 : 0;
+// server detection
+$is_Apache	= strpos($HTTP_SERVER_VARS['SERVER_SOFTWARE'], 'Apache') !== false ? 1 : 0;
+$is_IIS		= strpos($HTTP_SERVER_VARS['SERVER_SOFTWARE'], 'Microsoft-IIS') !== false ? 1 : 0;
 
 
-# sorts the smilies' array by length
-# this is important if you want :)) to superseede :) for example
+/* sorts the smilies' array by length
+  this is important if you want :)) to superseede :) for example
+*/
 if (!function_exists('smiliescmp')) 
 {
-	function smiliescmp ($a, $b) 
+	function smiliescmp($a, $b)
 	{
-	   if (strlen($a) == strlen($b)) 
-		 {
-		  return strcmp($a, $b);
-	   }
-	   return (strlen($a) > strlen($b)) ? -1 : 1;
+		if(($diff = strlen($b) - strlen($a)) == 0)
+		{
+			return strcmp($a, $b);
+		}
+		return $diff;
 	}
 }
+	
 uksort($b2smiliestrans, 'smiliescmp');
-// arsort($b2smiliestrans);
 
-
-# generates smilies' search & replace arrays
+// generates smilies' search & replace arrays
 foreach($b2smiliestrans as $smiley => $img) 
 {
 	$b2_smiliessearch[] = $smiley;
@@ -110,9 +121,10 @@ foreach($b2smiliestrans as $smiley => $img)
 	$smiley_masked = '';
 	for ($i = 0; $i < strlen($smiley); $i++ ) 
 	{
-		// fplanque: with the #160s we prevent recurrent replacing... bleh :(
-		// fplanque changed $smiley_masked .= substr($smiley, $i, 1).chr(160);
-		// better way: (added benefit: handles ' and " escaping for alt attribute!... needed in :') )
+		/* fplanque: with the #160s we prevent recurrent replacing... bleh :(
+		   fplanque changed $smiley_masked .= substr($smiley, $i, 1).chr(160);
+		   better way: (added benefit: handles ' and " escaping for alt attribute!... needed in :') )
+		*/
 		$smiley_masked .=  '&#'.ord(substr($smiley, $i, 1)).';';
 	}
 	
@@ -120,71 +132,70 @@ foreach($b2smiliestrans as $smiley => $img)
 	$b2_smiliesreplace[] = "<img src='$smilies_directory/$img' alt='$smiley_masked' class='middle' />";
 }
 
-# the weekdays and the months.. 
-$weekday[0]=NT_('Sunday');
-$weekday[1]=NT_('Monday');
-$weekday[2]=NT_('Tuesday');
-$weekday[3]=NT_('Wednesday');
-$weekday[4]=NT_('Thursday');
-$weekday[5]=NT_('Friday');
-$weekday[6]=NT_('Saturday');
+// the weekdays and the months..
+$weekday[0] = NT_('Sunday');
+$weekday[1] = NT_('Monday');
+$weekday[2] = NT_('Tuesday');
+$weekday[3] = NT_('Wednesday');
+$weekday[4] = NT_('Thursday');
+$weekday[5] = NT_('Friday');
+$weekday[6] = NT_('Saturday');
 
-# the weekdays short form (typically 3 letters)
+// the weekdays short form (typically 3 letters)
 // TRANS: abbrev. for Sunday
-$weekday_abbrev[0]=NT_('Sun');
+$weekday_abbrev[0] = NT_('Sun');
 // TRANS: abbrev. for Monday
-$weekday_abbrev[1]=NT_('Mon');
+$weekday_abbrev[1] = NT_('Mon');
 // TRANS: abbrev. for Tuesday
-$weekday_abbrev[2]=NT_('Tue');
+$weekday_abbrev[2] = NT_('Tue');
 // TRANS: abbrev. for Wednesday
-$weekday_abbrev[3]=NT_('Wed');
+$weekday_abbrev[3] = NT_('Wed');
 // TRANS: abbrev. for Thursday
-$weekday_abbrev[4]=NT_('Thu');
+$weekday_abbrev[4] = NT_('Thu');
 // TRANS: abbrev. for Friday
-$weekday_abbrev[5]=NT_('Fri');
+$weekday_abbrev[5] = NT_('Fri');
 // TRANS: abbrev. for Saturday
-$weekday_abbrev[6]=NT_('Sat');
+$weekday_abbrev[6] = NT_('Sat');
 
-# the months
-$month['01']=NT_('January');
-$month['02']=NT_('February');
-$month['03']=NT_('March');
-$month['04']=NT_('April');
+// the months
+$month['01'] = NT_('January');
+$month['02'] = NT_('February');
+$month['03'] = NT_('March');
+$month['04'] = NT_('April');
 // TRANS: space at the end only to differentiate from short form. You don't need to keep it in the translation.
-$month['05']=NT_('May ');
-$month['06']=NT_('June');
-$month['07']=NT_('July');
-$month['08']=NT_('August');
-$month['09']=NT_('September');
-$month['10']=NT_('October');
-$month['11']=NT_('November');
-$month['12']=NT_('December');
+$month['05'] = NT_('May ');
+$month['06'] = NT_('June');
+$month['07'] = NT_('July');
+$month['08'] = NT_('August');
+$month['09'] = NT_('September');
+$month['10'] = NT_('October');
+$month['11'] = NT_('November');
+$month['12'] = NT_('December');
 
-# the months short form (typically 3 letters)
+// the months short form (typically 3 letters)
 // TRANS: abbrev. for January
-$month_abbrev['01']=NT_('Jan');
+$month_abbrev['01'] = NT_('Jan');
 // TRANS: abbrev. for February
-$month_abbrev['02']=NT_('Feb');
+$month_abbrev['02'] = NT_('Feb');
 // TRANS: abbrev. for March
-$month_abbrev['03']=NT_('Mar');
+$month_abbrev['03'] = NT_('Mar');
 // TRANS: abbrev. for April
-$month_abbrev['04']=NT_('Apr');
+$month_abbrev['04'] = NT_('Apr');
 // TRANS: abbrev. for May
-$month_abbrev['05']=NT_('May');
+$month_abbrev['05'] = NT_('May');
 // TRANS: abbrev. for June
-$month_abbrev['06']=NT_('Jun');
+$month_abbrev['06'] = NT_('Jun');
 // TRANS: abbrev. for July
-$month_abbrev['07']=NT_('Jul');
+$month_abbrev['07'] = NT_('Jul');
 // TRANS: abbrev. for August
-$month_abbrev['08']=NT_('Aug');
+$month_abbrev['08'] = NT_('Aug');
 // TRANS: abbrev. for September
-$month_abbrev['09']=NT_('Sep');
+$month_abbrev['09'] = NT_('Sep');
 // TRANS: abbrev. for October
-$month_abbrev['10']=NT_('Oct');
+$month_abbrev['10'] = NT_('Oct');
 // TRANS: abbrev. for November
-$month_abbrev['11']=NT_('Nov');
+$month_abbrev['11'] = NT_('Nov');
 // TRANS: abbrev. for December
-$month_abbrev['12']=NT_('Dec');
-
+$month_abbrev['12'] = NT_('Dec');
 
 ?>
