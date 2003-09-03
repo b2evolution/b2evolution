093c9a3b66629180d0376c87ee6a59f7295470d8
Francois Planque <github@fplanque.net>
2003-09-03 21:06:58 +0000

    comment spam prevention (vig-rx syndrome)

---
+++ blogs/conf/_antispam.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * b2evolution anti spam config
+ * Version of this file: 0.8.5
+ *
+ * Reminder: everything that starts with #, /* or // is a comment
+ */
+
+# Block URLs containing the following strings:
+$block_urls = array(	
+	'penis-enlargement',
+	'online-casino',
+	'order-viagra',
+	'order-phentermine',
+	'order-xenical',
+	'order-propecia',
+	'sexy-lingerie',
+	'-porn-',
+	'-adult-'
+	);
+
+?>
