4c6f6615938b2a51b0383733df630c215c21dea0
Francois Planque <github@fplanque.net>
2003-09-08 17:36:12 +0000

    no message

--- blogs/b2evocore/_main.php
+++ blogs/b2evocore/_main.php
@@ -9,6 +9,14 @@
  * It is useful when you want to do very customized templates!
  * It is also called by more complete initializers.
  */
+if( isset( $main_init ) )
+{	// Prevent double loading since require_once won't work in all situations
+	// on windows when some subfolders have caps :(
+	// (Check it out on static page generation)
+	return;
+}
+$main_init = true;
+
 require_once (dirname(__FILE__).'/../conf/_config.php');
 require_once (dirname(__FILE__).'/_vars.php');					// sets various arrays and vars for use in b2
 require_once (dirname(__FILE__).'/_functions_template.php');	// function to be called from templates
