5ec7fc47c7331ca97bab3391818067d568a46db6
t3dworld <t3dworld@users.sourceforge.net>
2004-01-08 23:00:12 +0000

    Added Mailing List message Support

--- blogs/b2evocore/_blog_main.php
+++ blogs/b2evocore/_blog_main.php
@@ -38,6 +38,7 @@ param( 'more', 'integer', 0, true );
 param( 'tb', 'integer', 0, true );
 param( 'pb', 'integer', 0, true );
 param( 'disp', 'string', '', true );
+param( 'message', 'string', '', true);   //Get message for mailing list
 param( 'stats', 'integer', 0 );					// deprecated
 if(!isset($timestamp_min)) $timestamp_min = '';
 if(!isset($timestamp_max)) $timestamp_max = '';
