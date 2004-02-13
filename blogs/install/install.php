5664e250a58666824d8e21576454eb9403be72ca
Vegar Guldal <vegar@guldal.no>
2004-02-13 19:50:46 +0000

    Yet another default anti-spam entry. We *really* need an easier solution.

--- blogs/install/install.php
+++ blogs/install/install.php
@@ -420,6 +420,8 @@ function populate_antispam()
 	mysql_query($query) or mysql_oops( $query );
 	$query = "INSERT INTO $tableantispam VALUES ('', 'bushflash.com')";
 	mysql_query($query) or mysql_oops( $query );
+	$query = "INSERT INTO $tableantispam VALUES ('', 'buechercrawler.de')";
+	mysql_query($query) or mysql_oops( $query );
 	
 	/*
 	These may look innocent and human-made, but they are in fact clever tricks.
