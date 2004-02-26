8571c3c16ad5145400e1a67c05dba53def9d6d1c
Vegar Guldal <vegar@guldal.no>
2004-02-26 10:06:33 +0000

    Another one.

--- blogs/install/install.php
+++ blogs/install/install.php
@@ -452,6 +452,8 @@ function populate_antispam()
 	mysql_query($query) or mysql_oops( $query );
 	$query = "INSERT INTO $tableantispam VALUES ('', 'drnapr.net')";
 	mysql_query($query) or mysql_oops( $query );
+	$query = "INSERT INTO $tableantispam VALUES ('', 'gogreenpower.org')";
+	mysql_query($query) or mysql_oops( $query );
 	
 	/*
 	These may look innocent and human-made, but they are in fact clever tricks.
