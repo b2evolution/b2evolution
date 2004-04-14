4776fd80685673f5ba31c7bde072644c0bd5997d
Daniel Hahler <git+b2evo@thequod.de>
2004-04-14 16:48:21 +0000

    simple fix.

--- blogs/admin/_edit_form.php
+++ blogs/admin/_edit_form.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Displays post editing form
  *
@@ -13,8 +13,8 @@
 ?>
 <script type="text/javascript" language="javascript">
 	<!--
-<?php 
-if ($use_spellchecker) 
+<?php
+if ($use_spellchecker)
 { // --------------------------- SPELL CHECKER -------------------------------
 	?>
 	function DoSpell(formname, subject, body)
@@ -31,17 +31,17 @@ if ($use_spellchecker)
 <?php
 }
 
- // --------------------------- PREVIEW -------------------------------
+// --------------------------- PREVIEW -------------------------------
 ?>
 	/*
 	 * open_preview()
 	 * fplanque: created
 	 */
-	function open_preview(form) 
+	function open_preview(form)
 	{
 		// Stupid thing: having a field called action !
 		var saved_action =  form.attributes.getNamedItem('action').value;
-		form.attributes.getNamedItem('action').value = '<?php $Blog->disp( 'dynurl', 'raw' ) ?>';
+		form.attributes.getNamedItem('action').value = '<' . '?php $Blog->disp( \'dynurl\', \'raw\' ) ?' . '>';
 		form.target = 'b2evo_preview';
 		form.submit();
 		preview_window = window.open( '', 'b2evo_preview' );
@@ -53,7 +53,7 @@ if ($use_spellchecker)
 	 * edit_reload()
 	 * fplanque: created
 	 */
-	function edit_reload( form, blog ) 
+	function edit_reload( form, blog )
 	{
 		form.attributes.getNamedItem('action').value = '<?php echo $pagenow ?>';
 		form.blog.value = blog;
@@ -64,7 +64,7 @@ if ($use_spellchecker)
 		return false;
 	}
 
-	function launchupload() 
+	function launchupload()
 	{
 		window.open ("b2upload.php", "b2upload", "width=380,height=360,location=0,menubar=0,resizable=1,scrollbars=yes,status=1,toolbar=0");
 	}
@@ -78,7 +78,7 @@ if ($use_spellchecker)
 <?php echo $admin_2col_start;  ?>
 
 <div class="bPost">
-	
+
 	<input type="hidden" id="blog" name="blog" value="<?php echo $blog ?>" />
 	<input type="hidden" id="action" name="action" value="<?php echo $form_action ?>" />
 	<input type="hidden" name="mode" value="<?php echo $mode ?>" />
@@ -89,23 +89,23 @@ if ($use_spellchecker)
 	<!-- In case we send this to the blog for a preview : -->
 	<input type="hidden" name="preview" value="1" />
 	<input type="hidden" name="preview_userid" value="<?php echo $user_ID ?>" />
-	
-	<?php 
-	
-	if ($action != 'editcomment') 
+
+	<?php
+
+	if ($action != 'editcomment')
 	{ // ------------------------------ POST HEADER -----------------------
 	?>
-	
+
 		<span class="line">
 		<label for="post_title"><strong><?php echo T_('Title') ?>:</strong></label>
 		<input type="text" name="post_title" size="45" value="<?php echo $edited_post_title; ?>" id="post_title" tabindex="1" />
 		</span>
-		
+
 		<span class="line">
 		<label for="post_lang"><strong><?php echo T_('Language') ?>:</strong></label>
 		<select name="post_lang" id="post_lang" tabindex="2"><?php lang_options( $post_lang ) ?></select>
 		</span>
-		
+
 		<?php if( $use_post_url ) { ?>
 		<span class="line">
 		<label for="post_url"><strong><?php echo T_('Link to url') ?>:</strong></label>
@@ -115,38 +115,38 @@ if ($use_spellchecker)
 		<input type="hidden" name="post_url"  size="40" value="" id="post_url" />
 		<?php
 		}
-	} 
-	else 
+	}
+	else
 	{ // ------------------------------ COMMENT HEADER -----------------------
 		?>
 		<input type="hidden" name="comment_ID" value="<?php echo $comment ?>" />
-	
+
 		<span class="line">
 		<label for="name"><strong><?php echo T_('Name') ?>:</strong></label><input type="text" name="newcomment_author" size="20" value="<?php echo format_to_edit($commentdata['comment_author']) ?>" id="name" tabindex="1" />
 		</span>
-		
+
 		<span class="line">
 		<label for="email"><strong><?php echo T_('Email') ?>:</strong></label><input type="text" name="newcomment_author_email" size="20" value="<?php echo format_to_edit($commentdata['comment_author_email']) ?>" id="email" tabindex="2" />
 		</span>
-		
+
 		<span class="line">
 		<label for="URL"><strong><?php echo T_('URL') ?>:</strong></label><input type="text" name="newcomment_author_url" size="20" value="<?php echo format_to_edit($commentdata['comment_author_url']) ?>" id="URL" tabindex="3" />
 		</span>
-	
+
 		<?php
 	}
 	?>
-	
+
 	<div class="center">
 	<?php // --------------------------- QUICK TAGS ------------------------------------
-		require( dirname(__FILE__).'/_quicktags.php'); 
+		require( dirname(__FILE__). '/_quicktags.php');
 	?>
 	</div>
-	
+
 	<?php // ---------------------------- TEXTAREA ------------------------------------- ?>
 	<div style="width:100%"><img src="img/blank.gif" width="1" height="1" alt="" border="0" /><textarea rows="18" cols="40" class="large" name="content" wrap="virtual" id="content" tabindex="4"><?php echo $content ?></textarea></div>
   <?php
-    if( $use_quicktags )   
+    if( $use_quicktags )
 		{
 			?>
 			<script type="text/javascript" language="JavaScript">
@@ -161,48 +161,48 @@ if ($use_spellchecker)
 	<?php // --------------------------- AUTOBR -------------------------------------- ?>
 	<input type="checkbox" class="checkbox" name="post_autobr" value="1" <?php
 	if ($autobr) echo ' checked="checked"' ?> id="autobr" tabindex="6" /><label for="autobr"><strong><?php echo T_('Auto-BR') ?></strong> <span class="notes"><?php echo T_('(converts line-breaks into &lt;br /&gt; tags)') ?></span></label><br />
-	
-	<?php	
-	if($use_preview && ($action != 'editcomment') )  
+
+	<?php
+	if($use_preview && ($action != 'editcomment') )
 	{ // ------------------------------- PREVIEW ---------------------------------- ?>
-		<input type="button" value="<?php echo T_('Preview') ?>" onClick="open_preview(this.form);" 
+		<input type="button" value="<?php echo T_('Preview') ?>" onClick="open_preview(this.form);"
 		class="search" tabindex="9" />
-	<?php 
+	<?php
 	}
-	
+
 	// ------------------------------- BLOG THIS ! ---------------------------------- ?>
-	<input type="submit" value="<?php echo ($action == 'post') ? T_('Blog this !') : 
-		T_('Edit this !'); ?>" class="search" style="font-weight: bold;" tabindex="10" /> 
-	
-	
-	<?php if ($use_spellchecker) 
+	<input type="submit" value="<?php echo ($action == 'post') ? T_('Blog this !') :
+		T_('Edit this !'); ?>" class="search" style="font-weight: bold;" tabindex="10" />
+
+
+	<?php if ($use_spellchecker)
 	{ // ------------------------------- SPELL CHECKER ---------------------------------- ?>
-		<input type="button" value="<?php echo T_('Spellcheck') ?>" 
+		<input type="button" value="<?php echo T_('Spellcheck') ?>"
 		onClick="DoSpell('post','content','');" class="search" tabindex="11" />
-	<?php } 
-	
-	if( $current_User->check_perm( 'upload' ) ) 
+	<?php }
+
+	if( $current_User->check_perm( 'upload' ) )
 	{ // ------------------------------- UPLOAD ---------------------------------- ?>
-		<input type="button" value="<?php echo T_('Upload a file/image') ?>" 
+		<input type="button" value="<?php echo T_('Upload a file/image') ?>"
 		onClick="launchupload();" class="search" tabindex="12"  />
 	<?php } ?>
-	
+
 	<fieldset>
 		<legend><?php echo T_('Advanced properties') ?></legend>
 
 		<?php
-		if( $current_User->check_perm( 'edit_timestamp' ) ) 
+		if( $current_User->check_perm( 'edit_timestamp' ) )
 		{	// ------------------------------------ TIME STAMP -------------------------------------
 			?>
 			<div>
-			<input type="checkbox" class="checkbox" name="edit_date" value="1" id="timestamp" 
+			<input type="checkbox" class="checkbox" name="edit_date" value="1" id="timestamp"
 				tabindex="13" <?php if( $edit_date ) echo 'checked="checked"' ?> />
 			<label for="timestamp"><strong><?php echo T_('Edit timestamp') ?></strong>:</label>
 			<span class="nobr">
 			<input type="text" name="jj" value="<?php echo $jj ?>" size="2" maxlength="2" tabindex="14" />
 			<select name="mm" tabindex="15">
-			<?php 
-			for ($i=1; $i < 13; $i=$i+1) 
+			<?php
+			for ($i=1; $i < 13; $i=$i+1)
 			{
 				echo "\t\t\t<option value=\"$i\"";
 				if ($i == $mm)
@@ -218,7 +218,7 @@ if ($use_spellchecker)
 				else
 					echo T_($month[$ii]);
 				echo "</option>\n";
-			} 
+			}
 			?>
 		</select>
 		<input type="text" name="aa" value="<?php echo $aa ?>" size="4" maxlength="5" tabindex="16" />
@@ -235,15 +235,15 @@ if ($use_spellchecker)
 			{ // --------------------------- PINGBACK --------------------------------------
 		?>
 		<div><input type="checkbox" class="checkbox" name="post_pingback" value="1" id="post_pingback" <?php
-		if ($post_pingback) echo ' checked="checked"' ?> tabindex="20" /><label for="post_pingback"><strong><?php echo T_('Pingback') ?></strong> <span class="notes"><?php echo T_('(Send a pingback to all URLs in this post)') ?></span></label></div>	
+		if ($post_pingback) echo ' checked="checked"' ?> tabindex="20" /><label for="post_pingback"><strong><?php echo T_('Pingback') ?></strong> <span class="notes"><?php echo T_('(Send a pingback to all URLs in this post)') ?></span></label></div>
 		<?php
 			}
-	
+
 			if( get_bloginfo('allowtrackbacks') )
 			{	// --------------------------- TRACKBACK --------------------------------------
 		?>
 		<div><label for="trackback"><strong><?php echo T_('Trackback URLs') ?>:</strong> <span class="notes"><?php echo T_('(Separate by space)') ?></span></label><br /><input type="text" name="trackback_url" class="large" id="trackback_url" tabindex="21" value="<?php echo format_to_edit( $post_trackbacks ); ?>" /></div>
-		<?php 
+		<?php
 			}
 		}
 		?>
@@ -255,58 +255,58 @@ if ($use_spellchecker)
 <?php
 echo $admin_2col_nextcol;
 
-if( $action != 'editcomment' ) 
+if( $action != 'editcomment' )
 { // ------------------------------- POST STATUS ---------------------------------- ?>
 	<div class="bSideItem2">
 
 	<fieldset title="Status">
 		<legend><?php echo T_('Status') ?></legend>
 
-		<?php 
+		<?php
 		if( $current_User->check_perm( 'blog_post_statuses', 'published', false, $blog ) )
 		{
 		?>
 		<label title="<?php echo T_('The post will be publicly published') ?>"><input type="radio" name="post_status" value="published" class="checkbox" <?php if( $post_status == 'published' ) echo 'checked="checked"'; ?>><?php echo T_('Published (Public)') ?></label><br />
-		<?php 
+		<?php
 		}
 		if( $current_User->check_perm( 'blog_post_statuses', 'protected', false, $blog ) )
 		{
 		?>
 		<label title="<?php echo T_('The post will be published but visible only by logged-in team members') ?>"><input type="radio" name="post_status" value="protected" class="checkbox" <?php if( $post_status == 'protected' ) echo 'checked="checked"'; ?>><?php echo T_('Protected (Members only)') ?></label><br />
-		<?php 
+		<?php
 		}
 		if( $current_User->check_perm( 'blog_post_statuses', 'private', false, $blog ) )
 		{
 		?>
 		<label title="<?php echo T_('The post will be published but visible only by yourself') ?>"><input type="radio" name="post_status" value="private" class="checkbox" <?php if( $post_status == 'private' ) echo 'checked="checked"'; ?>><?php echo T_('Private (You only)') ?></label><br />
-		<?php 
+		<?php
 		}
 		if( $current_User->check_perm( 'blog_post_statuses', 'draft', false, $blog ) )
 		{
 		?>
 		<label title="<?php echo T_('The post will appear only in the backoffice') ?>"><input type="radio" name="post_status" value="draft" class="checkbox" <?php if( $post_status == 'draft' ) echo 'checked="checked"'; ?>><?php echo T_('Draft (Not published!)') ?></label><br />
-		<?php 
+		<?php
 		}
 		if( $current_User->check_perm( 'blog_post_statuses', 'deprecated', false, $blog ) )
 		{
 		?>
 		<label title="<?php echo T_('The post will appear only in the backoffice') ?>"><input type="radio" name="post_status" value="deprecated" class="checkbox" <?php if( $post_status == 'deprecated' ) echo 'checked="checked"'; ?>><?php echo T_('Deprecated (Not published!)') ?></label><br />
-		<?php 
+		<?php
 		}
 		?>
 
 	</fieldset>
-	
 
-	
+
+
 	<fieldset title="Categories" class="extracats">
 		<legend><?php echo T_('Categories') ?></legend>
-	
+
 		<div class="extracats">
-	
+
 		<p class="extracatnote"><?php echo T_('Select main category in target blog and optionnaly check addtionnal categories') ?>:</p>
-	
-	<?php 
+
+	<?php
 		// ----------------------------  CATEGORIES ------------------------------
 		$default_main_cat = 0;
 
@@ -316,13 +316,13 @@ if( $action != 'editcomment' )
 		{	// callback to start sublist
 			echo "\n<ul>\n";
 		}
-		
+
 		function cat_select_before_each( $cat_ID, $level )
 		{	// callback to display sublist element
 			global $current_blog_ID, $blog, $cat, $postdata, $post_extracats, $default_main_cat, $action, $tabindex, $allow_cross_posting;
 			$this_cat = get_the_category_by_ID( $cat_ID );
 			echo '<li>';
-			
+
 			if( $allow_cross_posting )
 			{ // We allow cross posting, display checkbox:
 				echo'<input type="checkbox" name="post_extracats[]" class="checkbox" title="', T_('Select as an additionnal category') , '" value="',$cat_ID,'"';
@@ -330,7 +330,7 @@ if( $action != 'editcomment' )
 					echo ' checked="checked"';
 				echo '>';
 			}
-			
+
 			// Radio for main cat:
 			if( $current_blog_ID == $blog )
 			{
@@ -342,7 +342,7 @@ if( $action != 'editcomment' )
 				if( ($cat_ID == $postdata["Category"]) || ($cat_ID == $default_main_cat))
 					echo ' checked="checked"';
 				echo '>';
-			}		
+			}
 			echo $this_cat['cat_name'];
 		}
 		function cat_select_after_each( $cat_ID, $level )
@@ -353,7 +353,7 @@ if( $action != 'editcomment' )
 		{	// callback to end sublist
 			echo "</ul>\n";
 		}
-	
+
 		if( $allow_cross_posting == 2 )
 		{	// If BLOG cross posting enabled, go through all blogs with cats:
 			foreach( $cache_blogs as $i_blog )
@@ -362,7 +362,7 @@ if( $action != 'editcomment' )
 				if( ! blog_has_cats( $current_blog_ID ) ) continue;
 				if( ! $current_User->check_perm( 'blog_post_statuses', 'any', false, $current_blog_ID ) ) continue;
 				echo "<h4>".$i_blog->blog_name."</h4>\n";
-				cat_children( $cache_categories, $current_blog_ID, NULL, 'cat_select_before_first', 
+				cat_children( $cache_categories, $current_blog_ID, NULL, 'cat_select_before_first',
 											'cat_select_before_each', 'cat_select_after_each', 'cat_select_after_last', 1 );
 			}
 			?>
@@ -372,7 +372,7 @@ if( $action != 'editcomment' )
 		else
 		{	// BLOG Cross posting is disabled. Current blog only:
 			$current_blog_ID = $blog;
-			cat_children( $cache_categories, $current_blog_ID, NULL, 'cat_select_before_first', 
+			cat_children( $cache_categories, $current_blog_ID, NULL, 'cat_select_before_first',
 										'cat_select_before_each', 'cat_select_after_each', 'cat_select_after_last', 1 );
 			?>
 			<p class="notes"><?php
@@ -401,7 +401,7 @@ if( $action != 'editcomment' )
 <?php
 }
 
-if ($action == "editcomment") 
+if ($action == "editcomment")
 {
 ?>
 	<div class="bSideItem">
@@ -412,9 +412,9 @@ if ($action == "editcomment")
 	</div>
 <?php
 }
-/* elseif ($action == "edit") 
+/* elseif ($action == "edit")
 {
-// 		<p><strong>Pings:</strong> <?php echo in_array( 'pingsdone', $postdata["Flags"] ) ? 'Done':'Not done yet'; 
+// 		<p><strong>Pings:</strong> <?php echo in_array( 'pingsdone', $postdata["Flags"] ) ? 'Done':'Not done yet';
 }*/
 
 echo $admin_2col_end;
