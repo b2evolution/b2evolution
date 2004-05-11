16f3d71d22389d934aa0d07faca4834ce5a5a5b5
Francois Planque <github@fplanque.net>
2004-05-11 20:56:12 +0000

    no message

--- blogs/admin/_blogs_general.form.php
+++ blogs/admin/_blogs_general.form.php
@@ -10,10 +10,49 @@
  */
 ?>
 <form class="fform" method="post">
-	<input type="hidden" name="action" value="update" />
+	<input type="hidden" name="action" value="<?php echo $next_action ?>" />
 	<input type="hidden" name="blog" value="<?php echo $blog; ?>" />
 
-	<?php require( dirname(__FILE__) . '/_blogs_main.subform.php' ); ?>
+	<fieldset>
+		<legend><?php echo T_('General parameters') ?></legend>
+		<?php
+			form_text( 'blog_name', $blog_name, 50, T_('Full Name'), T_('Will be displayed on top of the blog.') );
+			form_text( 'blog_shortname', $blog_shortname, 12, T_('Short Name'), T_('Will be used in selection menus and throughout the admin interface.') );
+			form_select( 'blog_locale', $blog_locale, 'locale_options', T_('Main Locale'), T_('Determines the language of the navigation links on the blog.') );
+		?>
+	</fieldset>
+	
+	<fieldset>
+		<legend><?php echo T_('Access parameters') ?></legend>
+	
+		<?php
+			if( $Settings->get('default_blog_ID') && ($Settings->get('default_blog_ID') != $blog) )
+			{
+				$defblog = $BlogCache->get_by_ID($Settings->get('default_blog_ID'));
+				$defblog = $defblog->dget('shortname');
+			}
+			form_radio( 'blog_access_type', $blog_access_type,
+					array(  array( 'default', T_('Default blog on index.php'), $baseurl.'index.php'.
+													( isset($defblog) ? '  ['. /* TRANS: current default blog */ 
+													T_('Current default is:').' '.$defblog.']' : '' ) ),
+									array( 'index.php', T_('Other blog through index.php'), 
+													$baseurl.'index.php'.( 
+													($Settings->get('links_extrapath')) ? '/'.$blog_stub : '?blog='.$blog) ),
+									array( 'stub', T_('Other blog through stub file'), $baseurl.$blog_stub ),
+								), T_('Preferred access type'), true );
+		?>
+		
+		<fieldset>
+			<div class="label"><label for="blog_siteurl"><?php echo T_('Blog Folder URL') ?>: </label></div>
+			<div class="input"><code><?php echo $baseurl ?></code><input type="text" name="blog_siteurl" id="blog_siteurl" size="40" maxlength="120" value="<?php echo format_to_output($blog_siteurl, 'formvalue') ?>"/>
+			<span class="notes"><?php echo T_('No trailing slash. (If you don\'t know, leave this field empty.)') ?></span></div>
+		</fieldset>
+	
+	
+		<?php
+			form_text( 'blog_stub', $blog_stub, 20, T_('URL blog name / Stub name'), T_('Used in URLs to identify this blog. This should be the stub filename if you use stub file access.'), 30 );
+		?>
+	</fieldset>
 				
 	<fieldset>
 		<legend><?php echo T_('Default display options') ?></legend>
@@ -26,7 +65,7 @@
 
 			form_checkbox( 'blog_in_bloglist', $blog_in_bloglist, T_('Include in public blog list'), T_('Check this if you want to this blog to be displayed in the list of all public blogs.') );
 		
-			form_select_object( 'blog_linkblog', $edited_Blog->get('links_blog_ID'), $BlogCache, T_('Default linkblog'), T_('Will be displayed next to this blog (if your skin supports this).'), true );
+			form_select_object( 'blog_linkblog', $blog_linkblog, $BlogCache, T_('Default linkblog'), T_('Will be displayed next to this blog (if your skin supports this).'), true );
 		?>
 	</fieldset>
 
@@ -60,7 +99,7 @@
 	<fieldset>
 		<fieldset>
 			<div class="input">
-				<input type="submit" name="submit" value="<?php echo T_('Update blog!') ?>" class="search">
+				<input type="submit" name="submit" value="<?php echo ($next_action == 'create') ? T_('Create new blog!') : T_('Update blog!') ?>" class="search">
 				<input type="reset" value="<?php echo T_('Reset') ?>" class="search">
 			</div>
 		</fieldset>
