36bc251a692025c3380d715ee15fc373aaeaeeb3
Francois Planque <github@fplanque.net>
2004-06-07 19:04:08 +0000

    no message

---
+++ blogs/install/_functions_miniblog.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * This file implements upgrading from Manywhere Miniblog
+ *
+ * b2evolution - {@link http://b2evolution.net/}
+ * Released under GNU GPL License - {@link http://b2evolution.net/about/license.html}
+ * @copyright (c)2003-2004 by Francois PLANQUE - {@link http://fplanque.net/}
+ *
+ * @package install
+ */
+if( substr(basename($_SERVER['SCRIPT_FILENAME']), 0, 1) == '_' )
+	die( 'Please, do not access this page directly.' );
+
+
+
+/*
+ * upgrade_cafelog_tables(-)
+ */
+function upgrade_miniblog_tables()
+{
+	global $tableposts, $tableusers, $tablesettings, $tablecategories, $tablecomments,
+					$tableblogs, $tablepostcats, $tablehitlog, $tableantispam, $tablegroups, $tableblogusers;
+	global $baseurl, $old_db_version, $new_db_version;
+	global $default_locale;
+	global $timestamp, $admin_email;
+	global $Group_Admins, $Group_Priviledged, $Group_Bloggers, $Group_Users;
+	global $blog_all_ID, $blog_a_ID, $blog_b_ID, $blog_linkblog_ID;
+	global $cat_ann_a, $cat_news, $cat_bg, $cat_ann_b, $cat_fun, $cat_life, $cat_web, $cat_sports, $cat_movies, $cat_music, $cat_b2evo, $cat_linkblog_b2evo, $cat_linkblog_contrib;
+	global $DB;
+	global $install_password, $random_password;
+
+	// Create blogs:
+	create_default_blogs( 'Blog A (Upg)', 'Blog A (Miniblog Upgrade)', T_("This blog holds all your posts upgraded from Miniblog. This blog is named '%s'. %s"), false );	
+
+
+	echo 'Creating default settings...';
+	$query = "INSERT INTO $tablesettings (set_name, set_value) 
+						VALUES
+									('db_version', '$new_db_version'),
+									('default_locale', '$default_locale'),
+									('posts_per_page', '5'),
+									('what_to_show', 'paged'),
+									('archive_mode', 'monthly'),
+									('time_difference', '0'),
+									('AutoBR', '0'),
+									('antispam_last_update', '2000-01-01 00:00:00'),
+									('newusers_grp_ID', '".$Group_Users->get('ID')."' ),
+									('newusers_level', '1'),
+									('newusers_canregister', '0'),
+									('links_extrapath', '0'),
+									('permalink_type', 'urltitle'),
+									( 'user_minpwdlen', '5' )
+									";
+	$DB->query( $query );
+	echo "OK.<br />\n";
+	
+		
+	echo 'Copying Miniblog users... ';
+	if( !isset( $install_password ) )
+	{
+		$random_password = substr(md5(uniqid(microtime())),0,6);
+	}
+	else
+	{
+		$random_password = $install_password;
+	}
+	// Admins:
+	$query = "INSERT INTO $tableusers( ID, user_login, user_pass, user_firstname, user_lastname,
+							user_nickname, user_email, user_url, user_level, user_locale, user_idmode, user_notify, user_grp_ID ) 
+						SELECT id, email, '".md5($random_password)."', '', '', name, email, homePage, 10, '$default_locale', 'nickname', 1, $Group_Admins->ID
+						FROM user
+						WHERE role = 'admin'";
+	$DB->query( $query );
+	// Bloggers:
+	$query = "INSERT INTO $tableusers( ID, user_login, user_pass, user_firstname, user_lastname,
+							user_nickname, user_email, user_url, user_level, user_locale, user_idmode, user_notify, user_grp_ID ) 
+						SELECT id, email, '".md5($random_password)."', '', '', name, email, homePage, 1, '$default_locale', 'nickname', 1, $Group_Bloggers->ID
+						FROM user
+						WHERE role = 'user'";
+	$DB->query( $query );
+	// Regular users:
+	$query = "INSERT INTO $tableusers( ID, user_login, user_pass, user_firstname, user_lastname,
+							user_nickname, user_email, user_url, user_level, user_locale, user_idmode, user_notify, user_grp_ID ) 
+						SELECT id, email, '".md5($random_password)."', '', '', name, email, homePage, 1, '$default_locale', 'nickname', 1, $Group_Users->ID
+						FROM user
+						WHERE role <> 'admin' and role <> 'user'";
+	$DB->query( $query );
+	echo "OK.<br />\n";
+
+	echo 'Make sure user #1 is at level 10... ';
+	$query = "UPDATE $tableusers
+							 SET user_level = 10
+						 WHERE ID = 1";
+	$DB->query( $query );
+	echo "OK.<br />\n";
+
+	echo 'Creating user blog permissions... ';
+	// Admin for all blogs:
+	$query = "INSERT INTO $tableblogusers( bloguser_blog_ID, bloguser_user_ID, bloguser_ismember,
+							bloguser_perm_poststatuses, bloguser_perm_delpost, bloguser_perm_comments, 
+							bloguser_perm_cats, bloguser_perm_properties)
+						SELECT blog_ID, ID, 1, 'published,deprecated,protected,private,draft', 1, 1, 1, 1
+						FROM $tableusers, $tableblogs
+						WHERE user_level = 10";
+	$DB->query( $query );
+
+	// Normal users for upgraded blog:
+	$query = "INSERT INTO $tableblogusers( bloguser_blog_ID, bloguser_user_ID, bloguser_ismember, 
+							bloguser_perm_poststatuses, bloguser_perm_delpost, bloguser_perm_comments, 
+							bloguser_perm_cats, bloguser_perm_properties)
+						SELECT $blog_a_ID, ID, 1, 'published,protected,private,draft', 0, 1, 0, 0
+						FROM $tableusers
+						WHERE user_level > 0 AND user_level < 10";
+	$DB->query( $query );
+	echo "OK.<br />\n";
+	
+	echo 'Creating a default category...';
+	$cat_imported = cat_create( 'Imported', 'NULL', $blog_a_ID );
+	echo "OK.<br />\n";
+
+	echo 'Copying Miniblog posts... ';
+	$query = "INSERT INTO $tableposts( ID, post_author, post_issue_date, post_mod_date, post_status, post_locale, post_content, post_title, post_category, post_autobr, post_flags, post_comments )  
+						SELECT id, authorId, created, modified, 'published', '$default_locale', content, title, $cat_imported, 0, 'pingsdone,imported', 'open'
+						FROM miniblog
+						WHERE parentId = 0";
+	$DB->query( $query );
+	echo "OK.<br />\n";
+
+	echo 'Generating wordcounts... ';
+	$query = "SELECT ID, post_content FROM $tableposts";
+	$q = $DB->get_results( $query, ARRAY_A );
+	if( count( $q ) ) foreach( $q as $row )
+	{
+		$query_update_wordcount = "UPDATE $tableposts 
+															SET post_wordcount = " . bpost_count_words($row['post_content']) . "
+															WHERE ID = " . $row['ID'];
+		$DB->query($query_update_wordcount);
+	}
+	echo "OK. (".count($q)." rows updated)<br />\n";
+
+	echo 'Generating postcats... ';
+	$query = "INSERT INTO $tablepostcats( postcat_post_ID, postcat_cat_ID ) 
+						SELECT ID, post_category FROM $tableposts";		
+	$DB->query( $query );
+	echo "OK.<br />\n";
+	
+	echo 'Copying Miniblog comments... ';
+	$query = "INSERT INTO $tablecomments( comment_ID, comment_post_ID, comment_type, comment_date, comment_content ) 
+						SELECT id, parentId, 'comment', created, content
+						FROM miniblog
+						WHERE parentId > 1";
+	$DB->query( $query );
+	echo "OK.<br />\n";
+	
+		
+	create_default_categories( false /* not for A */ );
+
+	// POPULATE THE LINKBLOG:
+	populate_linkblog( $now, $cat_linkblog_b2evo, $cat_linkblog_contrib );
+
+	// Create other default contents:
+	create_default_contents( false /* not for A */ );
+
+}
+
+?>
---
+++ blogs/install/_functions_wordpress.php
@@ -0,0 +1,168 @@
+<?php
+/**
+ * This file implements upgrading from WordPress
+ *
+ * b2evolution - {@link http://b2evolution.net/}
+ * Released under GNU GPL License - {@link http://b2evolution.net/about/license.html}
+ * @copyright (c)2003-2004 by Francois PLANQUE - {@link http://fplanque.net/}
+ *
+ * @package install
+ */
+if( substr(basename($_SERVER['SCRIPT_FILENAME']), 0, 1) == '_' )
+	die( 'Please, do not access this page directly.' );
+
+
+
+/*
+ * upgrade_cafelog_tables(-)
+ */
+function upgrade_cafelog_tables()
+{
+	global $tableposts, $tableusers, $tablesettings, $tablecategories, $tablecomments,
+					$tableblogs, $tablepostcats, $tablehitlog, $tableantispam, $tablegroups, $tableblogusers;
+	global $baseurl, $old_db_version, $new_db_version;
+	global $default_locale;
+	global $timestamp, $admin_email;
+	global $Group_Admins, $Group_Priviledged, $Group_Bloggers, $Group_Users;
+	global $blog_all_ID, $blog_a_ID, $blog_b_ID, $blog_linkblog_ID;
+	global $cat_ann_a, $cat_news, $cat_bg, $cat_ann_b, $cat_fun, $cat_life, $cat_web, $cat_sports, $cat_movies, $cat_music, $cat_b2evo, $cat_linkblog_b2evo, $cat_linkblog_contrib;
+	global $DB, $wp_prefix;
+
+	// Create blogs:
+	create_default_blogs( 'Blog A (Upg)', 'Blog A (WordPress Upgrade)', T_("This blog holds all your posts upgraded from WordPress. This blog is named '%s'. %s"), false );	
+
+
+	echo 'Creating default settings...';
+	$query = "INSERT INTO $tablesettings (set_name, set_value) 
+						VALUES
+									('db_version', '$new_db_version'),
+									('default_locale', '$default_locale'),
+									('posts_per_page', '5'),
+									('what_to_show', 'paged'),
+									('archive_mode', 'monthly'),
+									('time_difference', '0'),
+									('AutoBR', '0'),
+									('antispam_last_update', '2000-01-01 00:00:00'),
+									('newusers_grp_ID', '".$Group_Users->get('ID')."' ),
+									('newusers_level', '1'),
+									('newusers_canregister', '0'),
+									('links_extrapath', '0'),
+									('permalink_type', 'urltitle'),
+									( 'user_minpwdlen', '5' )
+									";
+	$DB->query( $query );
+	echo "OK.<br />\n";
+	
+	echo 'Copying WordPress users... ';
+	$query = "INSERT INTO $tableusers( ID, user_login, user_pass, user_firstname, user_lastname,
+							user_nickname, user_icq, user_email, user_url, user_ip, user_domain, user_browser,
+							dateYMDhour, user_level,	user_aim, user_msn, user_yim, user_idmode ) 
+						SELECT ID, user_login, user_pass, user_firstname, user_lastname, user_nickname,
+							user_icq, user_email, user_url, user_ip, user_domain, user_browser, dateYMDhour,
+							user_level,	user_aim, user_msn, user_yim, user_idmode 
+						FROM ".$wp_prefix.'users';
+	$DB->query( $query );
+	echo "OK.<br />\n";
+
+	echo 'Make sure user #1 is at level 10... ';
+	$query = "UPDATE $tableusers
+							 SET user_level = 10
+						 WHERE ID = 1";
+	$DB->query( $query );
+	echo "OK.<br />\n";
+
+	echo 'Setting groups... ';
+	$query = "UPDATE $tableusers
+							 SET user_grp_ID = ".$Group_Users->get('ID')."
+						 WHERE user_level = 0";
+	$DB->query( $query );
+
+	$query = "UPDATE $tableusers
+							 SET user_grp_ID = ".$Group_Bloggers->get('ID')."
+						 WHERE user_level > 0 and user_level < 10 ";
+	$DB->query( $query );
+
+  // Level 10 users get default group (1)
+
+	echo "OK.<br />\n";
+
+	echo 'Creating user blog permissions... ';
+	// Admin for all blogs:
+	$query = "INSERT INTO $tableblogusers( bloguser_blog_ID, bloguser_user_ID, bloguser_ismember,
+							bloguser_perm_poststatuses, bloguser_perm_delpost, bloguser_perm_comments, 
+							bloguser_perm_cats, bloguser_perm_properties)
+						SELECT blog_ID, ID, 1, 'published,deprecated,protected,private,draft', 1, 1, 1, 1
+						FROM $tableusers, $tableblogs
+						WHERE user_level = 10";
+	$DB->query( $query );
+
+	// Normal users for upgraded blog:
+	$query = "INSERT INTO $tableblogusers( bloguser_blog_ID, bloguser_user_ID, bloguser_ismember, 
+							bloguser_perm_poststatuses, bloguser_perm_delpost, bloguser_perm_comments, 
+							bloguser_perm_cats, bloguser_perm_properties)
+						SELECT $blog_a_ID, ID, 1, 'published,protected,private,draft', 0, 1, 0, 0
+						FROM $tableusers
+						WHERE user_level > 0 AND user_level < 10";
+	$DB->query( $query );
+	echo "OK.<br />\n";
+	
+	echo 'Copying WordPress categories... ';
+	$query = "INSERT INTO $tablecategories( cat_ID, cat_parent_ID, cat_name, cat_blog_ID, cat_longdesc ) 
+						SELECT DISTINCT cat_ID, category_parent, cat_name, 2, category_description
+						FROM ".$wp_prefix."categories";
+	$DB->query( $query );
+	echo "OK.<br />\n";
+
+	
+	echo 'Copying Cafelog posts... ';
+	// Warning: This will copy a lot of categories to 0. We need to fix those later!
+	$query = "INSERT INTO $tableposts( ID, post_author, post_issue_date, post_mod_date, post_status, post_locale, post_content,post_title, post_category, post_autobr, post_flags, post_karma)  
+						SELECT ID, post_author, post_date, post_date, 'published', '$default_locale', post_content, post_title, post_category, 1, 'pingsdone,imported', post_karma 
+						FROM $wp_prefix"."posts";
+	$DB->query( $query );
+	echo "OK.<br />\n";
+
+	echo 'Generating wordcounts... ';
+	$query = "SELECT ID, post_content FROM $tableposts";
+	$q = $DB->get_results( $query, ARRAY_A );
+	if( count( $q ) ) foreach( $q as $row )
+	{
+		$query_update_wordcount = "UPDATE $tableposts 
+															SET post_wordcount = " . bpost_count_words($row['post_content']) . "
+															WHERE ID = " . $row['ID'];
+		$DB->query($query_update_wordcount);
+	}
+	echo "OK. (".count($q)." rows updated)<br />\n";
+
+	echo 'Generating postcats... ';
+	$query = "INSERT INTO $tablepostcats( postcat_post_ID, postcat_cat_ID ) SELECT ID, post_category FROM $tableposts";		
+	$DB->query( $query );
+	echo "OK.<br />\n";
+	
+	echo 'Copying Cafelog comments... ';
+	$query = "INSERT INTO $tablecomments( comment_ID, comment_post_ID, comment_type, comment_author, comment_author_email, comment_author_url, comment_author_IP, comment_date, comment_content, comment_karma ) SELECT comment_ID, comment_post_ID, 'comment', comment_author, comment_author_email, comment_author_url, comment_author_IP, comment_date, comment_content, comment_karma FROM $oldtablecomments";
+	$DB->query( $query );
+	echo "OK.<br />\n";
+
+	echo 'Qualifying comments... Trackback... ';
+	$query = "UPDATE $tablecomments SET comment_type = 'trackback' WHERE comment_content LIKE '<trackback />%'";		
+	$DB->query( $query );
+	echo "OK.<br />\n";	
+
+	echo 'Qualifying comments... Pingback... ';
+	$query = "UPDATE $tablecomments SET comment_type = 'pingback' WHERE comment_content LIKE '<pingback />%'";		
+	$DB->query( $query );
+	echo "OK.<br />\n";	
+	
+	
+	create_default_categories( false /* not for A */ );
+
+	// POPULATE THE LINKBLOG:
+	populate_linkblog( $now, $cat_linkblog_b2evo, $cat_linkblog_contrib );
+
+	// Create other default contents:
+	create_default_contents( false /* not for A */ );
+
+}
+
+?>
