040c65ece936d83828263bbc2dc5fcd6f9b82c45
wmcroberts <wmcroberts@users.sourceforge.net>
2004-06-21 18:33:59 +0000

    changed get to socket_get changed post to socket_post
    changed to use \n instead of letting the server chose

--- blogs/b2evocore/_class_http.php
+++ blogs/b2evocore/_class_http.php
@@ -1,5 +1,5 @@
 <?
-/**
+ /**
  * This classfile implements http gets and posts
  *
  * b2evolution - {@link http://b2evolution.net/}
@@ -17,15 +17,15 @@
  * @author Welby McRoberts - {@link http://www.wheely-bin.co.uk/}
  */
 class http {
-  
-  function post($server, $port, $url, $vars) {
+
+  function socket_post($server, $port, $url, $vars) {
    /*
     *   $server = "www.wheely-bin.co.uk"
     *   $port   = 80
     *   $url    = "/facecake.php"
     *   $vars   = array("who" => "am three", "face" => cake")
     */
-   
+
         // lets make our ua something unique to b2evo ...
 	$user_agent = "b2evolution";
         $urlencoded = "";
@@ -37,17 +37,8 @@ class http {
 	//set the content length
         $content_length = strlen($urlencoded);
         //what headers do we need?
-	$headers = "POST $url HTTP/1.1
-Accept: */*
-Accept-Language: en
-Content-Type: application/x-www-form-urlencoded
-User-Agent: $user_agent
-Host: $server
-Connection: Keep-Alive
-Cache-Control: no-cache
-Content-Length: $content_length
-
-";
+        // changed this so its using \n, as some servers will send out \r\n (fscking iis BURN IT)
+	$headers = "POST $url HTTP/1.1\nAccept: */*\nAccept-Language: en\nContent-Type: application/x-www-form-urlencoded\nUser-Agent: $user_agent\nHost: $server\nConnection: Keep-Alive\nCache-Control: no-cache\nContent-Length: $content_length\n\n";
 	// lets open a socket for the post, 
 	$post = fsockopen($server, $port);
 	//could we create the socket ?
@@ -72,7 +63,7 @@ Content-Length: $content_length
   }
 
 
-  function get($server, $port, $url, $vars) {
+  function socket_get($server, $port, $url, $vars) {
   /*
    *   $server = "www.wheely-bin.co.uk"
    *   $port   = 80
@@ -94,15 +85,8 @@ Content-Length: $content_length
         //lets make geturl, be $url and $urlencoded
         $geturl = $url . "?" . $urlencoded;
         //what headers do we need?
-	$headers = "GET $geturl HTTP/1.1
-Accept: */*
-User-Agent: $user_agent
-Host: $server
-Connection: Keep-Alive
-Cache-Control: no-cache
-Content-Length: $content_length
-
-";
+        // changed this so its using \n, as some servers will send out \r\n (fscking iis BURN IT)
+	$headers = "GET $geturl HTTP/1.1\nAccept: */*\nUser-Agent: $user_agent\nHost: $server\nConnection: Keep-Alive\nCache-Control: no-cache\nContent-Length: $content_length\n\n";
 
 	// lets open a socket for the post, 
 	$get = fsockopen($server, $port);
