26a37e030c0c31515c81ae9932c0d84ebce7f38b
wmcroberts <wmcroberts@users.sourceforge.net>
2004-06-21 18:29:57 +0000

    first revision of download class (not complete)

---
+++ blogs/b2evocore/_class_download.php
@@ -0,0 +1,65 @@
+<?
+/**
+ * This classfile implements downloads (from say, SF to the webserver)
+ *
+ * b2evolution - {@link http://b2evolution.net/}
+ * Released under GNU GPL License - {@link http://b2evolution.net/about/license.html}
+ * @copyright (c)2004 by Welby McRoberts - {@link http://www.wheely-bin.co.uk/}
+ *
+ * @package evocore
+ */
+ //dirname(__FILE__).'/
+require_once( '_class_http.php');
+class download
+{
+
+      	function saveToTemp ($url, $type, $destination)
+	{
+         /*
+         *   $url = "http://www.wheely-bin.co.uk/facecake.php"
+         *   $type   =  get
+         *              post
+         *   $destination   = "/tmp/beecat.tar.gz"
+         */
+         if (!is_null($url){
+          /*
+           print_r(parse_url("http://username:password@hostname:81/path?arg=value#anchor"));
+           Array
+           (
+                [scheme] => http
+                [host] => hostname
+                [port] => 81
+                [user] => username
+                [pass] => password
+                [path] => /path
+                [query] => arg=value
+                [fragment] => anchor
+           )
+           */
+            $url_array = parse_url($url)
+            $hostname = $url_array["host"];
+            $path = $url_array["url"];
+            $vars = $url_array["query"];
+            $port = $url_array["port"];
+
+          };
+          else { die();};
+         if (!is_null($destination){
+          };
+          else { die();};
+          switch ($type)
+             {
+                case "get":
+                        http::get($hostname, $port, $url, $vars);
+                        break;
+                case "post":
+                        http::post($server, $port, $url, $vars);
+                        break;
+                default:
+                        http::get($server, $port, $url, $vars);
+                        break;
+             };
+        };
+};
+
+?>
