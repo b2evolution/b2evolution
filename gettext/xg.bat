df5178f1d58b5d0df9c957cc1a2af9f1c1d8b8f6
Daniel Hahler <git+b2evo@thequod.de>
2004-07-07 19:40:12 +0000

    no-wrap

--- gettext/xg.bat
+++ gettext/xg.bat
@@ -1,18 +1,18 @@
 @echo off
 echo Updating all .po files with latest translatable strings...
 dir /b /s ..\blogs\*.php > files.txt
-xgettext -D ../blogs/ -f files.txt --add-comments=TRANS --copyright-holder="Francois PLANQUE" --msgid-bugs-address=http://fplanque.net/ --output=..\blogs\locales\messages.pot --keyword=T_ --keyword=NT_
+xgettext -D ../blogs/ -f files.txt --no-wrap --add-comments=TRANS --copyright-holder="Francois PLANQUE" --msgid-bugs-address=http://fplanque.net/ --output=..\blogs\locales\messages.pot --keyword=T_ --keyword=NT_
 xchangecl +d! ..\blogs\locales\messages.pot !C:\www\b2evo091\blogs\!..\..\..\!
-msgmerge -U ..\blogs\locales\cs_CZ\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
-msgmerge -U ..\blogs\locales\de_DE\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
-msgmerge -U ..\blogs\locales\es_ES\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
-msgmerge -U ..\blogs\locales\fr_FR\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
-msgmerge -U ..\blogs\locales\it_IT\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
-msgmerge -U ..\blogs\locales\ja_JP\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
-msgmerge -U ..\blogs\locales\lt_LT\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
-msgmerge -U ..\blogs\locales\nb_NO\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
-msgmerge -U ..\blogs\locales\nl_NL\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
-msgmerge -U ..\blogs\locales\pt_BR\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
-msgmerge -U ..\blogs\locales\sv_SE\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
-msgmerge -U ..\blogs\locales\zh_CN\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
-msgmerge -U ..\blogs\locales\zh_TW\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
+msgmerge -U --no-wrap ..\blogs\locales\cs_CZ\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
+msgmerge -U --no-wrap ..\blogs\locales\de_DE\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
+msgmerge -U --no-wrap ..\blogs\locales\es_ES\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
+msgmerge -U --no-wrap ..\blogs\locales\fr_FR\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
+msgmerge -U --no-wrap ..\blogs\locales\it_IT\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
+msgmerge -U --no-wrap ..\blogs\locales\ja_JP\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
+msgmerge -U --no-wrap ..\blogs\locales\lt_LT\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
+msgmerge -U --no-wrap ..\blogs\locales\nb_NO\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
+msgmerge -U --no-wrap ..\blogs\locales\nl_NL\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
+msgmerge -U --no-wrap ..\blogs\locales\pt_BR\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
+msgmerge -U --no-wrap ..\blogs\locales\sv_SE\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
+msgmerge -U --no-wrap ..\blogs\locales\zh_CN\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
+msgmerge -U --no-wrap ..\blogs\locales\zh_TW\LC_MESSAGES\messages.po ..\blogs\locales\messages.pot
