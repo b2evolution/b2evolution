7b5cd866f9e42cb35014769a614e884cdb3d691b
Daniel Hahler <git+b2evo@thequod.de>
2005-03-13 18:55:21 +0000

    use "Name <recipient@example.com>" format for recipient (gets handled in send_mail())

--- blogs/htsrv/message_send.php
+++ blogs/htsrv/message_send.php
@@ -36,9 +36,7 @@ $recipient_address = '';
 if( !empty( $recipient_id ) )
 { // Get the email address for the recipient if a member.
 	$user = & $UserCache->get_by_ID( $recipient_id );
-	// fplanque: this fails on my php/mailserver:
-	// $recipient_address = trim($user->get('preferedname')) . ' <' . $user->get('email') . '>';
-	$recipient_address = $user->get('email');
+	$recipient_address = trim($user->get('preferedname')) . ' <' . $user->get('email') . '>';
 	// Change the locale so the email is in the recipients language
 	locale_temp_switch($user->locale);
 }
@@ -49,9 +47,7 @@ elseif( !empty( $comment_id ) )
 					FROM T_comments
 					WHERE comment_ID =' . $comment_id;
 	$row = $DB->get_row( $sql );
-	// fplanque: this fails on my php/mailserver:
-	// $recipient_address = trim($row->comment_author) . ' <' . $row->comment_author_email . '>';
-	$recipient_address = $row->comment_author_email;
+	$recipient_address = trim($row->comment_author) . ' <' . $row->comment_author_email . '>';
 }
 
 if( empty($recipient_address) )
