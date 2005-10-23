1b13b0f0dd07c60fba3d9fc525bffeac11e25cd6
Daniel Hahler <git+b2evo@thequod.de>
2005-10-23 18:02:40 +0000

    Use XHTML Strict, $app_name and debug_die().

--- blogs/evocore/_404_not_found.page.php
+++ blogs/evocore/_404_not_found.page.php
@@ -13,18 +13,18 @@
 
 header('HTTP/1.0 404 Not Found');
 ?>
- <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
- 		<title>404 Not Found</title>
+		<title>404 Not Found</title>
 	</head>
 	<body>
 		<h1>404 Not Found</h1>
-		<p>b2evolution cannot resolve the requested URL.</p>
-  </body>
+		<p><?php echo $app_name ?> cannot resolve the requested URL.</p>
+	</body>
 </html>
 <?php
 	debug_info();
- 	exit;
+	debug_die();
 ?>
--- blogs/evocore/_410_stats_gone.page.php
+++ blogs/evocore/_410_stats_gone.page.php
@@ -14,18 +14,18 @@
 
 header('HTTP/1.0 410 Gone');
 ?>
- <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
- 		<title>410 Gone</title>
+		<title>410 Gone</title>
 	</head>
 	<body>
 		<h1>410 Gone</h1>
-		<p>b2evolution does no longer publish referer statistics publicly in order not to attract spam robots.</p>
-  </body>
+		<p><?php echo $app_name ?> does no longer publish referer statistics publicly in order not to attract spam robots.</p>
+	</body>
 </html>
 <?php
 	debug_info();
- 	exit;
+	debug_die();
 ?>
