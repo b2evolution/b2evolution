aec253a8e3cc043fd63920379a462d6749ec6291
Daniel Hahler <git+b2evo@thequod.de>
2005-11-25 00:08:43 +0000

    Use $online_session_timeout instead of $timeout_online_user (it was rolled back in /conf/_advanced.php)

--- blogs/evocore/_sessions.class.php
+++ blogs/evocore/_sessions.class.php
@@ -81,12 +81,12 @@ class Sessions extends Widget
 		{
 			return true;
 		}
-		global $DB, $UserCache, $localtimenow, $timeout_online_user;
+		global $DB, $UserCache, $localtimenow, $online_session_timeout;
 
 		$this->_count_guests = 0;
 		$this->_registered_Users = array();
 
-		$timeout_YMD = date( 'Y-m-d H:i:s', ($localtimenow - $timeout_online_user) );
+		$timeout_YMD = date( 'Y-m-d H:i:s', ($localtimenow - $online_session_timeout) );
 
 		// We get all sessions that have been seen in $timeout_YMD and that have a session key.
 		// NOTE: we do not use DISTINCT here, because guest users are all "NULL".
@@ -257,6 +257,9 @@ class Sessions extends Widget
 
 /*
  * $Log$
+ * Revision 1.20  2005/11/25 00:08:43  blueyed
+ * Use $online_session_timeout instead of $timeout_online_user (it was rolled back in /conf/_advanced.php)
+ *
  * Revision 1.19  2005/11/18 03:37:55  blueyed
  * doc
  *
