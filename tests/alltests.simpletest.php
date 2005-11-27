232c43ac5232ff36b2681e43c6121132e8da650c
Daniel Hahler <git+b2evo@thequod.de>
2005-11-27 20:37:17 +0000

    Do not show passes.

--- tests/alltests.simpletest.php
+++ tests/alltests.simpletest.php
@@ -16,6 +16,6 @@ $test = new EvoGroupTest( 'evo Tests Suite');
 
 $test->loadAllTests( dirname(__FILE__).'/blogs/' );
 
-#$test->run( new HtmlReporter(), new TextReporter() );
-$test->run( new HtmlReporterShowPasses(), new TextReporter() );
+$test->run( new HtmlReporter(), new TextReporter() );
+#$test->run( new HtmlReporterShowPasses(), new TextReporter() );
 ?>
