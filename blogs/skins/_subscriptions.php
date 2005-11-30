0d3f112e7668ab1a12128c12343cbf45ccc68583
Daniel Hahler <git+b2evo@thequod.de>
2005-11-30 10:46:06 +0000

    whitespace

--- blogs/skins/_subscriptions.php
+++ blogs/skins/_subscriptions.php
@@ -71,16 +71,18 @@ $Form->begin_form( 'bComment' );
 	$Form->begin_fieldset( T_('Blog subscriptions') );
 
 		$sql = 'SELECT blog_ID, blog_shortname, sub_items, sub_comments
-							FROM T_blogs LEFT JOIN T_subscriptions ON ( blog_ID = sub_coll_ID AND sub_user_ID = '.$current_User->ID.' )
-						 WHERE blog_in_bloglist <> 0';
+		          FROM T_blogs LEFT JOIN T_subscriptions ON ( blog_ID = sub_coll_ID AND sub_user_ID = '.$current_User->ID.' )
+		         WHERE blog_in_bloglist <> 0';
 		$blog_subs = $DB->get_results( $sql );
 
 		$subs_blog_IDs = array();
 		foreach( $blog_subs AS $blog_sub )
 		{
 			$subs_blog_IDs[] = $blog_sub->blog_ID;
-			$subscriptions = array( array( 'sub_items_'.$blog_sub->blog_ID,    '1', T_('Posts'),    $blog_sub->sub_items ),
-															array( 'sub_comments_'.$blog_sub->blog_ID, '1', T_('Comments'), $blog_sub->sub_comments ) );
+			$subscriptions = array(
+					array( 'sub_items_'.$blog_sub->blog_ID,    '1', T_('Posts'),    $blog_sub->sub_items ),
+					array( 'sub_comments_'.$blog_sub->blog_ID, '1', T_('Comments'), $blog_sub->sub_comments )
+				);
 			$Form->checklist( $subscriptions, 'subscriptions', format_to_output( $blog_sub->blog_shortname, 'htmlbody' ) );
 		}
 
@@ -89,5 +91,5 @@ $Form->begin_form( 'bComment' );
 	$Form->end_fieldset();
 
 $Form->end_form( array( array( '', '', T_('Update'), 'SaveButton' ),
-													array( 'reset', '', T_('Reset'), 'ResetButton' ) ) );
+                        array( 'reset', '', T_('Reset'), 'ResetButton' ) ) );
 ?>
