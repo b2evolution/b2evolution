f1a0fbf59a723e92403e89986524a719f40903f9
Daniel Hahler <git+b2evo@thequod.de>
2005-12-05 21:34:29 +0000

    doc

--- blogs/admin/_files_upload.inc.php
+++ blogs/admin/_files_upload.inc.php
@@ -161,7 +161,7 @@ if( false )
 		}
 		$LogUpload->display( '', '', true, 'all' ); ?>
 
-		<?php /* DIV to prevent the "Upload into" fieldset from wrapping below the "Files to upload" box; which browser?? */ ?>
+		<?php /* DIV to prevent the "Upload into" fieldset from wrapping below the "Files to upload" box (on any browser), because padding/margin of the fieldset does not affect the width of the both boxes */ ?>
 		<div class="box_files_to_upload">
 		<fieldset class="files_to_upload">
 			<legend><?php echo T_('Files to upload') ?></legend>
@@ -269,16 +269,11 @@ if( false )
 
 /*
  * $Log$
- * Revision 1.16  2005/12/05 15:29:14  fplanque
- * no message
+ * Revision 1.17  2005/12/05 21:34:29  blueyed
+ * doc
  *
  * Revision 1.15  2005/11/27 06:17:52  blueyed
  * Layout fixes to not cause the "Upload into" fieldset wrap below the "Files to upload" box.
- * This is the WRONG place for that kind of comment!  + which browser?
- *
- * Revision 1.14  2005/11/21 18:33:19  fplanque
- * Too many undiscussed changes all around: Massive rollback! :((
- * As said before, I am only taking CLEARLY labelled bugfixes.
  *
  * Revision 1.12  2005/10/28 20:08:46  blueyed
  * Normalized AdminUI
