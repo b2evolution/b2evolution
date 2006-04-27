a2c99008087fd3525820369e975d5c135e5fd0e0
Daniel Hahler <git+b2evo@thequod.de>
2006-04-27 21:03:51 +0000

    Cleanup, fix and add Plugin hook

--- blogs/htsrv/trackback.php
+++ blogs/htsrv/trackback.php
@@ -26,7 +26,7 @@
  * @package htsrv
  *
  * {@internal Below is a list of authors who have contributed to design/coding of this file: }}
- * @author blueyed: Daniel HAHLER.
+ * @author blueyed: Daniel HAHLER
  * @author fplanque: Francois PLANQUE
  *
  * @version $Id$
@@ -56,101 +56,132 @@ if( empty($tb_id) )
 	$tb_id = intval( $path_elements[count($path_elements)-1] );
 }
 
-if( (strlen(''.$tb_id)) && (empty($_GET['__mode'])) && (strlen(''.$url)) )
-{
-	@header('Content-Type: text/xml');
 
-	$comment_post_ID = $tb_id;
-	$commented_Item = & $ItemCache->get_by_ID( $comment_post_ID );
-	if( !( $Blog = & $commented_Item->get_Blog() ) )
-	{
-		trackback_response( 1, 'Sorry, could not get the post\'s weblog.' );
-	}
-
-	if( !$Blog->get('allowtrackbacks') )
-	{
-		trackback_response( 1, 'Sorry, this weblog does not allow you to trackback its posts.' );
-	}
-
-	if( ! $commented_Item->comment_status != 'open' )
-	{
-		trackback_response( 1, 'Sorry, this item does not accept trackbacks.' );
-	}
-
-	$title = strip_tags($title);
-	$title = (strlen($title) > 255) ? substr($title, 0, 252).'...' : $title;
-	$excerpt = strip_tags($excerpt);
-	$excerpt = (strlen($excerpt) > 255) ? substr($excerpt, 0, 252).'...' : $excerpt;
-	$blog_name = htmlspecialchars($blog_name);
-	$blog_name = (strlen($blog_name) > 255) ? substr($blog_name, 0, 252).'...' : $blog_name;
-
-	$comment = "<strong>$title</strong><br />$excerpt";
-
-	$original_comment = $comment;
-
-	$now = date('Y-m-d H:i:s', $localtimenow );
-
-	// CHECK and FORMAT content
-	if( $error = validate_url( $url, $comments_allowed_uri_scheme ) )
-	{
-		$Messages->add( T_('Supplied URL is invalid: ').$error, 'error' );
-	}
-
-	$comment = format_to_post($comment,1,1);
-	if( empty($comment) )
-	{ // comment should not be empty!
-		$Messages->add( T_('Please do not send empty comment'), 'error' );
-	}
-	elseif( antispam_check( strip_tags($comment) ) )
-	{
-		$Messages->add( T_('Supplied comment is invalid'), 'error' );
-	}
-
-	if( $errstring = $Messages->get_string( 'Cannot insert trackback, please correct these errors:', '' ) )
-	{
-		trackback_response(2, $errstring);	// TODO: check that error code 2 is ok
-		die();
-	}
-
-
-	/*
-	 * ----------------------------
-	 * Create and record trackback:
-	 * ----------------------------
-	 */
-	$Comment = & new Comment();
-	$Comment->set( 'type', 'trackback' );
-	$Comment->set_Item( $commented_Item );
-	$Comment->set( 'author', $blog_name );
-	$Comment->set( 'author_url', $url );
-	$Comment->set( 'author_IP', $Hit->IP );
-	$Comment->set( 'date', $now );
-	$Comment->set( 'content', $comment );
-	// Assign default status for new comments:
-	$Comment->set( 'status', $commented_Item->Blog->get_setting('new_feedback_status') );
-
-	if( ! $Comment->dbinsert() )
-	{
-		trackback_response(2, "There is an error with the database, it can't store your comment...<br />Contact the <a href=\"mailto:$admin_email\">webmaster</a>");	// TODO: check that error code 2 is ok
-		die ();
-	}
-
-
-	/*
-	 * ----------------------------
-	 * New trackback notification:
-	 * ----------------------------
-	 */
-	$Comment->send_email_notifications();
-
-
-	trackback_response( 0, 'ok' );
-
-
+if( ! empty($_GET['__mode']) )
+{ // some MT extension (AFAIK), that we do not support
+	return;
 }
+
+if( empty($tb_id) )
+{
+	trackback_response( 1, 'No trackback post ID given.' ); // exits
+}
+if( empty($url) )
+{
+	trackback_response( 1, 'No url to your permanent entry given.' ); // exits
+}
+
+
+@header('Content-Type: text/xml');
+
+$comment_post_ID = $tb_id;
+$commented_Item = & $ItemCache->get_by_ID( $comment_post_ID );
+if( !( $Blog = & $commented_Item->get_Blog() ) )
+{
+	trackback_response( 1, 'Sorry, could not get the post\'s weblog.' ); // exits
+}
+
+if( ! $Blog->get('allowtrackbacks') )
+{
+	trackback_response( 1, 'Sorry, this weblog does not allow you to trackback its posts.' ); // exits
+}
+
+if( $commented_Item->comment_status != 'open' )
+{
+	trackback_response( 1, 'Sorry, this item does not accept trackbacks.' ); // exits
+}
+
+
+// Trigger Plugin event, which may add a message of category "error":
+$Plugins->trigger_event( 'TrackbackReceived', array(
+	'url' => & $url, 'title' => & $title, 'excerpt' => & $excerpt, 'blog_name' => & $blog_name, 'Item' => $commented_Item) );
+
+
+// CHECK content
+if( $error = validate_url( $url, $comments_allowed_uri_scheme ) )
+{
+	$Messages->add( T_('Supplied URL is invalid: ').$error, 'error' );
+}
+
+if( $Messages->count('error') )
+{
+	trackback_response( 1, $Messages->get_string( '', '', 'all', "\n" ) ); // exits
+}
+
+
+$title = strip_tags($title);
+$title = (strlen($title) > 255) ? substr($title, 0, 252).'...' : $title;
+$excerpt = strip_tags($excerpt);
+$excerpt = (strlen($excerpt) > 255) ? substr($excerpt, 0, 252).'...' : $excerpt;
+$blog_name = htmlspecialchars($blog_name);
+$blog_name = (strlen($blog_name) > 255) ? substr($blog_name, 0, 252).'...' : $blog_name;
+
+$comment = '';
+if( ! empty($title) )
+{
+	$comment .= '<strong>'.$title.'</strong>';
+
+	if( ! empty($excerpt) )
+	{
+		$comment .= '<br />';
+	}
+}
+$comment .= $excerpt;
+
+$comment = format_to_post($comment,1,1);
+if( empty($comment) )
+{ // comment should not be empty!
+	$Messages->add( T_('Please do not send empty comment'), 'error' );
+}
+elseif( antispam_check( strip_tags($comment) ) )
+{
+	$Messages->add( T_('Supplied comment is invalid'), 'error' );
+}
+
+if( $errstring = $Messages->get_string( 'Cannot insert trackback, please correct these errors:', '' ) )
+{
+	trackback_response(2, $errstring);	// TODO: check that error code 2 is ok; blueyed> Why should we use 2?
+}
+
+
+/*
+ * ----------------------------
+ * Create and record trackback:
+ * ----------------------------
+ */
+$Comment = & new Comment();
+$Comment->set( 'type', 'trackback' );
+$Comment->set_Item( $commented_Item );
+$Comment->set( 'author', $blog_name );
+$Comment->set( 'author_url', $url );
+$Comment->set( 'author_IP', $Hit->IP );
+$Comment->set( 'date', date('Y-m-d H:i:s', $localtimenow ) );
+$Comment->set( 'content', $comment );
+// Assign default status for new comments:
+$Comment->set( 'status', $commented_Item->Blog->get_setting('new_feedback_status') );
+
+if( ! $Comment->dbinsert() )
+{
+	trackback_response(2, "There is an error with the database, it can't store your comment...<br />Contact the <a href=\"mailto:$admin_email\">webmaster</a>");	// TODO: check that error code 2 is ok; blueyed> why should we use "2"?
+}
+
+
+/*
+ * ----------------------------
+ * New trackback notification:
+ * ----------------------------
+ */
+$Comment->send_email_notifications();
+
+
+trackback_response( 0, 'ok' );
+
 
 /*
  * $Log$
+ * Revision 1.44  2006/04/27 21:03:51  blueyed
+ * Cleanup, fix and add Plugin hook
+ *
  * Revision 1.43  2006/04/20 16:31:29  fplanque
  * comment moderation (finished for 1.8)
  *
