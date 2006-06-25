4b43a8983b632f7a1766029c29e0c485222d9060
Francois Planque <github@fplanque.net>
2006-06-25 17:41:55 +0000

    rollback

--- blogs/inc/_connect_db.inc.php
+++ blogs/inc/_connect_db.inc.php
@@ -1,23 +1,23 @@
 <?php
 /**
- * JEM Stripped extra line feeds and trimmed the text after the closing php tag
- * because it throws an error about sending text after the header was sent.
-*/ 
-/**
  * This files instantiates the global {@link $DB} object and connects to the database.
  */
+
 /**
  * Load basic settings
  */
 require_once dirname(__FILE__).'/../conf/_basic_config.php';
+
 /**
  * Load DB class
  */
 require_once dirname(__FILE__).'/_misc/_db.class.php';
+
 /**
  * Database connection (connection opened here)
  *
  * @global DB $DB
  */
 $DB = & new DB( $db_config );
+
 ?>
