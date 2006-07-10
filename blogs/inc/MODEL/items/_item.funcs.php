63301bc572a396350bd0c05a0b81086af3025eff
Daniel Hahler <git+b2evo@thequod.de>
2006-07-10 18:15:21 +0000

    Fix for default main cat, when switching blogs.

--- blogs/inc/MODEL/items/_item.funcs.php
+++ blogs/inc/MODEL/items/_item.funcs.php
@@ -767,11 +767,15 @@ function cat_select( $display_info = true, $form_fields = true )
 
 	$cat_select_form_fields = $form_fields;
 
+	cat_query( 'none' ); // make sure the caches are loaded
+
 	// Needed for tab switching in b2edit.php:
 	$default_main_cat = $Request->param( 'post_category', 'integer', $edited_Item->main_cat_ID );
+	if( $default_main_cat && $allow_cross_posting < 3 && get_catblog($default_main_cat) != $blog )
+	{ // the main cat is not in the list of categories; this happens, if the user switches blogs during editing: setting it to 0 uses the first cat in the list
+		$default_main_cat = 0;
+	}
 	$post_extracats = $Request->param( 'post_extracats', 'array', $post_extracats );
-
-	cat_query( 'none' ); // make sure the caches are loaded
 
 	if( $allow_cross_posting >= 2 )
 	{ // If BLOG cross posting enabled, go through all blogs with cats:
@@ -936,6 +940,9 @@ function cat_select_after_last( $parent_cat_ID, $level )
 
 /*
  * $Log$
+ * Revision 1.11  2006/07/10 18:15:21  blueyed
+ * Fix for default main cat, when switching blogs.
+ *
  * Revision 1.10  2006/07/08 22:33:43  blueyed
  * Integrated "simple edit form".
  *
