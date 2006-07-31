fdca824e24b2042524d6df3dfd7719504bb3785b
Yabba <waffler@innervisions.org.uk>
2006-07-31 07:52:03 +0000

    Moved search and replace arrays to Settings Added new Setting to enable rendering of comments Added 2 new Settings for comment search and replace arrays

--- blogs/plugins/_bbcode.plugin.php
+++ blogs/plugins/_bbcode.plugin.php
@@ -26,66 +26,13 @@ class bbcode_plugin extends Plugin
 	var $short_desc;
 	var $long_desc;
 
-
-	/**
-	 * BBcode formatting search array
-	 *
-	 * @access private
+	/*
+	 * Internal
 	 */
-	var $search = array(
-			'#\[b](.+?)\[/b]#is',		// Formatting tags
-			'#\[i](.+?)\[/i]#is',
-			'#\[u](.+?)\[/u]#is',
-			'#\[s](.+?)\[/s]#is',
-			'!\[color=(#?[A-Za-z0-9]+?)](.+?)\[/color]!is',
-			'#\[size=([0-9]+?)](.+?)\[/size]#is',
-			'#\[font=([A-Za-z0-9 ;\-]+?)](.+?)\[/font]#is',
-			// Following lines added by Georges (iznogoudmc)
-			'#\[code](.+?)\[/code]#is',
-			'#\[quote](.+?)\[/quote]#is',
-			'#\[list=1](.+?)\[/list]#is',
-			'#\[list=a](.+?)\[/list]#is',
-			'#\[list](.+?)\[/list]#is',
-			'#\[\*](.+?)\n#is',
-			// End of Georges' add
-			// (Remove comment if modification validated)
-			// The following are dangerous, until we security check resulting code.
-			//	'#\[img](.+?)\[/img]#is',		// Image
-			//	'#\[url](.+?)\[/url]#is',		// URL
-			//	'#\[url=(.+?)](.+?)\[/url]#is',
-			//	'#\[email](.+?)\[/email]#eis',		// E-mail
-			//	'#\[email=(.+?)](.+?)\[/email]#eis'
-		);
-
-	/**
-	 * HTML replace array
-	 *
-	 * @access private
-	 */
-	var $replace = array(
-			'<strong>$1</strong>',		// Formatting tags
-			'<em>$1</em>',
-			'<span style="text-decoration:underline">$1</span>',
-			'<span style="text-decoration:line-through">$1</span>',
-			'<span style="color:$1">$2</span>',
-			'<span style="font-size:$1px">$2</span>',
-			'<span style="font-family:$1">$2</span>',
-			// Following lines added by Georges (iznogoudmc)
-			'<pre>$1</pre>',
-			'&laquo;&nbsp;$1&nbsp;&raquo;',
-			'<ol type="1">$1</ol>',
-			'<ol type="a">$1</ol>',
-			'<ul>$1</ul>',
-			'<li>$1</li>',
-			// End of Georges' add
-			// (Remove comment if modification validated)
-			//	'<img src="$1" alt="" />',		// Image
-			//	'<a href="$1">$1</a>',		// URL
-			//	'<a href="$1" title="$2">$2</a>',
-			//	'<a href=\"mailto:$1\">$1</a>',		// E-mail
-			//	'<a href="mailto:$1">$2</a>'
-		);
-
+	var $post_search_list;
+	var $post_replace_list;
+	var $comment_search_list;
+	var $comment_replace_list;
 
 	/**
 	 * Init
@@ -96,6 +43,122 @@ class bbcode_plugin extends Plugin
 		$this->long_desc = T_('Available tags are: [b] [i] [u] [s] [color=...] [size=...] [font=...] [code] [quote] [list=1] [list=a] [list] [*]');
 	}
 
+	/**
+	 * Get the default settings of the plugin.
+	 *
+	 * @return array
+	 */
+	function GetDefaultSettings()
+	{
+		return array(
+			'post_settings_begin' => array(
+				'layout' => 'begin_fieldset',
+				'label' => $this->T_( 'Settings for posts' ),
+			),
+			'post_search_list' => array(
+				'label' => $this->T_( 'Search list'),
+				'note' => $this->T_( 'This is the BBcode search array for posts (one per line) ONLY CHANGE THESE IF YOU KNOW WHAT YOU\'RE DOING' ),
+				'type' => 'textarea',
+				'rows' => 10,
+				'allow_html' => 1,
+				'defaultvalue' => '#\[b](.+?)\[/b]#is
+#\[i](.+?)\[/i]#is
+#\[u](.+?)\[/u]#is
+#\[s](.+?)\[/s]#is
+!\[color=(#?[A-Za-z0-9]+?)](.+?)\[/color]!is
+#\[size=([0-9]+?)](.+?)\[/size]#is
+#\[font=([A-Za-z0-9 ;\-]+?)](.+?)\[/font]#is
+#\[code](.+?)\[/code]#is
+#\[quote](.+?)\[/quote]#is
+#\[list=1](.+?)\[/list]#is
+#\[list=a](.+?)\[/list]#is
+#\[list](.+?)\[/list]#is
+#\[\*](.+?)\n#is
+!\[bg=(#?[A-Za-z0-9]+?)](.+?)\[/bg]!is',
+			),
+			'post_replace_list' => array(
+				'label' => $this->T_( 'Replace list'),
+				'note' => $this->T_( 'This is the replace array for posts (one per line) it must match the exact order of the search array' ),
+				'type' => 'textarea',
+				'rows' => 10,
+				'allow_html' => 1,
+				'defaultvalue' => '<strong>$1</strong>
+<em>$1</em>
+<span style="text-decoration:underline">$1</span>
+<span style="text-decoration:line-through">$1</span>
+<span style="color:$1">$2</span>
+<span style="font-size:$1px">$2</span>
+<span style="font-family:$1">$2</span>
+<pre>$1</pre>
+&laquo;&nbsp;$1&nbsp;&raquo;
+<ol type="1">$1</ol>
+<ol type="a">$1</ol>
+<ul>$1</ul>
+<li>$1</li>
+<span style="background-color:$1">$2</span>',
+			),
+			'post_settings_end' => array(
+				'layout' => 'end_fieldset',
+			),
+			'comment_settings_begin' => array(
+				'type' => 'layout',
+				'layout' => 'begin_fieldset',
+				'label' => $this->T_( 'Settings for comments' ),
+			),
+			'render_comments' => array(
+				'label' => $this->T_('Render comments' ),
+				'note' => $this->T_('If enabled the BBcode in comments will be rendered'),
+				'defaultvalue' => 0,
+				'type' => 'checkbox',
+			),
+			'comment_search_list' => array(
+				'label' => $this->T_( 'Search list'),
+				'note' => $this->T_( 'This is the BBcode search array for COMMENTS (one per line) ONLY CHANGE THESE IF YOU KNOW WHAT YOU\'RE DOING' ),
+				'type' => 'textarea',
+				'rows' => 10,
+				'allow_html' => 1,
+				'defaultvalue' => '#\[b](.+?)\[/b]#is
+#\[i](.+?)\[/i]#is
+#\[u](.+?)\[/u]#is
+#\[s](.+?)\[/s]#is
+!\[color=(#?[A-Za-z0-9]+?)](.+?)\[/color]!is
+#\[size=([0-9]+?)](.+?)\[/size]#is
+#\[font=([A-Za-z0-9 ;\-]+?)](.+?)\[/font]#is
+#\[code](.+?)\[/code]#is
+#\[quote](.+?)\[/quote]#is
+#\[list=1](.+?)\[/list]#is
+#\[list=a](.+?)\[/list]#is
+#\[list](.+?)\[/list]#is
+#\[\*](.+?)\n#is
+!\[bg=(#?[A-Za-z0-9]+?)](.+?)\[/bg]!is',
+			),
+			'comment_replace_list' => array(
+				'label' => $this->T_( 'Replace list'),
+				'note' => $this->T_( 'This is the replace array for COMMENTS (one per line) it must match the exact order of the search array' ),
+				'type' => 'textarea',
+				'rows' => 10,
+				'allow_html' => 1,
+				'defaultvalue' => '<strong>$1</strong>
+<em>$1</em>
+<span style="text-decoration:underline">$1</span>
+<span style="text-decoration:line-through">$1</span>
+<span style="color:$1">$2</span>
+<span style="font-size:$1px">$2</span>
+<span style="font-family:$1">$2</span>
+<pre>$1</pre>
+&laquo;&nbsp;$1&nbsp;&raquo;
+<ol type="1">$1</ol>
+<ol type="a">$1</ol>
+<ul>$1</ul>
+<li>$1</li>
+<span style="background-color:$1">$2</span>',
+			),
+			'comment_settings_end' => array(
+				'type' => 'layout',
+				'layout' => 'end_fieldset',
+			),
+		);
+	}
 
 	/**
 	 * Perform rendering
@@ -105,8 +168,13 @@ class bbcode_plugin extends Plugin
 	function RenderItemAsHtml( & $params )
 	{
 		$content = & $params['data'];
+		if( !isset( $this->post_search_list ) )
+			$this->post_search_list = explode( "\n", str_replace( "\r", '', $this->Settings->get( 'post_search_list' ) ) );
 
-		$content = preg_replace( $this->search, $this->replace, $content );
+		if( !isset( $this->post_replace_list ) )
+			$this->post_replace_list = explode( "\n", str_replace( "\r", '', $this->Settings->get( 'post_replace_list' ) ) );
+
+		$content = preg_replace( $this->post_search_list, $this->post_replace_list, $content );
 
 		return true;
 	}
@@ -122,11 +190,36 @@ class bbcode_plugin extends Plugin
 		$this->RenderItemAsHtml( $params );
 	}
 
+	/**
+	 *
+	 * Render comments if required
+	 *
+	 * @see Plugin::FilterCommentContent()
+	 */
+	function FilterCommentContent( & $params )
+	{
+		if( $this->Settings->get( 'render_comments' ) )
+		{
+		$content = & $params['data'];
+		if( !isset( $this->comment_search_list ) )
+			$this->comment_search_list = explode( "\n", str_replace( "\r", '', $this->Settings->get( 'comment_search_list' ) ) );
+
+		if( !isset( $this->comment_replace_list ) )
+			$this->comment_replace_list = explode( "\n", str_replace( "\r", '', $this->Settings->get( 'comment_replace_list' ) ) );
+
+		$content = preg_replace( $this->comment_search_list, $this->comment_replace_list, $content );
+		}
+	}
 }
 
 
 /*
  * $Log$
+ * Revision 1.16  2006/07/31 07:52:03  yabs
+ * Moved search and replace arrays to Settings
+ * Added new Setting to enable rendering of comments
+ * Added 2 new Settings for comment search and replace arrays
+ *
  * Revision 1.15  2006/07/10 20:19:30  blueyed
  * Fixed PluginInit behaviour. It now gets called on both installed and non-installed Plugins, but with the "is_installed" param appropriately set.
  *
