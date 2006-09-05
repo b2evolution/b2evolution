4ef99f030a40dd7a3aebaac44b25e67cf063085e
Daniel Hahler <git+b2evo@thequod.de>
2006-09-05 18:41:04 +0000

    doc/typo

--- blogs/install/phpinfo.php
+++ blogs/install/phpinfo.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This is simply displayind standard phpinfo
+ * This is simply displaying standard phpinfo
  *
  * b2evolution - {@link http://b2evolution.net/}
  * Released under GNU GPL License - {@link http://b2evolution.net/about/license.html}
