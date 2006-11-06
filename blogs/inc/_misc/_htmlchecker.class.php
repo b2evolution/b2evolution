03254c9cad3feae775c80722100a6ea59668f160
Daniel Hahler <git+b2evo@thequod.de>
2006-11-06 22:56:53 +0000

    Added full(?) XHTML entities support to the html checker

--- blogs/inc/_misc/_htmlchecker.class.php
+++ blogs/inc/_misc/_htmlchecker.class.php
@@ -160,108 +160,18 @@ class SafeHtmlChecker
 
 		$xhtml_head .= '?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"';
 
-		// Include latin1 entities (http://www.w3.org/xhtml1/DTD/xhtml-lat1.ent)
-		$xhtml_head .= '[
-			<!ENTITY nbsp   "&#160;">
-			<!ENTITY iexcl  "&#161;">
-			<!ENTITY cent   "&#162;">
-			<!ENTITY pound  "&#163;">
-			<!ENTITY curren "&#164;">
-			<!ENTITY yen    "&#165;">
-			<!ENTITY brvbar "&#166;">
-			<!ENTITY sect   "&#167;">
-			<!ENTITY uml    "&#168;">
-			<!ENTITY copy   "&#169;">
-			<!ENTITY ordf   "&#170;">
-			<!ENTITY laquo  "&#171;">
-			<!ENTITY not    "&#172;">
-			<!ENTITY shy    "&#173;">
-			<!ENTITY reg    "&#174;">
-			<!ENTITY macr   "&#175;">
-			<!ENTITY deg    "&#176;">
-			<!ENTITY plusmn "&#177;">
-			<!ENTITY sup2   "&#178;">
-			<!ENTITY sup3   "&#179;">
-			<!ENTITY acute  "&#180;">
-			<!ENTITY micro  "&#181;">
-			<!ENTITY para   "&#182;">
-			<!ENTITY middot "&#183;">
-			<!ENTITY cedil  "&#184;">
-			<!ENTITY sup1   "&#185;">
-			<!ENTITY ordm   "&#186;">
-			<!ENTITY raquo  "&#187;">
-			<!ENTITY frac14 "&#188;">
-			<!ENTITY frac12 "&#189;">
-			<!ENTITY frac34 "&#190;">
-			<!ENTITY iquest "&#191;">
-			<!ENTITY Agrave "&#192;">
-			<!ENTITY Aacute "&#193;">
-			<!ENTITY Acirc  "&#194;">
-			<!ENTITY Atilde "&#195;">
-			<!ENTITY Auml   "&#196;">
-			<!ENTITY Aring  "&#197;">
-			<!ENTITY AElig  "&#198;">
-			<!ENTITY Ccedil "&#199;">
-			<!ENTITY Egrave "&#200;">
-			<!ENTITY Eacute "&#201;">
-			<!ENTITY Ecirc  "&#202;">
-			<!ENTITY Euml   "&#203;">
-			<!ENTITY Igrave "&#204;">
-			<!ENTITY Iacute "&#205;">
-			<!ENTITY Icirc  "&#206;">
-			<!ENTITY Iuml   "&#207;">
-			<!ENTITY ETH    "&#208;">
-			<!ENTITY Ntilde "&#209;">
-			<!ENTITY Ograve "&#210;">
-			<!ENTITY Oacute "&#211;">
-			<!ENTITY Ocirc  "&#212;">
-			<!ENTITY Otilde "&#213;">
-			<!ENTITY Ouml   "&#214;">
-			<!ENTITY times  "&#215;">
-			<!ENTITY Oslash "&#216;">
-			<!ENTITY Ugrave "&#217;">
-			<!ENTITY Uacute "&#218;">
-			<!ENTITY Ucirc  "&#219;">
-			<!ENTITY Uuml   "&#220;">
-			<!ENTITY Yacute "&#221;">
-			<!ENTITY THORN  "&#222;">
-			<!ENTITY szlig  "&#223;">
-			<!ENTITY agrave "&#224;">
-			<!ENTITY aacute "&#225;">
-			<!ENTITY acirc  "&#226;">
-			<!ENTITY atilde "&#227;">
-			<!ENTITY auml   "&#228;">
-			<!ENTITY aring  "&#229;">
-			<!ENTITY aelig  "&#230;">
-			<!ENTITY ccedil "&#231;">
-			<!ENTITY egrave "&#232;">
-			<!ENTITY eacute "&#233;">
-			<!ENTITY ecirc  "&#234;">
-			<!ENTITY euml   "&#235;">
-			<!ENTITY igrave "&#236;">
-			<!ENTITY iacute "&#237;">
-			<!ENTITY icirc  "&#238;">
-			<!ENTITY iuml   "&#239;">
-			<!ENTITY eth    "&#240;">
-			<!ENTITY ntilde "&#241;">
-			<!ENTITY ograve "&#242;">
-			<!ENTITY oacute "&#243;">
-			<!ENTITY ocirc  "&#244;">
-			<!ENTITY otilde "&#245;">
-			<!ENTITY ouml   "&#246;">
-			<!ENTITY divide "&#247;">
-			<!ENTITY oslash "&#248;">
-			<!ENTITY ugrave "&#249;">
-			<!ENTITY uacute "&#250;">
-			<!ENTITY ucirc  "&#251;">
-			<!ENTITY uuml   "&#252;">
-			<!ENTITY yacute "&#253;">
-			<!ENTITY thorn  "&#254;">
-			<!ENTITY yuml   "&#255;">
-			]';
-		$xhtml_head .= '>';
+		// Include entities:
+		$xhtml_head .= '[';
+		// Include latin1 entities (http://www.w3.org/TR/xhtml1/DTD/xhtml-lat1.ent):
+		$xhtml_head .= file_get_contents( dirname(__FILE__).'/_htmlchecker.xhtml-lat1.ent' );
+		// Include symbol entities (http://www.w3.org/TR/xhtml1/DTD/xhtml-symbol.ent):
+		$xhtml_head .= file_get_contents( dirname(__FILE__).'/_htmlchecker.xhtml-symbol.ent' );
+		// Include special entities (http://www.w3.org/TR/xhtml1/DTD/xhtml-special.ent):
+		$xhtml_head .= file_get_contents( dirname(__FILE__).'/_htmlchecker.xhtml-special.ent' );
+		$xhtml_head .= ']>';
 
 		$xhtml = $xhtml_head.'<body>'.$xhtml.'</body>';
+		unset($xhtml_head);
 
 		if( !xml_parse($this->parser, $xhtml) )
 		{
@@ -390,6 +300,9 @@ class SafeHtmlChecker
 
 /*
  * $Log$
+ * Revision 1.10  2006/11/06 22:56:53  blueyed
+ * Added full(?) XHTML entities support to the html checker
+ *
  * Revision 1.9  2006/11/04 21:44:59  blueyed
  * Include latin1 entities to let xml_parse() not choke on those
  *
