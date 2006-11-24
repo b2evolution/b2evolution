6c4b3777eeff4a466a2d322f4880305e2f2d53f6
Daniel Hahler <git+b2evo@thequod.de>
2006-11-24 18:47:32 +0000

    Fixed link to b2evo CVS browsing interface in file docblocks

--- blogs/rsc/js/form_extensions.js
+++ blogs/rsc/js/form_extensions.js
@@ -8,7 +8,7 @@
  * {@internal License choice
  * - If you have received this file as part of a package, please find the license.txt file in
  *   the same folder or the closest folder above for complete license terms.
- * - If you have received this file individually (e-g: from http://cvs.sourceforge.net/viewcvs.py/evocms/)
+ * - If you have received this file individually (e-g: from http://evocms.cvs.sourceforge.net/)
  *   then you must choose one of the following licenses before using the file:
  *   - GNU General Public License 2 (GPL) - http://www.opensource.org/licenses/gpl-license.php
  *   - Mozilla Public License 1.1 (MPL) - http://www.opensource.org/licenses/mozilla1.1.php
