47492ffd5c750519259614d15dc729ea690c2b57
Daniel Hahler <git+b2evo@thequod.de>
2006-11-30 23:55:23 +0000

    Removed files from ok-list which are probably not ok

--- tests/release/security_checks.simpletest.php
+++ tests/release/security_checks.simpletest.php
@@ -21,10 +21,6 @@ class SecurityChecksTestCase extends EvoUnitTestCase
 	var $entry_points = array(
 		'inc/_main.inc.php',
 		'inc/_blog_main.inc.php',
-		'inc/_connect_db.inc.php',
-		'inc/MODEL/settings/_locale.funcs.php',
-		'inc/_misc/ext/_xmlrpc.php',
-		'inc/_misc/ext/_xmlrpcs.php',
 		'conf/_formatting.php',
 		'conf/_config_TEST.php',
 		'conf/_upgrade.php',
@@ -39,29 +35,29 @@ class SecurityChecksTestCase extends EvoUnitTestCase
 		'conf/_stats.php',
 		'cron/cron_exec.php',
 		'cron/mms.php',
-  	'cron/getmail.php',
-  	'htsrv/comment_post.php',
-  	'htsrv/trackback.php',
-  	'htsrv/call_plugin.php',
-  	'htsrv/async.php',
-  	'htsrv/login.php',
-  	'htsrv/viewfile.php',
-  	'htsrv/subs_update.php',
-  	'htsrv/message_send.php',
-  	'htsrv/profile_update.php',
-  	'htsrv/getfile.php',
-  	'htsrv/register.php',
-  	'xmlsrv/atom.php',
-  	'xmlsrv/rdf.php',
-  	'xmlsrv/rss.comments.php',
-  	'xmlsrv/rss2.php',
-  	'xmlsrv/xmlrpc.php',
-  	'xmlsrv/rss.php',
-  	'xmlsrv/rss2.comments.php',
-  	'xmlsrv/rdf.comments.php',
-  	'xmlsrv/atom.comments.php',
-  	'install/phpinfo.php',
-  	'install/index.php',
+		'cron/getmail.php',
+		'htsrv/comment_post.php',
+		'htsrv/trackback.php',
+		'htsrv/call_plugin.php',
+		'htsrv/async.php',
+		'htsrv/login.php',
+		'htsrv/viewfile.php',
+		'htsrv/subs_update.php',
+		'htsrv/message_send.php',
+		'htsrv/profile_update.php',
+		'htsrv/getfile.php',
+		'htsrv/register.php',
+		'xmlsrv/atom.php',
+		'xmlsrv/rdf.php',
+		'xmlsrv/rss.comments.php',
+		'xmlsrv/rss2.php',
+		'xmlsrv/xmlrpc.php',
+		'xmlsrv/rss.php',
+		'xmlsrv/rss2.comments.php',
+		'xmlsrv/rdf.comments.php',
+		'xmlsrv/atom.comments.php',
+		'install/phpinfo.php',
+		'install/index.php',
 	);
 
 	function SecurityChecksTestCase()
@@ -140,7 +136,7 @@ class SecurityChecksTestCase extends EvoUnitTestCase
 		if( ! empty($badfiles) )
 		{
 			echo '<h1>Files which seem to miss the check for defined(EVO_MAIN_INIT)</h1>';
-			var_export( $badfiles );
+			pre_dump( $badfiles );
 		}
 		$this->assertFalse( $badfiles );
 	}
