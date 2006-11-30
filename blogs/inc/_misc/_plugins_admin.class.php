90a4af3f9e04c4827610584972db8441a2308082
Daniel Hahler <git+b2evo@thequod.de>
2006-11-30 05:57:54 +0000

    Moved Plugins::install() and sort() galore to Plugins_admin

--- blogs/inc/_misc/_plugins_admin.class.php
+++ blogs/inc/_misc/_plugins_admin.class.php
@@ -91,11 +91,251 @@ class Plugins_admin extends Plugins
 	}
 
 
+	/**
+	 * Install a plugin into DB.
+	 *
+	 * @todo Move to Plugins_admin
+	 * @param string Classname of the plugin to install
+	 * @param string Initial DB Status of the plugin ("enabled", "disabled", "needs_config", "broken")
+	 * @param string|NULL Optional classfile path, if not default (used for tests).
+	 * @return string|Plugin The installed Plugin (perhaps with $install_dep_notes set) or a string in case of error.
+	 */
+	function & install( $classname, $plug_status = 'enabled', $classfile_path = NULL )
+	{
+		global $DB, $Debuglog;
+
+		$this->load_plugins_table();
+
+		// Register the plugin:
+		$Plugin = & $this->register( $classname, 0, -1, NULL, $classfile_path ); // Auto-generates negative ID; New ID will be set a few lines below
+
+		if( is_string($Plugin) )
+		{ // return error message from register()
+			return $Plugin;
+		}
+
+		if( isset($Plugin->number_of_installs)
+		    && ( $this->count_regs( $Plugin->classname ) >= $Plugin->number_of_installs ) )
+		{
+			$this->unregister( $Plugin );
+			$r = T_('The plugin cannot be installed again.');
+			return $r;
+		}
+
+		$install_return = $Plugin->BeforeInstall();
+		if( $install_return !== true )
+		{
+			$this->unregister( $Plugin );
+			$r = T_('The installation of the plugin failed.');
+			if( is_string($install_return) )
+			{
+				$r .= '<br />'.$install_return;
+			}
+			return $r;
+		}
+
+		// Dependencies:
+		if( $this->is_admin_class )
+		{ // We must check dependencies against installed Plugins ($Plugins)
+			global $Plugins;
+			$dep_msgs = $Plugins->validate_dependencies( $Plugin, 'enable' );
+		}
+		else
+		{
+			$dep_msgs = $this->validate_dependencies( $Plugin, 'enable' );
+		}
+		if( ! empty( $dep_msgs['error'] ) )
+		{ // required dependencies
+			$this->unregister( $Plugin );
+			$r = T_('Some plugin dependencies are not fulfilled:').' <ul><li>'.implode( '</li><li>', $dep_msgs['error'] ).'</li></ul>';
+			return $r;
+		}
+
+		// All OK, install:
+		if( empty($Plugin->code) )
+		{
+			$Plugin->code = NULL;
+		}
+
+		$Plugin->status = $plug_status;
+
+		// Record into DB
+		$DB->begin();
+
+		$DB->query( '
+				INSERT INTO T_plugins( plug_classname, plug_priority, plug_code, plug_apply_rendering, plug_version, plug_status )
+				VALUES( "'.$classname.'", '.$Plugin->priority.', '.$DB->quote($Plugin->code).', '.$DB->quote($Plugin->apply_rendering).', '.$DB->quote($Plugin->version).', '.$DB->quote($Plugin->status).' ) ' );
+
+		// Unset auto-generated ID info
+		unset( $this->index_ID_Plugins[ $Plugin->ID ] );
+		$key = array_search( $Plugin->ID, $this->sorted_IDs );
+
+		// New ID:
+		$Plugin->ID = $DB->insert_id;
+		$this->index_ID_Plugins[ $Plugin->ID ] = & $Plugin;
+		$this->index_ID_rows[ $Plugin->ID ] = array(
+				'plug_ID' => $Plugin->ID,
+				'plug_priority' => $Plugin->priority,
+				'plug_classname' => $Plugin->classname,
+				'plug_code' => $Plugin->code,
+				'plug_apply_rendering' => $Plugin->apply_rendering,
+				'plug_status' => $Plugin->status,
+				'plug_version' => $Plugin->version,
+			);
+		$this->sorted_IDs[$key] = $Plugin->ID;
+
+		$this->save_events( $Plugin );
+
+		$DB->commit();
+
+		$Debuglog->add( 'Installed plugin: '.$Plugin->name.' ID: '.$Plugin->ID, 'plugins' );
+
+		if( ! empty($dep_msgs['note']) )
+		{ // Add dependency notes
+			$Plugin->install_dep_notes = $dep_msgs['note'];
+		}
+
+		// Do the stuff that we've skipped in register method at the beginning:
+
+		$this->init_settings( $Plugin );
+
+		$tmp_params = array('db_row' => $this->index_ID_rows[$Plugin->ID], 'is_installed' => false);
+
+		if( $Plugin->PluginInit( $tmp_params ) === false && ! $this->is_admin_class )
+		{
+			$Debuglog->add( 'Unregistered plugin, because PluginInit returned false.', 'plugins' );
+			$this->unregister( $Plugin );
+			$Plugin = '';
+		}
+
+		if( ! defined('EVO_IS_INSTALLING') || ! EVO_IS_INSTALLING )
+		{ // do not sort, if we're installing/upgrading.. instantiating Plugins might cause a fatal error!
+			$this->sort();
+		}
+
+		return $Plugin;
+	}
+
+
+	/**
+	 * Sort the list of plugins.
+	 *
+	 * WARNING: do NOT sort by anything else than priority unless you're handling a list of NOT-YET-INSTALLED plugins!
+	 *
+	 * @todo Move to Plugins_admin
+	 * @param string Order: 'priority' (default), 'name'
+	 */
+	function sort( $order = 'priority' )
+	{
+		$this->load_plugins_table();
+
+		foreach( $this->sorted_IDs as $k => $plugin_ID )
+		{ // Instantiate every plugin, so invalid ones do not get unregistered during sorting (crashes PHP, because $sorted_IDs gets changed etc)
+			if( ! $this->get_by_ID( $plugin_ID ) )
+			{
+				unset($this->sorted_IDs[$k]);
+			}
+		}
+
+		switch( $order )
+		{
+			case 'name':
+				usort( $this->sorted_IDs, array( & $this, 'sort_Plugin_name') );
+				break;
+
+			case 'group':
+				usort( $this->sorted_IDs, array( & $this, 'sort_Plugin_group') );
+				break;
+
+			default:
+				// Sort array by priority:
+				usort( $this->sorted_IDs, array( & $this, 'sort_Plugin_priority') );
+		}
+
+		$this->current_idx = 0;
+	}
+
+	/**
+	 * Callback function to sort plugins by priority (and classname, if they have same priority).
+	 * @todo Move to Plugins_admin
+	 */
+	function sort_Plugin_priority( & $a_ID, & $b_ID )
+	{
+		$a_Plugin = & $this->get_by_ID( $a_ID );
+		$b_Plugin = & $this->get_by_ID( $b_ID );
+
+		$r = $a_Plugin->priority - $b_Plugin->priority;
+
+		if( $r == 0 )
+		{
+			$r = strcasecmp( $a_Plugin->classname, $b_Plugin->classname );
+		}
+
+		return $r;
+	}
+
+	/**
+	 * Callback function to sort plugins by name.
+	 *
+	 * WARNING: do NOT sort by anything else than priority unless you're handling a list of NOT-YET-INSTALLED plugins
+	 * @todo Move to Plugins_admin
+	 */
+	function sort_Plugin_name( & $a_ID, & $b_ID )
+	{
+		$a_Plugin = & $this->get_by_ID( $a_ID );
+		$b_Plugin = & $this->get_by_ID( $b_ID );
+
+		return strcasecmp( $a_Plugin->name, $b_Plugin->name );
+	}
+
+
+	/**
+	 * Callback function to sort plugins by group, sub-group and name.
+	 *
+	 * Those, which have a group get sorted above the ones without one.
+	 *
+	 * WARNING: do NOT sort by anything else than priority unless you're handling a list of NOT-YET-INSTALLED plugins
+	 * @todo Move to Plugins_admin
+	 */
+	function sort_Plugin_group( & $a_ID, & $b_ID )
+	{
+		$a_Plugin = & $this->get_by_ID( $a_ID );
+		$b_Plugin = & $this->get_by_ID( $b_ID );
+
+		// first check if both have a group (-1: only A has a group; 1: only B has a group; 0: both have a group or no group):
+		$r = (int)empty($a_Plugin->group) - (int)empty($b_Plugin->group);
+		if( $r != 0 )
+		{
+			return $r;
+		}
+
+		// Compare Group
+		$r = strcasecmp( $a_Plugin->group, $b_Plugin->group );
+		if( $r != 0 )
+		{
+			return $r;
+		}
+
+		// Compare Sub Group
+		$r = strcasecmp( $a_Plugin->sub_group, $b_Plugin->sub_group );
+		if( $r != 0 )
+		{
+			return $r;
+		}
+
+		// Compare Name
+		return strcasecmp( $a_Plugin->name, $b_Plugin->name );
+	}
+
+
 }
 
 
 /* {{{ Revision log:
  * $Log$
+ * Revision 1.2  2006/11/30 05:57:54  blueyed
+ * Moved Plugins::install() and sort() galore to Plugins_admin
+ *
  * Revision 1.1  2006/11/30 05:43:40  blueyed
  * Moved Plugins::discover() to Plugins_admin::discover(); Renamed Plugins_no_DB to Plugins_admin_no_DB (and deriving from Plugins_admin)
  *
