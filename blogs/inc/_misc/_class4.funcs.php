0b53d8e35ed13e961bf6bdb1533109058d063329
Daniel Hahler <git+b2evo@thequod.de>
2006-12-01 02:01:38 +0000

    Added "Plugins_admin" to get_Cache() + doc

--- blogs/inc/_misc/_class4.funcs.php
+++ blogs/inc/_misc/_class4.funcs.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Function for handling Classes in PHP 4.
- * 
+ *
  * Note: in PHP 5, another file should be included. It shoudl handle clone for example.
  *
  * This file is part of the evoCore framework - {@link http://evocore.net/}
@@ -27,6 +27,9 @@
 if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
 
 
+/**
+ * Load class file
+ */
 function load_class( $class_path )
 {
 	global $inc_path;
@@ -34,6 +37,9 @@ function load_class( $class_path )
 }
 
 
+/**
+ * Load functions file
+ */
 function load_funcs( $funcs_path )
 {
 	global $inc_path;
@@ -45,36 +51,36 @@ function & get_Cache( $objectName )
 {
 	global $Plugins;
 	global $$objectName;
-	
+
 	if( isset( $$objectName ) )
 	{	// Cache already exists:
 		return $$objectName;
 	}
-	
+
 	switch( $objectName )
 	{
 		case 'BlogCache';
-			load_class( '/MODEL/collections/_blogcache.class.php' );
+			load_class( 'MODEL/collections/_blogcache.class.php' );
 			$BlogCache = new BlogCache(); // COPY (FUNC)
 			return $BlogCache;
 
 		case 'ChapterCache';
-			load_class( '/MODEL/collections/_chaptercache.class.php' );
+			load_class( 'MODEL/collections/_chaptercache.class.php' );
 			$ChapterCache = new ChapterCache(); // COPY (FUNC)
 			return $ChapterCache;
 
 		case 'FileCache';
-			load_class( '/MODEL/files/_filecache.class.php' );
+			load_class( 'MODEL/files/_filecache.class.php' );
 			$FileCache = new FileCache(); // COPY (FUNC)
 			return $FileCache;
 
 		case 'FileRootCache';
-			load_class( '/MODEL/files/_filerootcache.class.php' );
+			load_class( 'MODEL/files/_filerootcache.class.php' );
 			$Plugins->get_object_from_cacheplugin_or_create( 'FileRootCache' );
 			return $FileRootCache;
 
 		case 'FiletypeCache';
-			load_class( '/MODEL/files/_filerootcache.class.php' );
+			load_class( 'MODEL/files/_filerootcache.class.php' );
 			$Plugins->get_object_from_cacheplugin_or_create( 'FiletypeCache' );
 			return $FiletypeCache;
 
@@ -83,36 +89,44 @@ function & get_Cache( $objectName )
 			return $GroupCache;
 
 		case 'ItemCache';
-			load_class( '/MODEL/items/_itemcache.class.php' );
+			load_class( 'MODEL/items/_itemcache.class.php' );
 			$ItemCache = new ItemCache(); // COPY (FUNC)
 			return $ItemCache;
-			
+
 		case 'ItemStatusCache';
 			$Plugins->get_object_from_cacheplugin_or_create( 'ItemStatusCache', 'new GenericCache( \'GenericElement\', true, \'T_itemstatuses\', \'pst_\', \'pst_ID\' )' );
 			return $ItemStatusCache;
 
 		case 'ItemTypeCache';
-			load_class( '/MODEL/items/_itemtypecache.class.php' );
+			load_class( 'MODEL/items/_itemtypecache.class.php' );
 			$Plugins->get_object_from_cacheplugin_or_create( 'ItemTypeCache', 'new ItemTypeCache( \'ptyp_\', \'ptyp_ID\' )' );
 			return $ItemTypeCache;
 
 		case 'LinkCache';
-			load_class( '/MODEL/items/_linkcache.class.php' );
+			load_class( 'MODEL/items/_linkcache.class.php' );
 			$LinkCache = new LinkCache(); // COPY (FUNC)
 			return $LinkCache;
-			
+
+		case 'Plugins_admin':
+			load_class( '_misc/_plugins_admin.class.php' );
+			$Plugins_admin = new Plugins_admin(); // COPY (FUNC)
+			return $LinkCache;
+
 		case 'UserCache';
-			load_class( '/MODEL/users/_usercache.class.php' );
+			load_class( 'MODEL/users/_usercache.class.php' );
 			$UserCache = new UserCache(); // COPY (FUNC)
 			return $UserCache;
-			
+
 		default:
-			debug_die( 'getCache(): Unknown Cache type:'.$objectName );		
+			debug_die( 'getCache(): Unknown Cache type:'.$objectName );
 	}
 }
- 
+
 /*
  * $Log$
+ * Revision 1.6  2006/12/01 02:01:38  blueyed
+ * Added "Plugins_admin" to get_Cache() + doc
+ *
  * Revision 1.5  2006/11/30 22:34:15  fplanque
  * bleh
  *
