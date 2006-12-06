8ddecc78a26acb0563e8b05dc022fcdffed3d3b8
Francois Planque <github@fplanque.net>
2006-12-06 21:22:49 +0000

    the jQuery tentative and more

--- blogs/inc/VIEW/files/_set_files.form.php
+++ blogs/inc/VIEW/files/_set_files.form.php
@@ -44,14 +44,48 @@ global $Settings;
  * EXPERIMENTAL
  * Will be moved to another file, I'm leaving it here for a short period, in order to provide context
  *
- * @param string DOM id
+ * PROBLEM: jQuery is not necessarily loaded at the moment we use this :(
+ *
+ * @param string DOM class
  * @param string DOM id
  */
-function JS_showhide_on_checkbox( $div_id, $checkbox_id )
+function JS_showhide_class_on_checkbox( $class, $checkbox_id )
 {
 	return '<script type="text/javascript">
-		document.getElementById("'.$div_id.'").style.display = (document.getElementById("'.$checkbox_id.'").checked==true ? "" : "none")
+    if( document.getElementById("'.$checkbox_id.'").checked )
+		{
+ 			$(".'.$class.'").show();
+		}
+		else
+		{
+ 			$(".'.$class.'").hide();
+		}
 	</script>';
+}
+
+/**
+ * Javascript to init hidden/shown state of something (like a DIV) based on a checkbox
+ *
+ * EXPERIMENTAL
+ * Will be moved to another file, I'm leaving it here for a short period, in order to provide context
+ *
+ * @param array|string DOM id
+ * @param string DOM id
+ */
+function JS_showhide_ids_on_checkbox( $div_ids, $checkbox_id )
+{
+	if( !is_array($div_ids) )
+	{
+		$div_ids = array( $div_ids );
+	}
+	$r = '<script type="text/javascript">
+		var display = document.getElementById("'.$checkbox_id.'").checked ? "" : "none";'."\n";
+	foreach( $div_ids as $div_id )
+	{
+		$r .= 'document.getElementById("'.$div_id.'").style.display = display;'."\n";
+	}
+	$r .= '</script>';
+	return $r;
 }
 
 /**
@@ -66,8 +100,28 @@ function JS_showhide_on_checkbox( $div_id, $checkbox_id )
 function JS_showhide_ffield_on_checkbox( $field_id, $checkbox_id )
 {
 	return '<script type="text/javascript">
-		document.getElementById("ffield_'.$field_id.'").style.display = (document.getElementById("'.$checkbox_id.'").checked==true ? "" : "none")
+		document.getElementById("ffield_'.$field_id.'").style.display = (document.getElementById("'.$checkbox_id.'").checked ? "" : "none")
 	</script>';
+}
+
+/**
+ * Javascript hide/show all DOM elements with a particular class based on current checkbox
+ *
+ * EXPERIMENTAL
+ * Will be moved to another file, I'm leaving it here for a short period, in order to provide context
+ *
+ * @param string DOM class name
+ */
+function JS_showhide_class_on_this( $class )
+{
+	return 'if( this.checked )
+		{
+ 			$(".'.$class.'").show();
+		}
+		else
+		{
+ 			$(".'.$class.'").hide();
+		}';
 }
 
 /**
@@ -76,11 +130,20 @@ function JS_showhide_ffield_on_checkbox( $field_id, $checkbox_id )
  * EXPERIMENTAL
  * Will be moved to another file, I'm leaving it here for a short period, in order to provide context
  *
- * @param string DOM id
+ * @param array|string DOM ids
  */
-function JS_showhide_on_this( $div_id )
+function JS_showhide_ids_on_this( $div_ids )
 {
-	return 'document.getElementById("'.$div_id.'").style.display = (this.checked==true ? "" : "none")';
+	if( !is_array($div_ids) )
+	{
+		$div_ids = array( $div_ids );
+	}
+	$r = 'var display = this.checked ? "" : "none";'."\n";
+	foreach( $div_ids as $div_id )
+	{
+		$r .= 'document.getElementById("'.$div_id.'").style.display = display;'."\n";
+	}
+	return $r;
 }
 
 /**
@@ -93,7 +156,7 @@ function JS_showhide_on_this( $div_id )
  */
 function JS_showhide_ffield_on_this( $field_id )
 {
-	return 'document.getElementById("ffield_'.$field_id.'").style.display = (this.checked==true ? "" : "none")';
+	return 'document.getElementById("ffield_'.$field_id.'").style.display = (this.checked ? "" : "none")';
 }
 
 
@@ -106,18 +169,16 @@ $Form->hidden( 'action', 'update' );
 
 $Form->begin_fieldset( T_('File Manager') );
 	$Form->checkbox_input( 'fm_enabled', $Settings->get('fm_enabled'), T_('Enable Filemanager'), array(
-		'note' => T_('Check to enable the Filemanager.' ), 'onclick' => JS_showhide_on_this('additional_file_settings') ) );
+		'note' => T_('Check to enable the Filemanager.' ), 'onclick' => JS_showhide_ids_on_this(array( 'ffset_fileroots', 'ffset_filecreate', 'ffset_fileadvanced' )) ) );
 $Form->end_fieldset();
 
-echo '<div id="additional_file_settings">';	// fp> TODO: not compatible with TABLE layout (many such abuses already in the code)
-
-$Form->begin_fieldset( T_('Accessible file roots') );
+$Form->begin_fieldset( T_('Accessible file roots'), array( 'id' => 'ffset_fileroots', 'class' => 'additional_file_settings' ) );
 	$Form->checkbox( 'fm_enable_roots_blog', $Settings->get('fm_enable_roots_blog'), T_('Enable blog directories'), T_('Check to enable root directories for blogs.' ) );
 	// $Form->checkbox( 'fm_enable_roots_group', $Settings->get('fm_enable_roots_group'), T_('Enable group directories'), T_('Check to enable root directories for groups.' ) );
 	$Form->checkbox( 'fm_enable_roots_user', $Settings->get('fm_enable_roots_user'), T_('Enable user directories'), T_('Check to enable root directories for users.' ) );
 $Form->end_fieldset();
 
-$Form->begin_fieldset( T_('File creation options') );
+$Form->begin_fieldset( T_('File creation options'), array( 'id' => 'ffset_filecreate', 'class' => 'additional_file_settings' ) );
 	$Form->checkbox( 'fm_enable_create_dir', $Settings->get('fm_enable_create_dir'), T_('Enable creation of folders'), T_('Check to enable creation of directories.' ) );
 	$Form->checkbox( 'fm_enable_create_file', $Settings->get('fm_enable_create_file'), T_('Enable creation of files'), T_('Check to enable creation of files.' ) );
 	$Form->checkbox_input( 'upload_enabled', $Settings->get('upload_enabled'), T_('Enable upload of files'), array(
@@ -128,7 +189,7 @@ $Form->begin_fieldset( T_('File creation options') );
 	echo JS_showhide_ffield_on_checkbox( 'upload_maxkb', 'upload_enabled' );
 $Form->end_fieldset();
 
-$Form->begin_fieldset( T_('Advanced options') );
+$Form->begin_fieldset( T_('Advanced options'), array( 'id' => 'ffset_fileadvanced', 'class' => 'additional_file_settings' ) );
 
 	$Form->text_input( 'fm_default_chmod_dir', $Settings->get('fm_default_chmod_dir'), 4, T_('Default folder permissions'), array('note'=>T_('Default CHMOD (UNIX permissions) for new directories created by the file manager.' )) );
 
@@ -162,9 +223,8 @@ $Form->begin_fieldset( T_('Advanced options') );
 
 $Form->end_fieldset();
 
-echo '</div>';
 // Javascript to init hidden/shown state:
-echo JS_showhide_on_checkbox( 'additional_file_settings', 'fm_enabled' );
+echo JS_showhide_ids_on_checkbox( array( 'ffset_fileroots', 'ffset_filecreate', 'ffset_fileadvanced' ), 'fm_enabled' );
 
 if( $current_User->check_perm( 'options', 'edit', false ) )
 { // We have permission to modify:
@@ -181,6 +241,9 @@ $Form->end_form();
 
 /*
  * $Log$
+ * Revision 1.10  2006/12/06 21:22:49  fplanque
+ * the jQuery tentative and more
+ *
  * Revision 1.9  2006/12/06 18:06:18  fplanque
  * an experiment with JS hiding/showing form parts
  *
