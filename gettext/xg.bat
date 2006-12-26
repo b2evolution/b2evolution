f24d4b8904e2e8bf9e26270d30413e517f27fad6
Francois Planque <github@fplanque.net>
2006-12-26 00:57:11 +0000

    i18n update

--- gettext/xg.bat
+++ gettext/xg.bat
@@ -7,7 +7,7 @@ echo Extract strings.
 xgettext -D ../blogs/ -f files.txt --no-wrap --add-comments=TRANS --copyright-holder="Francois PLANQUE" --msgid-bugs-address=http://fplanque.net/ --output=..\blogs\locales\messages.pot --keyword=T_ --keyword=NT_ --keyword=TS_
 
 echo Correct paths.
-xchangecl +d! ..\blogs\locales\messages.pot !C:\www\b2evo\blogs\!..\..\..\!
+xchangecl +d! ..\blogs\locales\messages.pot !D:\www\b2evo20\blogs\!..\..\..\!
 
 echo Correct Header.
 xchangecl +d! -t# ..\blogs\locales\messages.pot !#35##32#SOME#32#DESCRIPTIVE#32#TITLE.!#35##32#b2evolution#32#-#32#Language#32#file!#35##32#This#32#file#32#is#32#distributed#32#under#32#the#32#same#32#license#32#as#32#the#32#PACKAGE#32#package.!#35##32#This#32#file#32#is#32#distributed#32#under#32#the#32#same#32#license#32#as#32#the#32#b2evolution#32#package.!"Content-Type:#32#text/plain;#32#charset=CHARSET\n"!"Content-Type:#32#text/plain;#32#charset=iso-8859-1\n"!
