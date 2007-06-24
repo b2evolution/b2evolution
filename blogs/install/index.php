f0573c95d28cf352a0f3dfc061be11eef5c746b1
Francois Planque <github@fplanque.net>
2007-06-24 17:38:28 +0000

    revert

--- blogs/install/index.php
+++ blogs/install/index.php
@@ -46,6 +46,7 @@ require_once $model_path.'items/_item.class.php';
 require_once $model_path.'items/_item.funcs.php';
 require_once $misc_inc_path.'_form.funcs.php';
 require_once $model_path.'users/_user.funcs.php';
+require_once $model_path.'skins/_skin.funcs.php';
 require_once $misc_inc_path.'_timer.class.php';
 require_once $misc_inc_path.'_plugins.class.php';
 require_once dirname(__FILE__).'/_functions_install.php';
@@ -647,9 +648,8 @@ to
 <?php
 /*
  * $Log$
- * Revision 1.130  2007/06/24 01:05:31  fplanque
- * skin_include() now does all the template magic for skins 2.0.
- * .disp.php templates still need to be cleaned up.
+ * Revision 1.131  2007/06/24 17:38:28  fplanque
+ * revert
  *
  * Revision 1.129  2007/06/12 21:00:02  blueyed
  * Added non-JS handling of deletedb confirmation
