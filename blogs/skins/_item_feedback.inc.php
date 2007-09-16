b16e57c8e407f12490dcd858ecfab9d080e99cbd
Francois Planque <github@fplanque.net>
2007-09-16 22:07:06 +0000

    cleaned up feedback form

--- blogs/skins/_item_feedback.inc.php
+++ blogs/skins/_item_feedback.inc.php
@@ -20,6 +20,9 @@
  */
 if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
 
+?>
+<!-- ===================== START OF FEEDBACK ===================== -->
+<?php
 
 // Default params:
 $params = array_merge( array(
@@ -123,6 +126,18 @@ if( $params['disp_trackback_url'] )
 
 if( $params['disp_comments'] || $params['disp_trackbacks'] || $params['disp_pingbacks']  )
 {
+	if( empty($disp_title) )
+	{	// No title yet
+		if( $title = $Item->get_feedback_title( 'feedbacks', '', T_('Feedback awaiting moderation'), T_('Feedback awaiting moderation'), 'draft' ) )
+		{ // We have some feedback awaiting moderation: we'll want to show that in the title
+			$disp_title[] = $title;
+		}
+	}
+
+	if( empty($disp_title) )
+	{	// Still no title
+		$disp_title[] = T_('No feedback yet');
+	}
 
 	echo $params['before_section_title'];
 	echo implode( ', ', $disp_title);
@@ -195,165 +210,169 @@ if( $params['disp_comments'] || $params['disp_trackbacks'] || $params['disp_ping
 
 	// _______________________________________________________________
 
+}
 
 
 
+/*
+ * Comment form:
+ */
+if( $params['disp_comment_form'] && $Item->can_comment() )
+{ // We want to display the comments form and the item can be commented on:
 
-	// Comment form:
-	if( $params['disp_comment_form'] && $Item->can_comment() )
-	{ // We want to display the comments form and the item can be commented on:
+	// Default form params:
+	$comment_author = isset($_COOKIE[$cookie_name]) ? trim($_COOKIE[$cookie_name]) : '';
+	$comment_author_email = isset($_COOKIE[$cookie_email]) ? trim($_COOKIE[$cookie_email]) : '';
+	$comment_author_url = isset($_COOKIE[$cookie_url]) ? trim($_COOKIE[$cookie_url]) : '';
+	$comment_content = '';
 
-		// Default form params:
-		$comment_author = isset($_COOKIE[$cookie_name]) ? trim($_COOKIE[$cookie_name]) : '';
-		$comment_author_email = isset($_COOKIE[$cookie_email]) ? trim($_COOKIE[$cookie_email]) : '';
-		$comment_author_url = isset($_COOKIE[$cookie_url]) ? trim($_COOKIE[$cookie_url]) : '';
-		$comment_content = '';
+	// PREVIEW:
+	$preview_Comment = $Session->get('core.preview_Comment');
 
-		// PREVIEW:
-		$preview_Comment = $Session->get('core.preview_Comment');
-
-		if( $preview_Comment )
-		{
-			if( $preview_Comment->item_ID == $Item->ID )
-			{ // display PREVIEW:
-				?>
-				<div class="bComment" id="comment_preview">
-					<div class="bCommentTitle">
-					<?php
-						echo T_('PREVIEW Comment from:').' ';
-						$preview_Comment->author();
-						$preview_Comment->msgform_link( $Blog->get('msgformurl') );
-						$preview_Comment->author_url( '', ' &middot; ', '' );
-					?>
-					</div>
-					<div class="bCommentText">
-						<?php $preview_Comment->content() ?>
-					</div>
-					<div class="bCommentSmallPrint">
-						<?php $preview_Comment->date() ?> @ <?php $preview_Comment->time( 'H:i' ) ?>
-					</div>
-				</div>
-
+	if( $preview_Comment )
+	{
+		if( $preview_Comment->item_ID == $Item->ID )
+		{ // display PREVIEW:
+			?>
+			<div class="bComment" id="comment_preview">
+				<div class="bCommentTitle">
 				<?php
-				// Form fields:
-				$comment_content = $preview_Comment->original_content;
-				// for visitors:
-				$comment_author = $preview_Comment->author;
-				$comment_author_email = $preview_Comment->author_email;
-				$comment_author_url = $preview_Comment->author_url;
-			}
+					echo T_('PREVIEW Comment from:').' ';
+					$preview_Comment->author();
+					$preview_Comment->msgform_link( $Blog->get('msgformurl') );
+					$preview_Comment->author_url( '', ' &middot; ', '' );
+				?>
+				</div>
+				<div class="bCommentText">
+					<?php $preview_Comment->content() ?>
+				</div>
+				<div class="bCommentSmallPrint">
+					<?php $preview_Comment->date() ?> @ <?php $preview_Comment->time( 'H:i' ) ?>
+				</div>
+			</div>
 
-			// delete any preview comment from session data:
-			$Session->delete( 'core.preview_Comment' );
-			$preview_Comment = NULL;
+			<?php
+			// Form fields:
+			$comment_content = $preview_Comment->original_content;
+			// for visitors:
+			$comment_author = $preview_Comment->author;
+			$comment_author_email = $preview_Comment->author_email;
+			$comment_author_url = $preview_Comment->author_url;
 		}
 
-
-		echo $params['before_section_title'];
-		echo T_('Leave a comment');
-		echo $params['after_section_title'];
-
-
-		$Form = & new Form( $htsrv_url.'comment_post.php', 'bComment_form_id_'.$Item->ID );
-		$Form->begin_form( 'bComment', '', array( 'target' => '_self' ) );
-
-		// TODO: dh> a plugin hook would be useful here to add something to the top of the Form.
-		//           Actually, the best would be, if the $Form object could be changed by a plugin
-		//           before display!
-
-		$Form->hidden( 'comment_post_ID', $Item->ID );
-		$Form->hidden( 'redirect_to',
-				// Make sure we get back to the right page (on the right domain)
-				// fplanque>> TODO: check if we can use the permalink instead but we must check that application wide,
-				// that is to say: check with the comments in a pop-up etc...
-				url_rel_to_same_host(regenerate_url( '', '', $Blog->get('blogurl'), '&' ), $htsrv_url) );
-
-		if( is_logged_in() )
-		{ // User is logged in:
-			$Form->begin_fieldset();
-			$Form->info_field( T_('User'), '<strong>'.$current_User->get_preferred_name().'</strong>'
-				.' '.get_user_profile_link( ' [', ']', T_('Edit profile') ) );
-			$Form->end_fieldset();
-		}
-		else
-		{ // User is not logged in:
-			// Note: we use funky field names to defeat the most basic guestbook spam bots
-			$Form->text( 'u', $comment_author, 40, T_('Name'), '', 100, 'bComment' );
-			$Form->text( 'i', $comment_author_email, 40, T_('Email'), T_('Your email address will <strong>not</strong> be displayed on this site.'), 100, 'bComment' );
-			$Form->text( 'o', $comment_author_url, 40, T_('Site/Url'), T_('Your URL will be displayed.'), 100, 'bComment' );
-		}
-
-		echo '<div class="comment_toolbars">';
-		// CALL PLUGINS NOW:
-		$Plugins->trigger_event( 'DisplayCommentToolbar', array() );
-		echo '</div>';
-
-		// Message field:
-		// TODO: dh> this uses "id" "p" - should be more distinctive..
-		$Form->textarea( 'p', $comment_content, 10, T_('Comment text'),
-										T_('Allowed XHTML tags').': '.htmlspecialchars(str_replace( '><',', ', $comment_allowed_tags)), 40, 'bComment' );
-		// set b2evoCanvas for plugins
-		echo '<script type="text/javascript">var b2evoCanvas = document.getElementById( "p" );</script>';
-		$comment_options = array();
-		$Form->output = false;
-		$Form->label_to_the_left = false;
-		$old_label_suffix = $Form->label_suffix;
-		$Form->label_suffix = '';
-		$Form->switch_layout('inline');
-		if( substr($comments_use_autobr,0,4) == 'opt-')
-		{
-			$comment_options[] = $Form->checkbox_input( 'comment_autobr', ($comments_use_autobr == 'opt-out'), T_('Auto-BR'), array(
-				'note' => '('.T_('Line breaks become &lt;br /&gt;').')', 'tabindex' => 6 ) );
-		}
-		if( ! is_logged_in() )
-		{ // User is not logged in:
-			$comment_options[] = $Form->checkbox_input( 'comment_cookies', true, T_('Remember me'), array(
-				'note' => '('.T_('Set cookies for name, email and url').')', 'tabindex' => 7 ) );
-			// TODO: If we got info from cookies, Add a link called "Forget me now!" (without posting a comment).
-
-			$comment_options[] = $Form->checkbox_input( 'comment_allow_msgform', true, T_('Allow message form'), array(
-				'note' => '('.T_('Allow users to contact you through a message form (your email will NOT be displayed.)').')', 'tabindex' => 8 ) );
-			// TODO: If we have an email in a cookie, Add links called "Add a contact icon to all my previous comments" and "Remove contact icon from all my previous comments".
-		}
-		$Form->output = true;
-		$Form->label_to_the_left = true;
-		$Form->label_suffix = $old_label_suffix;
-		$Form->switch_layout(NULL);
-
-		if( ! empty($comment_options) )
-		{
-			$Form->begin_fieldset();
-				echo $Form->begin_field( NULL, T_('Options'), true );
-				echo implode( '<br />', $comment_options );
-				echo $Form->end_field();
-			$Form->end_fieldset();
-		}
-
-		$Plugins->trigger_event( 'DisplayCommentFormFieldset', array( 'Form' => & $Form, 'Item' => & $Item ) );
-
-		$Form->begin_fieldset();
-			echo '<div class="input">';
-
-			$Form->button_input( array( 'name' => 'submit_comment_post_'.$Item->ID.'[save]', 'class' => 'submit', 'value' => T_('Send comment'), 'tabindex' => 10 ) );
-			$Form->button_input( array( 'name' => 'submit_comment_post_'.$Item->ID.'[preview]', 'class' => 'preview', 'value' => T_('Preview'), 'tabindex' => 9 ) );
-
-			$Plugins->trigger_event( 'DisplayCommentFormButton', array( 'Form' => & $Form, 'Item' => & $Item ) );
-
-			echo '</div>';
-		$Form->end_fieldset();
-		?>
-
-		<div class="clear"></div>
-
-		<?php
-		$Form->end_form();
+		// delete any preview comment from session data:
+		$Session->delete( 'core.preview_Comment' );
+		$preview_Comment = NULL;
 	}
 
+
+	echo $params['before_section_title'];
+	echo T_('Leave a comment');
+	echo $params['after_section_title'];
+
+
+	$Form = & new Form( $htsrv_url.'comment_post.php', 'bComment_form_id_'.$Item->ID );
+	$Form->begin_form( 'bComment', '', array( 'target' => '_self' ) );
+
+	// TODO: dh> a plugin hook would be useful here to add something to the top of the Form.
+	//           Actually, the best would be, if the $Form object could be changed by a plugin
+	//           before display!
+
+	$Form->hidden( 'comment_post_ID', $Item->ID );
+	$Form->hidden( 'redirect_to',
+			// Make sure we get back to the right page (on the right domain)
+			// fplanque>> TODO: check if we can use the permalink instead but we must check that application wide,
+			// that is to say: check with the comments in a pop-up etc...
+			url_rel_to_same_host(regenerate_url( '', '', $Blog->get('blogurl'), '&' ), $htsrv_url) );
+
+	if( is_logged_in() )
+	{ // User is logged in:
+		$Form->begin_fieldset();
+		$Form->info_field( T_('User'), '<strong>'.$current_User->get_preferred_name().'</strong>'
+			.' '.get_user_profile_link( ' [', ']', T_('Edit profile') ) );
+		$Form->end_fieldset();
+	}
+	else
+	{ // User is not logged in:
+		// Note: we use funky field names to defeat the most basic guestbook spam bots
+		$Form->text( 'u', $comment_author, 40, T_('Name'), '', 100, 'bComment' );
+		$Form->text( 'i', $comment_author_email, 40, T_('Email'), T_('Your email address will <strong>not</strong> be displayed on this site.'), 100, 'bComment' );
+		$Form->text( 'o', $comment_author_url, 40, T_('Site/Url'), T_('Your URL will be displayed.'), 100, 'bComment' );
+	}
+
+	echo '<div class="comment_toolbars">';
+	// CALL PLUGINS NOW:
+	$Plugins->trigger_event( 'DisplayCommentToolbar', array() );
+	echo '</div>';
+
+	// Message field:
+	$note = '';
+	// $note = T_('Allowed XHTML tags').': '.htmlspecialchars(str_replace( '><',', ', $comment_allowed_tags));
+	$Form->textarea( 'p', $comment_content, 10, T_('Comment text'), $note, 40, 'bComment' );
+
+	// set b2evoCanvas for plugins
+	echo '<script type="text/javascript">var b2evoCanvas = document.getElementById( "p" );</script>';
+	$comment_options = array();
+	$Form->output = false;
+	$Form->label_to_the_left = false;
+	$old_label_suffix = $Form->label_suffix;
+	$Form->label_suffix = '';
+	$Form->switch_layout('inline');
+	if( substr($comments_use_autobr,0,4) == 'opt-')
+	{
+		$comment_options[] = $Form->checkbox_input( 'comment_autobr', ($comments_use_autobr == 'opt-out'), T_('Auto-BR'), array(
+			'note' => '('.T_('Line breaks become &lt;br /&gt;').')', 'tabindex' => 6 ) );
+	}
+	if( ! is_logged_in() )
+	{ // User is not logged in:
+		$comment_options[] = $Form->checkbox_input( 'comment_cookies', true, T_('Remember me'), array(
+			'note' => '('.T_('Set cookies for name, email and url').')', 'tabindex' => 7 ) );
+		// TODO: If we got info from cookies, Add a link called "Forget me now!" (without posting a comment).
+
+		$comment_options[] = $Form->checkbox_input( 'comment_allow_msgform', true, T_('Allow message form'), array(
+			'note' => '('.T_('Allow users to contact you through a message form (your email will NOT be displayed.)').')', 'tabindex' => 8 ) );
+		// TODO: If we have an email in a cookie, Add links called "Add a contact icon to all my previous comments" and "Remove contact icon from all my previous comments".
+	}
+	$Form->output = true;
+	$Form->label_to_the_left = true;
+	$Form->label_suffix = $old_label_suffix;
+	$Form->switch_layout(NULL);
+
+	if( ! empty($comment_options) )
+	{
+		$Form->begin_fieldset();
+			echo $Form->begin_field( NULL, T_('Options'), true );
+			echo implode( '<br />', $comment_options );
+			echo $Form->end_field();
+		$Form->end_fieldset();
+	}
+
+	$Plugins->trigger_event( 'DisplayCommentFormFieldset', array( 'Form' => & $Form, 'Item' => & $Item ) );
+
+	$Form->begin_fieldset();
+		echo '<div class="input">';
+
+		$Form->button_input( array( 'name' => 'submit_comment_post_'.$Item->ID.'[save]', 'class' => 'submit', 'value' => T_('Send comment'), 'tabindex' => 10 ) );
+		$Form->button_input( array( 'name' => 'submit_comment_post_'.$Item->ID.'[preview]', 'class' => 'preview', 'value' => T_('Preview'), 'tabindex' => 9 ) );
+
+		$Plugins->trigger_event( 'DisplayCommentFormButton', array( 'Form' => & $Form, 'Item' => & $Item ) );
+
+		echo '</div>';
+	$Form->end_fieldset();
+	?>
+
+	<div class="clear"></div>
+
+	<?php
+	$Form->end_form();
 }
 
 
 /*
  * $Log$
+ * Revision 1.5  2007/09/16 22:07:06  fplanque
+ * cleaned up feedback form
+ *
  * Revision 1.4  2007/09/08 19:31:28  fplanque
  * cleanup of XML feeds for comments on individual posts.
  *
