70fe55c7f2c624add26eb1a2aa1e4e3b7e08e3a6
stephankn <stephankn@users.sourceforge.net>
2008-06-08 18:50:43 +0000

    beautify code (inserted spaces) to meet b2evo coding guidelines

--- blogs/htsrv/getfile.php
+++ blogs/htsrv/getfile.php
@@ -34,13 +34,13 @@
 /**
  * Load config, init and get the {@link $mode mode param}.
  */
-require_once dirname(__FILE__).'/../conf/_config.php';
-require_once $inc_path.'/_main.inc.php';
+require_once dirname( __FILE__ ) . '/../conf/_config.php';
+require_once $inc_path . '/_main.inc.php';
 
 
 // Check permission:
 // fp> TODO: skip this if $public_access_to_media  (needs testing before enabling)
-if( ! isset($current_User) )
+if ( ! isset( $current_User ) )
 {
 	debug_die( 'No permission to get file (not logged in)!' );
 }
@@ -48,13 +48,13 @@ $current_User->check_perm( 'files', 'view', true );
 // fp> TODO: check specific READ perm for requested fileroot
 
 // Load params:
-param( 'root', 'string', true );	// the root directory from the dropdown box (user_X or blog_X; X is ID - 'user' for current user (default))
+param( 'root', 'string', true ); // the root directory from the dropdown box (user_X or blog_X; X is ID - 'user' for current user (default))
 param( 'path', 'string', true );
-param( 'size', 'string', NULL );	// Can be used for images.
+param( 'size', 'string', null ); // Can be used for images.
 
-if ( false !== strpos( urldecode( $path ), '..' ) ) 
+if ( false !== strpos( urldecode( $path ), '..' ) )
 {
-  debug_die( 'Relative pathnames not allowed!' );
+	debug_die( 'Relative pathnames not allowed!' );
 }
 
 // Load fileroot info:
@@ -64,20 +64,22 @@ $FileRoot = & $FileRootCache->get_by_ID( $root );
 // Load file object (not the file content):
 $File = & new File( $FileRoot->type, $FileRoot->in_type_ID, $path );
 
-if( !empty($size) && $File->is_image() )
-{	// We want a thumbnail:
+if ( !empty( $size ) && $File->is_image() )
+{ 
+	// We want a thumbnail:
 	// This will do all the magic:
 	$File->thumbnail( $size );
 }
 else
-{	// We want the regular file:
+{
+	// We want the regular file:
 	// Headers to display the file directly in the browser
-	header('Content-type: '.$File->Filetype->mimetype );
-	header('Content-Length: '.filesize( $File->get_full_path() ) );
+	header( 'Content-type: ' . $File->Filetype->mimetype );
+	header( 'Content-Length: ' . filesize( $File->get_full_path() ) );
 
-	if( $File->Filetype->viewtype == 'download' )
+	if ( $File->Filetype->viewtype == 'download' )
 	{
-		header('Content-disposition: attachment; filename="'.$File->get_name().'"' );
+		header( 'Content-disposition: attachment; filename="' . $File->get_name() . '"' );
 	}
 
 	// Display the content of the file
@@ -86,6 +88,9 @@ else
 
 /*
  * $Log$
+ * Revision 1.16  2008/06/08 18:50:43  stephankn
+ * beautify code (inserted spaces) to meet b2evo coding guidelines
+ *
  * Revision 1.15  2008/06/08 17:39:01  stephankn
  * check for relative paths and stop execution if detected
  *
