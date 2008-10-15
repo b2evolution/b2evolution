f02a75575ccffd59d92dda679ed61566d813d26e
Daniel Hahler <git+b2evo@thequod.de>
2008-10-15 22:07:54 +0000

    todo/bug

--- blogs/inc/files/model/_file.class.php
+++ blogs/inc/files/model/_file.class.php
@@ -570,6 +570,7 @@ class File extends DataObject
 				$root = $this->_FileRoot->ID;
 				$url = $htsrv_url.'getfile.php/'
 								// This is for clean 'save as':
+								// TODO: dh> this requires working PATH_INFO support! - otherwise the request might get redirected to index.php and fails (404)..
 								.rawurlencode( $this->_name )
 								// This is for locating the file:
 								.'?root='.$root.'&amp;path='.$this->_rdfp_rel_path;
@@ -1816,6 +1817,9 @@ class File extends DataObject
 
 /*
  * $Log$
+ * Revision 1.15  2008/10/15 22:07:54  blueyed
+ * todo/bug
+ *
  * Revision 1.14  2008/10/07 16:59:59  blueyed
  * todo/bug/notice
  *
