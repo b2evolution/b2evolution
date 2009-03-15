225f82973a9e137a5bdd4dfbf75dc96a6c9f9c6c
Francois Planque <github@fplanque.net>
2009-03-15 21:56:22 +0000

    factoring benefits are now falling into place...

--- blogs/inc/widgets/widgets/_linkblog.widget.php
+++ blogs/inc/widgets/widgets/_linkblog.widget.php
@@ -25,7 +25,7 @@
  */
 if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
 
-load_class( 'widgets/model/_widget.class.php' );
+load_class( 'widgets/widgets/_coll_item_list.widget.php' );
 
 /**
  * linkblog_widget class
@@ -34,7 +34,7 @@ load_class( 'widgets/model/_widget.class.php' );
  *
  * @package evocore
  */
-class linkblog_Widget extends ComponentWidget
+class linkblog_Widget extends coll_item_list_Widget
 {
 	/**
 	 * Constructor
@@ -47,12 +47,37 @@ class linkblog_Widget extends ComponentWidget
 
 
 	/**
+	 * Get definitions for editable params
+	 *
+	 * @see Plugin::GetDefaultSettings()
+	 * @param local params like 'for_editing' => true
+	 */
+	function get_param_definitions( $params )
+	{
+		// This is derived from coll_post_list_Widget, so we DO NOT ADD ANY param here!
+		$r = parent::get_param_definitions( $params );
+		// We only change the defaults and hide some params.
+		$r['title']['defaultvalue'] = T_('Linkblog');
+		$r['title_link']['no_edit'] = true;
+		$r['blog_ID']['defaultvalue'] = 0;		// zero is a magic number that we'll use to try and use defaults used in previous versions of B2evo
+		$r['item_group_by']['defaultvalue'] = 'chapter';
+		$r['item_title_link_type']['no_edit'] = true;
+		$r['disp_excerpt']['no_edit'] = true;
+		$r['disp_teaser']['no_edit'] = true;
+		$r['disp_teaser_maxwords']['no_edit'] = true;
+		$r['widget_css_class']['no_edit'] = true;
+		$r['widget_ID']['no_edit'] = true;
+
+		return $r;
+	}
+
+
+	/**
 	 * Get name of widget
 	 */
 	function get_name()
 	{
-		$title = T_('Linkblog');
-		return $title;
+		return T_('Linkblog');
 	}
 
 
@@ -70,86 +95,7 @@ class linkblog_Widget extends ComponentWidget
 	 */
 	function get_desc()
 	{
-		return T_('Display blog entries, grouped by category.');
-	}
-
-
-	/**
-	 * Get definitions for editable params
-	 *
-	 * @see Plugin::GetDefaultSettings()
-	 * @param local params like 'for_editing' => true
-	 */
-	function get_param_definitions( $params )
-	{
-		global $Blog;
-		// Demo data:
-		$r = array_merge( array(
-				'title' => array(
-					'label' => 'Block title',
-					'size' => 60,
-					'defaultvalue' => T_('Linkblog'),
-					'note' => T_( 'This is the title to display in your skin.' ),
-				),
-				'linkblog_ID' => array(
-					'label' => T_( 'Blog ID' ),
-					'size' => 4,
-					'defaultvalue' => $Blog->get('links_blog_ID'),	// Here we conveniently recycle the previous value from its deprecated links_blog_ID param. We will eventually drop that field from the database.
-					'note' => T_( 'This is the ID number of the blog to display.' ),
-				),
-				'order_by' => array(
-					'label' => T_('Order by'),
-					'note' => T_('How to sort the items'),
-					'type' => 'select',
-					'options' => get_available_sort_options(),
-					'defaultvalue' => 'title',
-				),
-				'order_dir' => array(
-					'label' => T_('Direction'),
-					'note' => T_('How to sort the items'),
-					'type' => 'select',
-					'options' => array( 'ASC'  => T_('Ascending'), 'DESC' => T_('Descending') ),
-					'defaultvalue' => 'ASC',
-				),
-				'limit' => array(
-					'label' => T_( 'Limit' ),
-					'size' => 4,
-					'defaultvalue' => 20,
-					'note' => T_( 'Maximum number of items to display.' ),
-				),
-				'disp_excerpt' => array(
-					'label' => T_( 'Excerpt' ),
-					'note' => T_( 'Display excerpt for each item.' ),
-					'type' => 'checkbox',
-					'defaultvalue' => false,
-				),
-				'item_title_link_type' => array(
-					'label' => T_('Link titles'),
-					'note' => T_('Where should titles be linked to?'),
-					'type' => 'select',
-					'options' => array(
-							'auto'        => T_('Automatic'),
-							'permalink'   => T_('Item permalink'),
-							'linkto_url'  => T_('Item URL'),
-							'none'        => T_('Nowhere'),
-						),
-					'defaultvalue' => 'auto',
-				),
-				'disp_teaser' => array(
-					'label' => T_( 'Teaser' ),
-					'type' => 'checkbox',
-					'defaultvalue' => false,
-					'note' => T_( 'Display teaser for each item.' ),
-				),
-				'disp_teaser_maxwords' => array(
-					'label' => T_( 'Max Words' ),
-					'type' => 'integer',
-					'defaultvalue' => 20,
-					'note' => T_( 'Max number of words for the teasers' ),
-				),
-			), parent::get_param_definitions( $params )	);
-
-		return $r;
+		return T_('Simplified Item list for listing posts.');
 	}
 
 
@@ -160,10 +106,25 @@ class linkblog_Widget extends ComponentWidget
 	 */
 	function display( $params )
 	{
+		global $Blog;
+
 		$this->init_display( $params );
 
-		// List of pages:
-		$this->disp_cat_item_list();
+		// Force some params (because this is a simplified widget):
+
+		if( $this->disp_params['blog_ID'] == 0 )
+		{	// We want to try and use previous defaults:
+			if( !empty( $this->disp_params['linkblog_ID'] ) )
+			{
+				$this->disp_params['blog_ID'] = $this->disp_params['linkblog_ID'];
+			}
+			else
+			{ // Recycle the previous value from deprecated links_blog_ID param. We will eventually drop that field from the database.
+				$this->disp_params['blog_ID'] = $Blog->get('links_blog_ID');
+			}
+		}
+
+		parent::display( $params );
 
 		return true;
 	}
@@ -172,6 +133,9 @@ class linkblog_Widget extends ComponentWidget
 
 /*
  * $Log$
+ * Revision 1.20  2009/03/15 21:56:22  fplanque
+ * factoring benefits are now falling into place...
+ *
  * Revision 1.19  2009/03/15 02:19:47  fplanque
  * auto link option for titles
  *
@@ -205,33 +169,5 @@ class linkblog_Widget extends ComponentWidget
  *
  * Revision 1.8  2008/01/21 09:35:37  fplanque
  * (c) 2008
- *
- * Revision 1.7  2008/01/17 18:10:12  fplanque
- * deprecated linkblog_ID blog param
- *
- * Revision 1.6  2007/12/23 14:14:25  fplanque
- * Enhanced widget name display
- *
- * Revision 1.5  2007/12/22 19:55:00  yabs
- * cleanup from adding core params
- *
- * Revision 1.3  2007/12/20 10:48:50  fplanque
- * doc
- *
- * Revision 1.2  2007/12/18 10:27:30  yabs
- * adding params
- *
- * Revision 1.1  2007/06/25 11:02:26  fplanque
- * MODULES (refactored MVC)
- *
- * Revision 1.1  2007/06/21 00:44:37  fplanque
- * linkblog now a widget
- *
- * Revision 1.2  2007/06/20 21:42:13  fplanque
- * implemented working widget/plugin params
- *
- * Revision 1.1  2007/06/18 21:25:47  fplanque
- * one class per core widget
- *
  */
 ?>
