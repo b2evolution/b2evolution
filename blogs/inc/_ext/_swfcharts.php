3b0801f3a32dcf8580c077891b0de8f60f5831d2
Tilman Blumenbach <tilman+git@ax86.net>
2009-03-16 15:58:26 +0000

    Code cleanup (if this should violate the "PHP/SWF charts" license, please revert...)

--- blogs/inc/_ext/_swfcharts.php
+++ blogs/inc/_ext/_swfcharts.php
@@ -9,106 +9,145 @@
  * PHP/SWF Charts is a shareware. See http://www.maani.us/charts/ for
  * more information.
  * ------------------------------------------------------------------------
+ * @version $Id$
  * @package libs
  */
-if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
-
+if( ! defined( 'EVO_MAIN_INIT' ) ) die( 'Please, do not access this page directly.' );
 
 /**
- * This function draws the SWF chart.
+ * Draw the SWF chart.
  *
- * @param array
+ * @param array Chart data
  */
-function DrawChart($chart)
+function DrawChart( $chart )
 {
-	// fplanque:
+	// by fplanque:
 	global $rsc_url;
 	$path = $rsc_url;
 
-	//defaults
-	if(!isset($chart[ 'canvas_bg' ]['width' ])){$chart[ 'canvas_bg' ]['width' ] =400;}
-	if(!isset($chart[ 'canvas_bg' ]['height' ])){$chart[ 'canvas_bg' ]['height' ] =250;}
-	if(!isset($chart[ 'canvas_bg' ]['color' ])){$chart[ 'canvas_bg' ]['color' ] = "666666";}
-
-	$params="";
-	$allKeys= array_keys($chart);
-	for ($i=0;$i<count($allKeys);$i++)
+	// defaults:
+	if ( ! isset( $chart['canvas_bg']['width'] ) )
 	{
-		switch($allKeys[$i]){
-			case "chart_data":
-			$params=$params."rows=".count($chart[ 'chart_data' ])."&";
-			$params=$params."cols=".count($chart[ 'chart_data' ][0])."&";
-			for ($r=0;$r<count($chart[ 'chart_data' ]);$r++){
-				$params=$params."r".$r."=";
-				for ($c=0;$c<count($chart[ 'chart_data' ][$r]);$c++)
+		$chart['canvas_bg']['width'] = 400;
+	}
+	if ( ! isset( $chart['canvas_bg']['height'] ) )
+	{
+		$chart['canvas_bg']['height'] = 250;
+	}
+	if ( ! isset( $chart['canvas_bg']['color'] ) )
+	{
+		$chart['canvas_bg']['color'] = '666666';
+	}
+
+	$params  = '';
+	foreach ( $chart as $k => $v )
+	{
+		$count = is_array( $v ) ? count( $v ) : 0;
+		switch( $k )
+		{
+			case 'chart_data':
+				$params .= 'rows='.$count.'&'
+						  .'cols='.count( $v[0] ).'&';
+				for ( $r = 0; $r < $count; ++$r )
 				{
-					$params=$params.$chart[ 'chart_data' ][$r][$c];
-					if($c==count($chart[ 'chart_data' ][$r])-1){$params=$params."&";}
-					else{$params=$params.";";}
+					$params .= 'r'.$r.'='.implode( ';', $v[$r] ).'&';
 				}
-			}
-			break;
+				break;
 
-			case "draw_text":
-			for ($r=0;$r<count($chart[ 'draw_text' ]);$r++){
-				$params=$params."text_".$r."=";
-				$allKeys2= array_keys($chart[ 'draw_text' ][$r]);
-				for ($k2=0;$k2<count($allKeys2);$k2++){
-					$params=$params.$allKeys2[$k2].":".$chart[ 'draw_text' ][$r][$allKeys2[$k2]];
-					if($k2<count($allKeys2)-1){$params=$params.";";}
+			case 'draw_text':
+				for ( $r = 0; $r < $count; ++$r )
+				{
+					$params .= 'text_'.$r.'=';
+					$first   = true;
+					foreach ( $v[$r] as $tk => $tv )
+					{
+						if ( $first )
+						{
+							$first = false;
+						}
+						else
+						{
+							$params .= ';';
+						}
+						$params .= $tk.':'.$tv;
+					}
+					$params .= '&';
 				}
-				$params=$params."&";
-			}
-			break;
+				break;
 
-			case "link":
-			for ($r=0;$r<count($chart[ 'link' ]);$r++){
-				$params=$params."link_".$r."=";
-				$allKeys2= array_keys($chart[ 'link' ][$r]);
-				for ($k2=0;$k2<count($allKeys2);$k2++){
-					$params=$params.$allKeys2[$k2].":".$chart[ 'link' ][$r][$allKeys2[$k2]];
-					if($k2<count($allKeys2)-1){$params=$params.";";}
+			case 'link':
+				for ( $r = 0; $r < $count; ++$r )
+				{
+					$params .= 'link_'.$r.'=';
+					$first   = true;
+					foreach ( $v as $lk => $lt )
+					{
+						if ( $first )
+						{
+							$first = false;
+						}
+						else
+						{
+							$params .= ';';
+						}
+						$params .= $lk.':'.$lt;
+					}
+					$params .= '&';
 				}
-				$params=$params."&";
-			}
-			break;
+				break;
 
 			default:
-			if(gettype($chart[$allKeys[$i]])=="array" ){
-				$params=$params.$allKeys[$i]."=";
-				$allKeys2= array_keys($chart[$allKeys[$i]]);
-				for ($k2=0;$k2<count($allKeys2);$k2++){
-					$params=$params.$allKeys2[$k2].":".$chart[$allKeys[$i]][$allKeys2[$k2]];
-					if($k2<count($allKeys2)-1){$params=$params.";";}
+				if ( is_array( $v ) )
+				{
+					$params .= $k.'=';
+					$first   = true;
+					foreach( $v as $dk => $dv )
+					{
+						if ( $first )
+						{
+							$first = false;
+						}
+						else
+						{
+							$params .= ';';
+						}
+						$params .= $dk.':'.$dv;
+					}
+					$params .= '&';
 				}
-				$params=$params."&";
-			}else{
-				$params=$params.$allKeys[$i]."=".$chart[$allKeys[$i]]."&";
-			}
+				else
+				{
+					$params .= $k.'='.$v.'&';
+				}
+				break;
 		}
 	}
 ?>
 <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"
-				codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0"
-				width="<?php print $chart[ 'canvas_bg' ]['width' ]; ?>"
-				height="<?php print $chart[ 'canvas_bg' ]['height' ]; ?>"
-				id="charts">
-
-	<PARAM NAME="movie" VALUE="<?php print $path."charts.swf"; ?>?<?php print $params; ?>" />
+		codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0"
+		width="<?php echo $chart['canvas_bg']['width']; ?>"
+		height="<?php echo $chart['canvas_bg']['height']; ?>"
+		id="charts">
+	<PARAM NAME="movie" VALUE="<?php echo $path.'charts.swf'; ?>?<?php echo $params; ?>" />
 	<PARAM NAME="quality" VALUE="high" />
-	<PARAM NAME="bgcolor" VALUE="<?php print $chart[ 'canvas_bg' ]['color' ]; ?>" />
+	<PARAM NAME="bgcolor" VALUE="<?php echo $chart['canvas_bg']['color']; ?>" />
 
-	<EMBED src="<?php print $path."charts.swf"; ?>?<?php print $params; ?>"
-				quality="high"
-				NAME="charts"
-				TYPE="application/x-shockwave-flash"
-				PLUGINSPAGE="http://www.macromedia.com/go/getflashplayer"
-				bgcolor="<?php print $chart[ 'canvas_bg' ]['color' ]; ?>"
-				WIDTH="<?php print $chart[ 'canvas_bg' ]['width' ]; ?>"
-				HEIGHT="<?php print $chart[ 'canvas_bg' ]['height' ]; ?>"></EMBED>
-
+	<EMBED src="<?php echo $path.'charts.swf'; ?>?<?php echo $params; ?>"
+			quality="high"
+			NAME="charts"
+			TYPE="application/x-shockwave-flash"
+			PLUGINSPAGE="http://www.macromedia.com/go/getflashplayer"
+			bgcolor="<?php echo $chart['canvas_bg']['color']; ?>"
+			WIDTH="<?php echo $chart['canvas_bg']['width']; ?>"
+			HEIGHT="<?php echo $chart['canvas_bg']['height']; ?>"></EMBED>
 </object>
-
 <?php
 }
+
+/*
+ * $Log$
+ * Revision 1.2  2009/03/16 15:58:26  tblue246
+ * Code cleanup (if this should violate the "PHP/SWF charts" license, please revert...)
+ *
+ */
 ?>
