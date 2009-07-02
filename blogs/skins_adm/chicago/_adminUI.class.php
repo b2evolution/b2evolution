152822b3be066d8a944888088709dc1f71839f15
Francois Planque <github@fplanque.net>
2009-07-02 00:18:06 +0000

    no message

--- blogs/skins_adm/chicago/_adminUI.class.php
+++ blogs/skins_adm/chicago/_adminUI.class.php
@@ -291,7 +291,7 @@ class AdminUI extends AdminUI_general
 
 					'title_fmt' => '<div class="fieldset_title_bg" $title_attribs$><span style="float:right">$global_icons$</span>$title$</div></div></div><fieldset>'."\n",
 					'no_title_fmt' => '<div class="fieldset_title_bg" $title_attribs$><span style="float:right">$global_icons$</span>&nbsp;</div></div></div><fieldset>'."\n",
-					'fieldset_begin' => '<div class="fieldset_wrapper$class$" id="$ID$"><h2 $title_attribs$>$fieldset_title$</h2>',
+					'fieldset_begin' => '<div class="fieldset_wrapper$class$" id="$id$"><h2 $title_attribs$>$fieldset_title$</h2>',
 					'fieldset_end' => '</div>',
 					'fieldstart' => '<fieldset $ID$>'."\n",
 					'labelstart' => '<div class="label">',
@@ -315,7 +315,7 @@ class AdminUI extends AdminUI_general
 					'title_fmt' => '<span style="float:right">$global_icons$</span><h2>$title$</h2>'."\n",
 					'no_title_fmt' => '<span style="float:right">$global_icons$</span>'."\n",
 					'fieldstart' => '<fieldset $ID$>'."\n",
-					'fieldset_begin' => '<div class="fieldset_wrapper$class$" id="$ID$"><div class="fieldset_title"><div class="fieldset_title_right">
+					'fieldset_begin' => '<div class="fieldset_wrapper$class$" id="$id$"><div class="fieldset_title"><div class="fieldset_title_right">
 																<div class="fieldset_title_bg" $title_attribs$>$fieldset_title$</div></div></div>
 																<fieldset $fieldset_attribs$>'."\n",
 					'fieldset_end' => '</fieldset></div>'."\n",
@@ -382,6 +382,9 @@ class AdminUI extends AdminUI_general
 
 /*
  * $Log$
+ * Revision 1.22  2009/07/02 00:18:06  fplanque
+ * no message
+ *
  * Revision 1.21  2009/06/09 11:59:55  yabs
  * bug fix
  *
