837431ea83f4802c114a3e51cf80b631ddefb946
Walter Cruz <walter.php@gmail.com>
2009-08-31 18:26:13 +0000

    adding wp.getPageList

--- blogs/inc/xmlrpc/apis/_wordpress.api.php
+++ blogs/inc/xmlrpc/apis/_wordpress.api.php
@@ -15,6 +15,89 @@
  */
 if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
 
+
+$wordpressgetpagelist_doc = 'Get an array of all the pages on a blog. Just the minimum details, lighter than wp.getPages. ';
+$wordpressgetpagelist_sig =  array(array($xmlrpcArray,$xmlrpcString,$xmlrpcString,$xmlrpcString));
+/**
+ * metaWeblog.getRecentPosts
+ *
+ * @see http://www.xmlrpc.com/metaWeblogApi#metawebloggetrecentposts
+ *
+ * @param xmlrpcmsg XML-RPC Message
+ *					0 blogid (string): Unique identifier of the blog the post will be added to.
+ *						Currently ignored in b2evo, in favor of the category.
+ *					1 username (string): Login for a Blogger user who has permission to edit the given
+ *						post (either the user who originally created it or an admin of the blog).
+ *					2 password (string): Password for said username.
+ */
+function wp_getpagelist( $m )
+{
+	// CHECK LOGIN:
+	/**
+	 * @var User
+	 */
+	if( ! $current_User = & xmlrpcs_login( $m, 1, 2 ) )
+	{	// Login failed, return (last) error:
+		return xmlrpcs_resperror();
+	}
+
+	// GET BLOG:
+	/**
+	 * @var Blog
+	 */
+	if( ! $Blog = & xmlrpcs_get_Blog( $m, 0 ) )
+	{	// Login failed, return (last) error:
+		return xmlrpcs_resperror();
+	}
+
+	// Get the pages to display:
+	load_class( 'items/model/_itemlistlight.class.php' );
+	$MainList = & new ItemListLight( $Blog, NULL, NULL,  NULL );
+
+	// Protected and private get checked by statuses_where_clause().
+	$statuses = array( 'published', 'redirected', 'protected', 'private' );
+	if( $current_User->check_perm( 'blog_ismember', 'view', false, $Blog->ID ) )
+	{	// These statuses require member status:
+		$statuses = array_merge( $statuses, array( 'draft', 'deprecated' ) );
+	}
+	logIO( 'Statuses: '.implode( ', ', $statuses ) );
+
+	$MainList->set_filters( array(
+			'visibility_array' => $statuses,
+			'order' => 'DESC',
+			'unit' => 'posts',
+			'types' => '1000',
+		) );
+	// Run the query:
+	$MainList->query();
+
+	logIO( 'Items:'.$MainList->result_num_rows );
+
+	$data = array();
+	/**
+	 * @var Item
+	 */
+	while( $Item = & $MainList->get_item() )
+	{
+		logIO( 'Item:'.$Item->title.
+					' - Issued: '.$Item->issue_date.
+					' - Modified: '.$Item->mod_date );
+		$post_date = mysql2date('U', $Item->issue_date);
+		$post_date = gmdate('Ymd', $post_date).'T'.gmdate('H:i:s', $post_date);
+		$data[] = new xmlrpcval(array(
+				'dateCreated' => new xmlrpcval($post_date,'dateTime.iso8601'),
+				'page_id' => new xmlrpcval($Item->ID),
+				'page_title' => new xmlrpcval($Item->title),
+				'page_parent_id ' => new xmlrpcval(0),
+			),'struct');
+	}
+
+	logIO( 'OK.' );
+	return new xmlrpcresp( new xmlrpcval( $data, 'array' ) );
+}
+
+
+
 // Wordpress has some aliases to metaweblog APIS.
 
 $xmlrpc_procs['wp.getCategories'] = array(
@@ -26,5 +109,10 @@ $xmlrpc_procs['wp.uploadFile '] = array(
 				'function' => 'mw_newmediaobject',
 				'signature' => $mwnewMediaObject_sig,
 				'docstring' => $mwnewMediaObject_doc);
+
+$xmlrpc_procs['wp.getPageList'] = array(
+				'function' => 'wp_getpagelist',
+				'signature' => $wordpressgetpagelist_sig,
+				'docstring' => $wordpressgetpagelist_doc);
 
 ?>
