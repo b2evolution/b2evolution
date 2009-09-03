17a8f394fc8f21cc5720a726ab69548b7ef08a74
Francois Planque <github@fplanque.net>
2009-09-03 23:37:54 +0000

    whitespace

--- blogs/conf/_stats.php
+++ blogs/conf/_stats.php
@@ -150,7 +150,7 @@ $search_engines = array(
 	'yandex.ru/yandsearch',
 	'go.mail.ru/search',
 	'//www.bing.com', //q=
-	'//cc.bingj.com', // q= 
+	'//cc.bingj.com', // q=
 	'.qip.ru/', // query=
 );
 
