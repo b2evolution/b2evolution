750fed687cbba3d0d8e243497ad963113a4dd58e
Alex <support@thumbshots.ru>
2009-10-02 01:37:43 +0000

    minor

--- blogs/inc/_core/_class5.funcs.php
+++ blogs/inc/_core/_class5.funcs.php
@@ -83,7 +83,7 @@ else
  * It only registers the class & file name so that __autoload() can later
  * load the class IF and ONLY IF the class is actually needed during execution.
  */
-function load_class( $class_path, $classname )
+function load_class( $class_path, $classname = NULL )
 {
 	global $map_class_path, $inc_path;
 	if( !is_null($classname) )
@@ -106,6 +106,9 @@ function duplicate( $Obj )
 
 /*
  * $Log$
+ * Revision 1.26  2009/10/02 01:37:43  sam2kb
+ * minor
+ *
  * Revision 1.25  2009/09/18 15:47:11  fplanque
  * doc/cleanup
  *
