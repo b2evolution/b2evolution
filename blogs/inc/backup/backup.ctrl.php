8fdca97b9d54ca00b272d2f0dac884ebabb1f930
Francois Planque <github@fplanque.net>
2009-10-18 17:26:26 +0000

    doc

--- blogs/inc/backup/backup.ctrl.php
+++ blogs/inc/backup/backup.ctrl.php
@@ -1,6 +1,28 @@
 <?php
 /**
  * Backup - This is a LINEAR controller
+ *
+ * This file is part of b2evolution - {@link http://b2evolution.net/}
+ * See also {@link http://sourceforge.net/projects/evocms/}.
+ *
+ * @copyright (c)2009 by Francois PLANQUE - {@link http://fplanque.net/}
+ * Parts of this file are copyright (c)2009 by The Evo Factory - {@link http://www.evofactory.com/}.
+ *
+ * Released under GNU GPL License - {@link http://b2evolution.net/about/license.html}
+ *
+ * {@internal Open Source relicensing agreement:
+ * The Evo Factory grants Francois PLANQUE the right to license
+ * The Evo Factory's contributions to this file and the b2evolution project
+ * under any OSI approved OSS license (http://www.opensource.org/licenses/).
+ * }}
+ *
+ * @package maintenance
+ *
+ * {@internal Below is a list of authors who have contributed to design/coding of this file: }}
+ * @author efy-maxim: Evo Factory / Maxim.
+ * @author fplanque: Francois Planque.
+ *
+ * @version $Id$
  */
 
 if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
@@ -66,6 +88,9 @@ $AdminUI->disp_global_footer();
 
 /*
  * $Log$
+ * Revision 1.6  2009/10/18 17:26:26  fplanque
+ * doc
+ *
  * Revision 1.5  2009/10/18 17:20:58  fplanque
  * doc/messages/minor refact
  *
--- blogs/inc/upgrade/model/_updater.class.php
+++ blogs/inc/upgrade/model/_updater.class.php
@@ -1,4 +1,27 @@
 <?php
+/**
+ * This file is part of b2evolution - {@link http://b2evolution.net/}
+ * See also {@link http://sourceforge.net/projects/evocms/}.
+ *
+ * @copyright (c)2009 by Francois PLANQUE - {@link http://fplanque.net/}
+ * Parts of this file are copyright (c)2009 by The Evo Factory - {@link http://www.evofactory.com/}.
+ *
+ * Released under GNU GPL License - {@link http://b2evolution.net/about/license.html}
+ *
+ * {@internal Open Source relicensing agreement:
+ * The Evo Factory grants Francois PLANQUE the right to license
+ * The Evo Factory's contributions to this file and the b2evolution project
+ * under any OSI approved OSS license (http://www.opensource.org/licenses/).
+ * }}
+ *
+ * @package maintenance
+ *
+ * {@internal Below is a list of authors who have contributed to design/coding of this file: }}
+ * @author efy-maxim: Evo Factory / Maxim.
+ * @author fplanque: Francois Planque.
+ *
+ * @version $Id$
+ */
 
 if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
 
@@ -51,6 +74,9 @@ class Updater
 
 /*
  * $Log$
+ * Revision 1.4  2009/10/18 17:26:26  fplanque
+ * doc
+ *
  * Revision 1.3  2009/10/18 08:16:55  efy-maxim
  * log
  *
