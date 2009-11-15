1c9f277fe1badb831f08fd7a6cddc9ab36ac90a5
Francois Planque <github@fplanque.net>
2009-11-15 22:48:03 +0000

    version number

--- blogs/conf/_application.php
+++ blogs/conf/_application.php
@@ -15,7 +15,7 @@ $app_shortname = 'b2evo';
  * Note: This has to be compatible with {@link http://us2.php.net/en/version-compare}.
  * @global string
  */
-$app_version = '4.0.0-alpha';
+$app_version = '4.0.0-dev';
 
 /**
  * Release date (ISO)
