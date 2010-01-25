5647bbd3ab9a01b6cb88efb2a7b034daeb063c10
efy-eugene <efy-eugene@users.sourceforge.net>
2010-01-25 17:38:36 +0000

    Convert this skin WP

--- blogs/skins/colourise/_body_header.inc.php
+++ blogs/skins/colourise/_body_header.inc.php
@@ -1,3 +1,65 @@
-<!-- wrap starts here -->
+<?php 
+/**
+ * This is the BODY header include template.
+ * 
+ */
+?>
 <div id="wrap">
-
+	<div id="header" class="pageHeader">
+	
+		<?php
+			// Display container and contents:
+			skin_container( NT_('Page Top'), array(
+					// The following params will be used as defaults for widgets included in this container:
+					'block_start' => '<div class="$wi_class$">',
+					'block_end' => '</div>',
+					'block_display_title' => false,
+					'list_start' => '<ul>',
+					'list_end' => '</ul>',
+					'item_start' => '<li>',
+					'item_end' => '</li>',
+				) );
+		?>
+			
+		<?php
+			// ------------------------- "Header" CONTAINER EMBEDDED HERE --------------------------
+			// Display container and contents:
+			skin_container( NT_('Header'), array(
+					// The following params will be used as defaults for widgets included in this container:
+					'block_start'       => '<div class="$wi_class$">',
+					'block_end'         => '</div>',
+					'block_title_start' => '<h1>',
+					'block_title_end'   => '</h1>',
+				) );
+			// ----------------------------- END OF "Header" CONTAINER -----------------------------
+		?>
+		
+		<div id="nav">
+			<ul>
+			<?php
+				// ------------------------- "Menu" CONTAINER EMBEDDED HERE --------------------------
+				// Display container and contents:
+				skin_container( NT_('Menu'), array(
+						// The following params will be used as defaults for widgets included in this container:
+						'block_start' => '',
+						'block_end' => '',
+						'block_display_title' => false,
+						'list_start' => '',
+						'list_end' => '',
+						'item_start' => '<li>',
+						'item_end' => '</li>',
+					) );
+				// ----------------------------- END OF "Menu" CONTAINER -----------------------------
+			?>
+			</ul>
+		</div>
+		
+		<form action="<?php $Blog->gen_blogurl() ?>" method="get" class="search" id="quick-search">
+			<p>
+			<label for="qsearch">Search:</label>
+			<input type="text" onblur="if (this.value == '') {this.value = 'Search...';}" onfocus="if (this.value == 'Search...') {this.value = '';}" value="Search..." name="s" id="qsearch" class="tbox"/>
+			<input type="submit" value="Submit" class="btn"/>
+			</p>
+		</form>
+		
+	</div>
--- blogs/skins/colourise/index.main.php
+++ blogs/skins/colourise/index.main.php
@@ -18,44 +18,30 @@ skin_include( '_body_header.inc.php' );
 // /skins/_body_footer.inc.php file into the current skin folder.
 // ------------------------------- END OF FOOTER --------------------------------
 ?>
-<div id="header">
-	
-	<h1 id="logo-text"><a href="http://demo.themelab.com/">Theme Lab Demo Server</a></h1>
-	<p id="intro">Just another WordPress theme site</p>
-	
-	<div id="nav">
-		<ul>
-		<?php
-			// ------------------------- "Menu" CONTAINER EMBEDDED HERE --------------------------
-			// Display container and contents:
-			skin_container( NT_('Menu'), array(
-					// The following params will be used as defaults for widgets included in this container:
-					'block_start' => '',
-					'block_end' => '',
-					'block_display_title' => false,
-					'list_start' => '',
-					'list_end' => '',
-					'item_start' => '<li>',
-					'item_end' => '</li>',
-				) );
-			// ----------------------------- END OF "Menu" CONTAINER -----------------------------
-		?>
-		</ul>
-	</div>
-	
-	<form action="<?php $Blog->gen_blogurl() ?>" method="get" class="search" id="quick-search">
-		<p>
-		<label for="qsearch">Search:</label>
-		<input type="text" onblur="if (this.value == '') {this.value = 'Search...';}" onfocus="if (this.value == 'Search...') {this.value = '';}" value="Search..." name="s" id="qsearch" class="tbox"/>
-		<input type="submit" value="Submit" class="btn"/>
-		</p>
-	</form>
-	
-</div>
 
 <div id="content-wrap">
-
 	<div id="main">
+		<?php
+			// ------------------------- MESSAGES GENERATED FROM ACTIONS -------------------------
+			messages( array(
+				'block_start' => '<div class="action_messages">',
+				'block_end'   => '</div>',
+			) );
+			// --------------------------------- END OF MESSAGES ---------------------------------
+		?>
+	
+		<?php
+			// ------------------------- TITLE FOR THE CURRENT REQUEST -------------------------
+			request_title( array(
+					'title_before'=> '<h2>',
+					'title_after' => '</h2>',
+					'title_none'  => '',
+					'glue'        => ' - ',
+					'title_single_disp' => true,
+					'format'      => 'htmlbody',
+				) );
+			// ------------------------------ END OF REQUEST TITLE -----------------------------
+		?>
 		<?php
 		// -------------- MAIN CONTENT TEMPLATE INCLUDED HERE (Based on $disp) --------------
 		skin_include( '$disp$', array(
