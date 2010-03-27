1c1e011d160ad505b7cff03f8cfaf5e5524d4d0b
Daniel Hahler <git+b2evo@thequod.de>
2010-03-27 19:42:04 +0000

    Add get_default_font and use it in settings note.

--- blogs/plugins/watermark_plugin/_watermark.plugin.php
+++ blogs/plugins/watermark_plugin/_watermark.plugin.php
@@ -48,7 +48,7 @@ class watermark_plugin extends Plugin
 	function GetDefaultSettings( & $params )
 	{
 		global $ReqHost;
-		
+
 		return array(
 			'text' => array(
 					'label' => 'Image text',
@@ -60,7 +60,8 @@ class watermark_plugin extends Plugin
 					'label' => 'Font file name',
 					'size' => 30,
 					'defaultvalue' => '',
-					'note' => sprintf(T_('You can upload your own fonts to the font directory (%s).'), rel_path_to_base($this->fonts_dir)),
+					'note' => sprintf(T_('You can upload your own fonts to the plugin font directory (%s), then use the filename here. By default "%s" is used.'),
+						rel_path_to_base($this->fonts_dir), rel_path_to_base($this->get_default_font())),
 				),
 			'font_size' => array(
 					'label' => 'Font size',
@@ -71,14 +72,14 @@ class watermark_plugin extends Plugin
 				),
 			);
 	}
-	
-	
+
+
 	function PluginSettingsUpdateAction()
 	{
 		global $media_path, $Settings;
-		
+
 		$font = $this->Settings->get('font');
-		
+
 		if( !empty($font) && !is_readable($this->fonts_dir.'/'.$font) )
 		{
 			$this->msg( sprintf( T_('Unable to load font file: %s'), $this->fonts_dir.'/'.$font ), 'error' );
@@ -99,8 +100,8 @@ class watermark_plugin extends Plugin
 			}
 		}
 	}
-	
-	
+
+
 	function BeforeEnable()
 	{
 		if( !function_exists('imagettftext') )
@@ -110,8 +111,20 @@ class watermark_plugin extends Plugin
 
 		return true;
 	}
-	
-	
+
+
+	/**
+	 * Get hardcoded default font.
+	 * @return string
+	 */
+	function get_default_font()
+	{
+		global $rsc_path;
+		return $rsc_path.'fonts/LiberationSans-Regular.ttf';
+	}
+
+
+
 	/**
 	 * This gets called before an image thumbnail gets created.
 	 *
@@ -126,72 +139,70 @@ class watermark_plugin extends Plugin
 	 */
 	function BeforeThumbCreate( & $params )
 	{
-		global $rsc_path;
-		
 		if( !function_exists('imagettftext') )
 		{	// The function imagettftext() is not available.
 			return;
 		}
-		
+
 		// Canvas width & height
 		$width = imagesx( $params['imh'] ) ;
 		$height = imagesy( $params['imh'] );
-		
+
 		if( $width < 161 || $height < 161 )
 		{	// Skip small thumbnails
 			return;
 		}
-		
+
 		if( $font = $this->Settings->get('font') )
 		{	// Custom font
 			$font = $this->fonts_dir.'/'.$font;
 		}
 		else
-		{	// Default font
-			$font = $rsc_path.'fonts/LiberationSans-Regular.ttf';
+		{
+			$font = $this->get_default_font();
 		}
-		
+
 		if( !is_readable($font) )
 		{	// Font file not found
-			// TODO: debuglog
+			$this->debug_log( sprintf('Font file (%s) is not readable!', $font) );
 			return;
 		}
-		
+
 		$text = html_entity_decode($this->Settings->get('text'));
 		$font_size = $this->Settings->get('font_size');
-		
+
 		// Text margins
 		$margin_left = 10;
 		$margin_bottom = 10;
-		
+
 		// Create a transparent image
 		$im = imagecreatetruecolor($width, $height);
 		imagealphablending($im, true);
 		imagefill($im, 0, 0, imagecolortransparent($im, imagecolorallocatealpha($im, 0, 0, 0, 127)));
 		imagesavealpha($im, true);
-		
+
 		// Create some colors
 		$white = imagecolorallocate($im, 255, 255, 255);
 		$light_grey = imagecolorallocate($im, 230, 230, 230);
 		$grey = imagecolorallocate($im, 60, 60, 60);
 		$black = imagecolorallocate($im, 0, 0, 0);
-		
+
 		// Add text shadow
 		imagettftext($im, $font_size, 0, $margin_left + 2, $height - $margin_bottom, $grey, $font, $text);
 
 		// Add text
 		imagettftext($im, $font_size, 0, $margin_left, $height - $margin_bottom, $light_grey, $font, $text);
-		
+
 		// Merge images
 		imagecopyresampled($params['imh'], $im, 0, 0, 0, 0, $width, $height, $width, $height);
-		
+
 		// Low quality :(
 		//imagecopymerge($params['imh'], $im, 0, 0, 0, 0, $width, $height, 90);
-		
+
 		return true;
 	}
-	
-	
+
+
 	/**
 	 * Event handler: Called before an uploaded file gets saved on server.
 	 *
