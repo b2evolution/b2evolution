083d256f5cc2c0de11bf01767450ec7ca5c3b386
Daniel Hahler <git+b2evo@thequod.de>
2010-04-12 21:24:34 +0000

    Silence header line on error display/view.

--- blogs/skins_adm/conf_error.main.php
+++ blogs/skins_adm/conf_error.main.php
@@ -15,7 +15,7 @@ if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.'
 global $app_name;
 global $app_version;
 
-header('Content-Type: text/html; charset=iso-8859-1');
+@header('Content-Type: text/html; charset=iso-8859-1');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
