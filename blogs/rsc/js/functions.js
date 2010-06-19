8e90d5da30671789f3f405f6a07925ae8dbb47b4
Daniel Hahler <git+b2evo@thequod.de>
2010-06-19 01:11:07 +0000

    Remove obsolete and clunky disableEnterKey function, which was used wrongly in Form::username as dirty workaround for a flaw with jquery.hintbox.

--- blogs/rsc/js/functions.js
+++ blogs/rsc/js/functions.js
@@ -503,22 +503,3 @@ function evoFadeBg( selector, bgs, options )
 
 	eval(toEval);
 }
-
-
-/**
- * Disable Enter Key
- */
-function disableEnterKey(e)
-{
-     var key;
-
-     if(window.event)
-          key = window.event.keyCode;     //IE
-     else
-          key = e.which;     //firefox
-
-     if(key == 13)
-          return false;
-     else
-          return true;
-}
