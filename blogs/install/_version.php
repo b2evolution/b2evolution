15ef4b5346b880e14a245815794eb6eddcdcb180
Francois Planque <github@fplanque.net>
2011-03-12 00:15:10 +0000

    bump

--- blogs/install/_version.php
+++ blogs/install/_version.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * This system parameter is used to compare current version of application
+ * This system parameter is used to compare current version of the application
  * with version of available upgrade package.
  *
  * Please, don't change it manually!
  */
-$current_version = 1;
+$current_version = 100;		// 4.1.0
 
 ?>
