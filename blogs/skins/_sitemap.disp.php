c02ff14dc48fa801b30df7c4445967606288cec9
Alex <support@thumbshots.ru>
2011-08-03 15:03:33 +0000

    Display all posts and pages

--- blogs/skins/_sitemap.disp.php
+++ blogs/skins/_sitemap.disp.php
@@ -17,6 +17,7 @@ if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.'
 
 // Note: this is a very imperfect sitemap, but it's a start :)
 
+echo '<h3>'.T_('Common links').'</h3>';
 // --------------------------------- START OF COMMON LINKS --------------------------------
 skin_widget( array(
 		// CODE for the widget:
@@ -38,6 +39,7 @@ skin_widget( array(
 // ---------------------------------- END OF COMMON LINKS ---------------------------------
 
 
+echo '<h3>'.T_('Pages').'</h3>';
 // --------------------------------- START OF PAGE LIST --------------------------------
 skin_widget( array(
 		// CODE for the widget:
@@ -48,10 +50,12 @@ skin_widget( array(
 		'block_display_title' => false,
 		'order_by' => 'title',
 		'order_dir' => 'ASC',
+		'limit' => NULL,
 	) );
 // ---------------------------------- END OF PAGE LIST ---------------------------------
 
 
+echo '<h3>'.T_('Categories').'</h3>';
 // --------------------------------- START OF CATEGORY LIST --------------------------------
 skin_widget( array(
 		// CODE for the widget:
@@ -64,6 +68,7 @@ skin_widget( array(
 // ---------------------------------- END OF CATEGORY LIST ---------------------------------
 
 
+echo '<h3>'.T_('Posts').'</h3>';
 // --------------------------------- START OF POST LIST --------------------------------
 skin_widget( array(
 		// CODE for the widget:
@@ -74,12 +79,16 @@ skin_widget( array(
 		'block_display_title' => false,
 		'order_by' => 'title',
 		'order_dir' => 'ASC',
+		'limit' => NULL,
 	) );
 // ---------------------------------- END OF POST LIST ---------------------------------
 
 
 /*
  * $Log$
+ * Revision 1.3  2011/08/03 15:03:33  sam2kb
+ * Display all posts and pages
+ *
  * Revision 1.2  2010/02/08 17:56:14  efy-yury
  * copyright 2009 -> 2010
  *
