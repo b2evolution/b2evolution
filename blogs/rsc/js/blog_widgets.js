50d13b62be72a9cab9ef47e4ed54f34e92b90d9c
Alex <support@thumbshots.ru>
2011-09-07 06:40:56 +0000

    Restore some delay on widgets edit, otherwise reorder does not work

--- blogs/rsc/js/blog_widgets.js
+++ blogs/rsc/js/blog_widgets.js
@@ -35,7 +35,7 @@ var reorder_widgets_queue;
  * @internal integer reorder_delay
  * time, in milliseconds, to buffer requests for
  */
-var reorder_delay = 0;
+var reorder_delay = 200;
 
 /**
  * @internal integer reorder_delay_remaining
