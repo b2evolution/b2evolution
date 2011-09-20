42e1baa9285c466c842f38ed848a63ecc231d489
Attila <kitsisimo@gmail.com>
2011-09-20 09:07:31 +0000

    Login problems with multidomain installs - fix

--- blogs/conf/_advanced.php
+++ blogs/conf/_advanced.php
@@ -328,7 +328,7 @@ $cookie_name    = 'cookie'.$instance_name.'name';
 $cookie_email   = 'cookie'.$instance_name.'email';
 $cookie_url     = 'cookie'.$instance_name.'url';
 // The following handles the session:
-$cookie_session = 'cookie'.$instance_name.'session';
+$cookie_session = str_replace( '.', '_', 'cookie'.$instance_name.'session'.$cookie_domain );
 /**#@-*/
 
 /**
