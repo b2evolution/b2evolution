57e7ae549d6807b9b9374ae9d7bfd2a1046f675f
Francois Planque <github@fplanque.net>
2011-10-05 21:13:45 +0000

    no message

--- blogs/inc/collections/views/_coll_features.form.php
+++ blogs/inc/collections/views/_coll_features.form.php
@@ -36,13 +36,41 @@ $Form->hidden( 'action', 'update' );
 $Form->hidden( 'tab', 'features' );
 $Form->hidden( 'blog', $edited_Blog->ID );
 
+$Form->begin_fieldset( T_('Post list') );
+  $Form->select_input_array( 'orderby', $edited_Blog->get_setting('orderby'), get_available_sort_options(), T_('Order by'), T_('Default ordering of posts.') );
+  $Form->select_input_array( 'orderdir', $edited_Blog->get_setting('orderdir'), array(
+                        'ASC'  => T_('Ascending'),
+                        'DESC' => T_('Descending'), ), T_('Direction') );
+  $Form->radio( 'what_to_show', $edited_Blog->get_setting('what_to_show'),
+                array(  array( 'days', T_('days') ),
+                        array( 'posts', T_('posts') ),
+                      ), T_('Display unit'), false,  T_('Do you want to restrict on the number of days or the number of posts?') );
+  $Form->text( 'posts_per_page', $edited_Blog->get_setting('posts_per_page'), 4, T_('Posts/Days per page'), T_('How many days or posts do you want to display on the home page?'), 4 );
+
+  $Form->radio( 'timestamp_min', $edited_Blog->get_setting('timestamp_min'),
+                array(  array( 'yes', T_('yes') ),
+                        array( 'no', T_('no') ),
+                        array( 'duration', T_('only the last') ),
+                      ), T_('Show past posts'), true );
+  $Form->duration_input( 'timestamp_min_duration', $edited_Blog->get_setting('timestamp_min_duration'), '' );
+
+  $Form->radio( 'timestamp_max', $edited_Blog->get_setting('timestamp_max'),
+                array(  array( 'yes', T_('yes') ),
+                        array( 'no', T_('no') ),
+                        array( 'duration', T_('only the next') ),
+                      ), T_('Show future posts'), true );
+  $Form->duration_input( 'timestamp_max_duration', $edited_Blog->get_setting('timestamp_max_duration'), '' );
+
+$Form->end_fieldset();
+
+
 $Form->begin_fieldset( T_('Post options').get_manual_link('blog_features_settings') );
 	$Form->radio( 'require_title', $edited_Blog->get_setting('require_title'),
 								array(  array( 'required', T_('Always'), T_('The blogger must provide a title') ),
 												array( 'optional', T_('Optional'), T_('The blogger can leave the title field empty') ),
 												array( 'none', T_('Never'), T_('No title field') ),
 											), T_('Post titles'), true );
-											
+
 	$Form->checkbox( 'enable_goto_blog', $edited_Blog->get_setting( 'enable_goto_blog' ),
 						T_( 'View blog after publishing' ), T_( 'Check this to automatically view the blog after publishing a post.' ) );
 
@@ -109,38 +137,6 @@ $Form->begin_fieldset( T_('Custom field names') );
 $Form->end_fieldset();
 
 
-$Form->begin_fieldset( T_('Post list') );
-	$Form->select_input_array( 'orderby', $edited_Blog->get_setting('orderby'), get_available_sort_options(), T_('Order by'), T_('Default ordering of posts.') );
-	$Form->select_input_array( 'orderdir', $edited_Blog->get_setting('orderdir'), array(
-												'ASC'  => T_('Ascending'),
-												'DESC' => T_('Descending'), ), T_('Direction') );
-	$Form->radio( 'what_to_show', $edited_Blog->get_setting('what_to_show'),
-								array(  array( 'days', T_('days') ),
-												array( 'posts', T_('posts') ),
-											), T_('Display unit'), false,  T_('Do you want to restrict on the number of days or the number of posts?') );
-	$Form->text( 'posts_per_page', $edited_Blog->get_setting('posts_per_page'), 4, T_('Posts/Days per page'), T_('How many days or posts do you want to display on the home page?'), 4 );
-$Form->end_fieldset();
-
-
-$Form->begin_fieldset( T_('Time frame') );
-
-	$Form->radio( 'timestamp_min', $edited_Blog->get_setting('timestamp_min'),
-								array(  array( 'yes', T_('yes') ),
-												array( 'no', T_('no') ),
-												array( 'duration', T_('only the last') ),
-											), T_('Show past posts'), true );
-	$Form->duration_input( 'timestamp_min_duration', $edited_Blog->get_setting('timestamp_min_duration'), '' );
-
-	$Form->radio( 'timestamp_max', $edited_Blog->get_setting('timestamp_max'),
-								array(  array( 'yes', T_('yes') ),
-												array( 'no', T_('no') ),
-												array( 'duration', T_('only the next') ),
-											), T_('Show future posts'), true );
-	$Form->duration_input( 'timestamp_max_duration', $edited_Blog->get_setting('timestamp_max_duration'), '' );
-
-$Form->end_fieldset();
-
-
 $Form->end_form( array(
 	array( 'submit', 'submit', T_('Save !'), 'SaveButton' ),
 	array( 'reset', '', T_('Reset'), 'ResetButton' ) ) );
@@ -151,6 +147,9 @@ $Form->end_form( array(
 
 /*
  * $Log$
+ * Revision 1.55  2011/10/05 21:13:45  fplanque
+ * no message
+ *
  * Revision 1.54  2011/10/05 13:49:07  efy-yurybakh
  * Add settings for a $timestamp_min & $timestamp_max
  *
@@ -198,135 +197,5 @@ $Form->end_form( array(
  *
  * Revision 1.39  2011/03/02 09:45:59  efy-asimo
  * Update collection features allow_comments, disable_comments_bypost, allow_attachments, allow_rating
- *
- * Revision 1.38  2010/11/03 19:44:14  sam2kb
- * Increased modularity - files_Module
- * Todo:
- * - split core functions from _file.funcs.php
- * - check mtimport.ctrl.php and wpimport.ctrl.php
- * - do not create demo Photoblog and posts with images (Blog A)
- *
- * Revision 1.37  2010/10/19 02:00:53  fplanque
- * MFB
- *
- * Revision 1.36  2010/10/13 14:07:55  efy-asimo
- * Optional paged comments in the front end
- *
- * Revision 1.35  2010/09/08 15:07:44  efy-asimo
- * manual links
- *
- * Revision 1.34  2010/07/26 06:52:16  efy-asimo
- * MFB v-4-0
- *
- * Revision 1.33  2010/07/13 23:54:50  sam2kb
- * type
- *
- * Revision 1.32  2010/07/06 08:17:39  efy-asimo
- * Move "Multiple authors" block to Blog setings advanced tab. Fix validating urlname when user has no blog_admin permission.
- *
- * Revision 1.31  2010/06/08 22:29:25  sam2kb
- * Per blog settings for different default gravatar types
- *
- * Revision 1.30  2010/06/08 01:49:53  sam2kb
- * Paged comments in frontend
- *
- * Revision 1.29  2010/05/22 12:22:49  efy-asimo
- * move $allow_cross_posting in the backoffice
- *
- * Revision 1.28  2010/02/26 22:15:47  fplanque
- * whitespace/doc/minor
- *
- * Revision 1.26  2010/02/08 17:52:09  efy-yury
- * copyright 2009 -> 2010
- *
- * Revision 1.25  2010/02/06 11:48:32  efy-yury
- * add checkbox 'go to blog after posting' in blog settings
- *
- * Revision 1.24  2010/01/30 18:55:21  blueyed
- * Fix "Assigning the return value of new by reference is deprecated" (PHP 5.3)
- *
- * Revision 1.23  2010/01/27 15:20:07  efy-asimo
- * Change select list to radio button
- *
- * Revision 1.22  2010/01/22 04:28:51  fplanque
- * fixes
- *
- * Revision 1.21  2010/01/20 20:08:31  efy-asimo
- * Countries&Currencies redirect fix + RSS/Atom feeds image size select list
- *
- * Revision 1.20  2010/01/03 13:45:36  fplanque
- * set some crumbs (needs checking)
- *
- * Revision 1.19  2009/10/04 23:06:30  fplanque
- * doc
- *
- * Revision 1.18  2009/09/29 16:56:12  tblue246
- * Added setting to disable sitemaps skins
- *
- * Revision 1.17  2009/09/26 12:00:42  tblue246
- * Minor/coding style
- *
- * Revision 1.16  2009/09/25 07:32:52  efy-cantor
- * replace get_cache to get_*cache
- *
- * Revision 1.15  2009/08/31 17:21:32  fplanque
- * minor
- *
- * Revision 1.14  2009/08/27 12:24:27  tblue246
- * Added blog setting to display comments in ascending/descending order
- *
- * Revision 1.13  2009/03/08 23:57:42  fplanque
- * 2009
- *
- * Revision 1.12  2008/07/01 08:32:12  fplanque
- * minor
- *
- * Revision 1.11  2008/06/30 23:47:04  blueyed
- * require_title setting for Blogs, defaulting to 'required'. This makes the title field now a requirement (by default), since it often gets forgotten when posting first (and then the urltitle is ugly already)
- *
- * Revision 1.10  2008/04/03 22:03:06  fplanque
- * added "save & edit" and "publish now" buttons to edit screen.
- *
- * Revision 1.9  2008/02/09 20:14:14  fplanque
- * custom fields management
- *
- * Revision 1.8  2008/01/21 09:35:27  fplanque
- * (c) 2008
- *
- * Revision 1.7  2008/01/17 18:10:11  fplanque
- * deprecated linkblog_ID blog param
- *
- * Revision 1.6  2008/01/15 08:19:40  fplanque
- * blog footer text tag
- *
- * Revision 1.5  2008/01/10 19:59:51  fplanque
- * reduced comment PITA
- *
- * Revision 1.4  2007/12/26 17:21:17  fplanque
- * Anne's pony about full posts in RSS
- *
- * Revision 1.3  2007/12/18 23:50:40  fplanque
- * minor
- *
- * Revision 1.2  2007/11/02 01:49:16  fplanque
- * comment ratings
- *
- * Revision 1.1  2007/06/25 10:59:35  fplanque
- * MODULES (refactored MVC)
- *
- * Revision 1.4  2007/05/31 03:02:23  fplanque
- * Advanced perms now disabled by default (simpler interface).
- * Except when upgrading.
- * Enable advanced perms in blog settings -> features
- *
- * Revision 1.3  2007/05/13 22:53:31  fplanque
- * allow feeds restricted to post excerpts
- *
- * Revision 1.2  2007/04/26 00:11:05  fplanque
- * (c) 2007
- *
- * Revision 1.1  2006/12/16 01:30:47  fplanque
- * Setting to allow/disable email subscriptions on a per blog basis
- *
  */
 ?>
