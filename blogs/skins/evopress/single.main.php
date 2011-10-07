144ac7cb0d2a2a7b7aa6e7658fb57c39497b3302
Vitaliy <vitalij.dev@gmail.com>
2011-10-07 14:56:41 +0000

    change container

--- blogs/skins/evopress/single.main.php
+++ blogs/skins/evopress/single.main.php
@@ -124,7 +124,7 @@ while( $Item = & mainlist_get_item() )
       // ------------------------- "Item - Single" CONTAINER EMBEDDED HERE --------------------------
       // WARNING: EXPERIMENTAL -- NOT RECOMMENDED FOR PRODUCTION -- MAY CHANGE DRAMATICALLY BEFORE RELEASE.
       // Display container contents:
-      skin_container( NT_('Item - Single'), array(
+      skin_container( NT_('Item Single'), array(
           // The following (optional) params will be used as defaults for widgets included in this container:
           // This will enclose each widget in a block:
           'block_start' => '<div class="$wi_class$">',
