11431a08afd5362d0ea1a99eb708338ba4c45ce0
Attila <kitsisimo@gmail.com>
2011-10-17 12:14:14 +0000

    User main dispaly - update

--- blogs/skins/user.main.php
+++ blogs/skins/user.main.php
@@ -12,7 +12,7 @@
  */
 if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
 
-global $htsrv_url, $Messages;
+global $Messages;
 
 if( ! is_logged_in() && ! $Settings->get( 'allow_anonymous_user_profiles' ) )
 {	// Redirect to the login page if not logged in and allow anonymous user setting is OFF
@@ -21,37 +21,12 @@ if( ! is_logged_in() && ! $Settings->get( 'allow_anonymous_user_profiles' ) )
 	header_redirect( get_login_url( 'cannot see user', $redirect_to ), 302 );
 }
 
-add_js_headline( "// Paths used by JS functions:
-		var bgxy_expand = '".get_icon( 'expand', 'xy' )."';
-		var bgxy_collapse = '".get_icon( 'collapse', 'xy' )."';
-		var htsrv_url = '$htsrv_url';" );
-
 require $ads_current_skin_path.'index.main.php';
 
 /*
  * $Log$
- * Revision 1.1  2011/10/16 20:34:52  fplanque
- * To whomever forgot to check in this file: please check that this version is correct.
+ * Revision 1.2  2011/10/17 12:14:14  efy-asimo
+ * User main dispaly - update
  *
- * Revision 1.7  2011/10/13 17:40:53  fplanque
- * no message
- *
- * Revision 1.6  2011/10/11 06:38:50  efy-asimo
- * Add corresponding error messages when login required
- *
- * Revision 1.5  2011/10/10 20:46:39  fplanque
- * registration source tracking
- *
- * Revision 1.4  2011/10/07 06:12:22  efy-asimo
- * Check users availability before display
- *
- * Revision 1.3  2011/10/07 02:55:37  fplanque
- * doc
- *
- * Revision 1.2  2011/10/02 12:38:33  efy-yurybakh
- * fix sprite icons
- *
- * Revision 1.1  2011/09/30 12:24:56  efy-yurybakh
- * User directory
  */
 ?>
