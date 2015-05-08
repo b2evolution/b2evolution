60c3e28fbb4ff68c8e7877154f85359e606b4902
Francois Planque <fplanque@users.noreply.github.com>
2015-05-08 20:45:04 +0200

    date dump

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.4.0-alpha';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2015-05-06';
+$app_date = '2015-05-08';
 
 /**
  * Long version string for checking differences
