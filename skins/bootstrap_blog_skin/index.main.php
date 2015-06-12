75972aba8c94adf43519182d7f77b3fb8f403cb9
Francois Planque <fplanque@users.noreply.github.com>
2015-06-12 02:36:34 +0200

    better handling of custom templates

--- skins/bootstrap_blog_skin/index.main.php
+++ skins/bootstrap_blog_skin/index.main.php
@@ -170,7 +170,7 @@ siteskin_include( '_site_body_header.inc.php' );
 
 		<?php
 		// Go Grab the featured post:
-		if( $Item = & get_featured_Item() )
+		if( ! in_array( $disp, array( 'single', 'page' ) ) && $Item = & get_featured_Item() )
 		{ // We have a featured/intro post to display:
 			// ---------------------- ITEM BLOCK INCLUDED HERE ------------------------
 			echo '<div class="panel panel-default"><div class="panel-body">';
@@ -186,58 +186,8 @@ siteskin_include( '_site_body_header.inc.php' );
 		?>
 
 		<?php
-		if( $disp != 'front' && $disp != 'download' && $disp != 'search' )
-		{
-			// -------------------- PREV/NEXT PAGE LINKS (POST LIST MODE) --------------------
-			mainlist_page_links( array(
-					'block_start' => '<div class="center"><ul class="pagination">',
-					'block_end' => '</ul></div>',
-					'page_current_template' => '<span><b>$page_num$</b></span>',
-					'page_item_before' => '<li>',
-					'page_item_after' => '</li>',
-				) );
-			// ------------------------- END OF PREV/NEXT PAGE LINKS -------------------------
-		?>
-
-		<?php
-			// --------------------------------- START OF POSTS -------------------------------------
-			// Display message if no post:
-			display_if_empty();
-
-			while( $Item = & mainlist_get_item() )
-			{ // For each blog post, do everything below up to the closing curly brace "}"
-
-				// ---------------------- ITEM BLOCK INCLUDED HERE ------------------------
-				skin_include( '_item_block.inc.php', array(
-						'content_mode' => 'auto',		// 'auto' will auto select depending on $disp-detail
-					) );
-				// ----------------------------END ITEM BLOCK  ----------------------------
-
-			} // ---------------------------------- END OF POSTS ------------------------------------
-		?>
-
-		<?php
-			// -------------------- PREV/NEXT PAGE LINKS (POST LIST MODE) --------------------
-			mainlist_page_links( array(
-					'block_start' => '<div class="center"><ul class="pagination">',
-					'block_end' => '</ul></div>',
-					'page_current_template' => '<span><b>$page_num$</b></span>',
-					'page_item_before' => '<li>',
-					'page_item_after' => '</li>',
-					'prev_text' => '&lt;&lt;',
-					'next_text' => '&gt;&gt;',
-				) );
-			// ------------------------- END OF PREV/NEXT PAGE LINKS -------------------------
-		}
-		?>
-
-		<?php
 			// -------------- MAIN CONTENT TEMPLATE INCLUDED HERE (Based on $disp) --------------
 			skin_include( '$disp$', array(
-					'disp_posts'  => '',		// We already handled this case above
-					'disp_single' => '',		// We already handled this case above
-					'disp_page'   => '',		// We already handled this case above
-					'disp_front'  => '_front.disp.php',
 					'author_link_text' => 'preferredname',
 					// Profile tabs to switch between user edit forms
 					'profile_tabs' => array(
