a8ac3c2fb987b8ceddf366aa03b1af3ba308d2dd
Francois Planque <fplanque@users.noreply.github.com>
2015-06-13 02:09:07 +0200

    minor

--- skins_fallback_v6/_item_block.inc.php
+++ skins_fallback_v6/_item_block.inc.php
@@ -37,6 +37,7 @@ $params = array_merge( array(
 		'author_link_text'           => 'preferredname',
 	), $params );
 
+
 echo '<div id="styled_content_block">'; // Beginning of post display  TODO: get rid of this ID, use class .evo_content_block instead
 ?>
 
