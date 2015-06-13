dd0d734b385ed7e772538a396c6c0e4ccbaaaff7
Francois Planque <fplanque@users.noreply.github.com>
2015-06-14 01:33:48 +0200

    changed default post markup in v6 skin API

--- skins_fallback_v6/_item_block.inc.php
+++ skins_fallback_v6/_item_block.inc.php
@@ -152,14 +152,14 @@ echo '<div id="styled_content_block">'; // Beginning of post display  TODO: get 
 			if( ! $Item->is_intro() )
 			{ // List all tags attached to this post:
 				$Item->tags( array(
-						'before'    => '<nav class="small">'.T_('Tags').': ',
+						'before'    => '<nav class="small post_tags">',
 						'after'     => '</nav>',
-						'separator' => ', ',
+						'separator' => ' ',
 					) );
 			}
 		?>
 
-		<nav class="small">
+		<nav class="post_comments_link">
 		<?php
 			// Link to comments, trackbacks, etc.:
 			$Item->feedback_link( array(
