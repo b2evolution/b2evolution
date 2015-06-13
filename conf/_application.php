d02cb9268e4f3311a865e43b159bb91cdcf10d3f
Francois Planque <fplanque@users.noreply.github.com>
2015-06-14 01:36:12 +0200

    date bump

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.4.4-beta';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2015-06-12';
+$app_date = '2015-06-14';
 
 /**
  * Long version string for checking differences
