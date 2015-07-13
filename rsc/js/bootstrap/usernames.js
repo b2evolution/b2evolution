197c907920cbf6b904ff2ff7a60910bbae3a61b4
Francois Planque <fplanque@users.noreply.github.com>
2015-07-13 02:24:55 +0200

    JS changes

--- rsc/js/bootstrap/usernames.js
+++ rsc/js/bootstrap/usernames.js
@@ -1,8 +1,34 @@
 /**
  * This file is part of the evoCore framework - {@link http://evocore.net/}
  * See also {@link https://github.com/b2evolution/b2evolution}.
- * @version $Id: usernames.js 8373 2015-02-28 21:44:37Z fplanque $
+ * @version $Id: usernames.js 9783 2015-07-09 15:22:59Z yura $
  */
+
+var originalLeave = jQuery.fn.popover.Constructor.prototype.leave;
+jQuery.fn.popover.Constructor.prototype.leave = function( obj )
+{
+	var self = obj instanceof this.constructor ?
+		obj : jQuery( obj.currentTarget )[this.type](this.getDelegateOptions()).data( 'bs.' + this.type )
+	var container, timeout;
+
+	originalLeave.call( this, obj );
+
+	if( obj.currentTarget )
+	{
+		container = jQuery( obj.currentTarget ).siblings( '.popover' )
+		timeout = self.timeout;
+		container.one('mouseenter', function()
+		{
+			//We entered the actual popover – call off the dogs
+			clearTimeout( timeout );
+			//Let's monitor popover content instead
+			container.one('mouseleave', function()
+			{
+				jQuery.fn.popover.Constructor.prototype.leave.call( self, self );
+			} );
+		} )
+	}
+};
 
 jQuery( document ).ready(function()
 {
@@ -36,6 +62,7 @@ jQuery( document ).ready(function()
 				trigger: 'hover',
 				placement: 'top',
 				html: true,
+				delay: { 'hide': 400 },
 				template: '<div class="popover"><div class="arrow"></div><div class="popover-content"></div></div>'
 			};
 			if( jQuery( '#' + div_cache_ID ).length == 0 )
