531c79b54568a71d3d89c95c7b2d5ac906676305
Francois Planque <fplanque@users.noreply.github.com>
2015-07-13 02:32:40 +0200

    fixes

--- htsrv/anon_async.php
+++ htsrv/anon_async.php
@@ -156,7 +156,7 @@ switch( $action )
 			}
 
 			$avatar_overlay_text = '';
-			$link_overlay_class = '';
+			$link_class = '';
 			if( is_admin_page() )
 			{	// Set avatar size for Back-office
 				$avatar_size = $Settings->get('bubbletip_size_admin');
@@ -169,7 +169,7 @@ switch( $action )
 			{	// Set avatar size for Anonymous users
 				$avatar_size = $Settings->get('bubbletip_size_anonymous');
 				$avatar_overlay_text = $Settings->get('bubbletip_overlay');
-				$link_overlay_class = 'overlay_link';
+				$link_class = 'overlay_link';
 			}
 
 			$width = $thumbnail_sizes[$avatar_size][1];
@@ -177,7 +177,7 @@ switch( $action )
 			// Display user avatar with login
 			// Attributes 'w' & 'h' we use for following js-scale div If image is downloading first time (Fix bubbletip)
 			echo '<div class="center" w="'.$width.'" h="'.$height.'">';
-			echo get_avatar_imgtag( $User->login, 'login', true, $avatar_size, 'avatar_above_login', '', $avatar_overlay_text, $link_overlay_class );
+			echo get_avatar_imgtag( $User->login, 'login', true, $avatar_size, 'avatar_above_login', '', $avatar_overlay_text, $link_class, true, '' );
 			echo '</div>';
 
 			if( ! ( $Settings->get( 'allow_anonymous_user_profiles' ) || ( is_logged_in() && $current_User->check_perm( 'user', 'view', false, $User ) ) ) )
@@ -272,7 +272,7 @@ switch( $action )
 			echo '<div class="bubbletip_anon">';
 
 			echo $Comment->get_avatar( 'fit-160x160', 'bCommentAvatarCenter' );
-			echo '<div>'.$Comment->get_author_name_anonymous().'</div>';
+			echo '<div>'.$Comment->get_author_name_anonymous( 'htmlbody', array( 'rel' => '' ) ).'</div>';
 			echo '<div>'.T_('This user is not registered on this site.').'</div>';
 			echo $Comment->get_author_url_link( '', '<div>', '</div>');
 
