01beb737f86c06cb4b5f4fe3c3c71f9103077a08
Francois Planque <fplanque@mbp2-fp.local>
2015-07-17 03:13:31 +0200

    date bump

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.6.0-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2015-07-16';
+$app_date = '2015-07-17';
 
 /**
  * Long version string for checking differences
