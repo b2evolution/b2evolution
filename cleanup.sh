ab98f29784006b943f30758f7dcdf255f7973f71
Francois Planque <fplanque@users.noreply.github.com>
2015-07-20 23:12:42 +0200

    Add cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,9 @@
+echo Removing unnecessary files from distribution
+rm -rf _tests
+rm -rf _transifex
+rm -f Gruntfile.js
+rm -f package.json
+rm -f readme.md
+rm -f readme.template.html
+echo Removing myself now
+rm -f cleanup.sh
