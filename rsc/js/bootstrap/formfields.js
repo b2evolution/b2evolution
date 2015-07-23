b17f779568a8eadf6f86b67538440b4e498f6ed9
Francois Planque <fplanque@users.noreply.github.com>
2015-07-23 23:48:50 +0200

    Better error field handling in bootstrap

--- rsc/js/bootstrap/formfields.js
+++ rsc/js/bootstrap/formfields.js
@@ -36,18 +36,25 @@ function form_error_field_popover( this_obj, placement, trigger )
 	jQuery( this_obj ).popover( 'show' );
 }
 
+// Set what events use to display a tooltip depending on device type:
+if( 'ontouchstart' in window )
+{ // Touch devices (without mouse cursor)
+	var form_error_field_event = 'focus';
+	var form_error_field_trigger = 'focus';
+}
+else
+{ // Normal devices (with mouse cursor)
+	var form_error_field_event = 'mouseover';
+	var form_error_field_trigger = 'hover';
+}
+
 // Prepare event focus for the form fields with popover effect:
-jQuery( document ).on( 'focus', 'input.field_error[type=text], textarea.field_error', function()
-{ // <input type="text"> and <textarea> on focus
-	form_error_field_popover( this, 'bottom', 'focus' );
+jQuery( document ).on( form_error_field_event, 'input.field_error[type=text], input.field_error[type=radio], span.checkbox_error input[type=checkbox]', function()
+{ // <input type="text|radio|checkbox">
+	form_error_field_popover( this, 'bottom', form_error_field_trigger );
 } );
 
-jQuery( document ).on( 'mouseover', 'input.field_error[type=radio], span.checkbox_error input[type=checkbox]', function()
-{ // <input type="radio"> and <input type="checkbox"> on mouse over
-	form_error_field_popover( this, 'bottom', 'hover' );
-} );
-
-jQuery( document ).on( 'focus', 'select.field_error', function()
-{ // <select> on focus
-	form_error_field_popover( this, 'top', 'focus' );
+jQuery( document ).on( form_error_field_event, 'select.field_error, textarea.field_error', function()
+{ // <select> and <textarea>
+	form_error_field_popover( this, 'top', form_error_field_trigger );
 } );
