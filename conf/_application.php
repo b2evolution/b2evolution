c2d7526deafb558be0e72656cb1cbad33972b27b
yurabakhtin <yurybakh@gmail.com>
2015-08-03 09:30:42 +0300

    Set correct status color for buttons on all non-bootstrap skins

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.6.1-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2015-07-22';
+$app_date = '2015-08-03';
 
 /**
  * Long version string for checking differences
