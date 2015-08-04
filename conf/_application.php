8ab98df8ccfd9517666f7ee819fb8bf447907511
yurabakhtin <yurybakh@gmail.com>
2015-08-04 11:51:13 +0300

    Fix styles of the search results

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.7.0-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2015-08-03';
+$app_date = '2015-08-04';
 
 /**
  * Long version string for checking differences
