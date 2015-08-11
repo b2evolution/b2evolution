9613fe5979845b6e8e56df75c02cf6cb6c6f7cc3
yurabakhtin <yurybakh@gmail.com>
2015-08-11 16:45:16 +0300

    Update application date

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.6.2-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2015-08-06';
+$app_date = '2015-08-11';
 
 /**
  * Long version string for checking differences
