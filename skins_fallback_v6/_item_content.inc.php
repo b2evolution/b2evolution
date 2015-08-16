19209e1a3b5a5b2b9c7474560c5e5a912189823b
Manuel García <solipa@gmail.com>
2015-08-16 23:07:13 +0200

    Include attached files set as "After more" when fullpost is enabled
    User request can be found on this post: http://forums.b2evolution.net/fullpost-contents-including-after-more-not-working-on

--- skins_fallback_v6/_item_content.inc.php
+++ skins_fallback_v6/_item_content.inc.php
@@ -311,7 +311,7 @@ switch( $content_mode )
 				) );
 
 			// Display images that are linked "after more" to this post:
-			if( ! empty($params['image_size']) && $more && $Item->has_content_parts($params) /* only if not displayed all images already */ )
+			if( ! empty($params['image_size']) && ($more || $params['force_more']) && $Item->has_content_parts($params) /* only if not displayed all images already */ )
 			{	
 				$Item->images( array(
 						'before'              => $params['before_images'],
