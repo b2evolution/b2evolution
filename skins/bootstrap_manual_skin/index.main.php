f797dc54425a11400838f4641285051463aa9d5e
yurabakhtin <yurybakh@gmail.com>
2015-08-24 12:57:14 +0300

    Fix column width on bootstrap_manual_skin

--- skins/bootstrap_manual_skin/index.main.php
+++ skins/bootstrap_manual_skin/index.main.php
@@ -117,7 +117,7 @@ siteskin_include( '_site_body_header.inc.php' );
 
 <div class="row">
 
-	<div class="<?php echo $Skin->is_left_navigation_visible() ? 'col-md-9 pull-right' : 'col-md-12' ?>">
+	<div class="<?php echo $Skin->is_left_navigation_visible() ? 'col-xs-12 col-md-9 pull-right' : 'col-md-12' ?>">
 
 		<main><!-- This is were a link like "Jump to main content" would land -->
 
@@ -192,7 +192,7 @@ siteskin_include( '_site_body_header.inc.php' );
 	{ // Display a left column with navigation only for several pages
 	?>
 		<!-- =================================== START OF SIDEBAR =================================== -->
-		<aside class="col-md-3 pull-left">
+		<aside class="col-xs-12 col-md-3 pull-left">
 
 			<div id="evo_container__sidebar">
 
