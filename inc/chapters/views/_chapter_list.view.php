e859a04ee6330dd374fdab1ec1df9c922e22ade3
Francois Planque <github@fplanque.net>
2015-09-26 01:29:12 +0200

    Nicer display of info alert

--- inc/chapters/views/_chapter_list.view.php
+++ inc/chapters/views/_chapter_list.view.php
@@ -333,7 +333,7 @@ echo '</div>'; // form wrapper end
 
 if( ! $Settings->get('allow_moving_chapters') )
 { // TODO: check perm
-	echo '<p class="note">'.sprintf( T_('<strong>Note:</strong> Moving categories across blogs is currently disabled in the %sblogs settings%s.'), '<a href="'.$dispatcher.'?ctrl=collections&tab=blog_settings#fieldset_wrapper_categories">', '</a>' ).'</p> ';
+	echo '<p class="alert alert-info">'.sprintf( T_('<strong>Note:</strong> Moving categories across blogs is currently disabled in the %sblogs settings%s.'), '<a href="'.$dispatcher.'?ctrl=collections&tab=blog_settings#fieldset_wrapper_categories">', '</a>' ).'</p> ';
 }
 
 //Flush fadeout
