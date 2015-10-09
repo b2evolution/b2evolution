7c1fffc638b01c1c418440202499708bf73785d9
yurabakhtin <yurybakh@gmail.com>
2015-10-09 09:23:28 +0300

    Display chapter info on search pages

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.6.4-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2015-10-08';
+$app_date = '2015-10-09';
 
 /**
  * Long version string for checking differences
