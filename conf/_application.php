a10c68b474c2ee0a4083f56a9726165a8f3f259c
yurabakhtin <yurybakh@gmail.com>
2015-10-13 17:14:47 +0300

    Reset image position and scrolling to top/left corner on zoom out of colorbox

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.6.5-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2015-10-12';
+$app_date = '2015-10-13';
 
 /**
  * Long version string for checking differences
