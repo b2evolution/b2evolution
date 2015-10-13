d8d3b52e702f9e9f4c69bbda2e2085ebbb847d5e
yurabakhtin <yurybakh@gmail.com>
2015-10-13 10:49:41 +0300

    Display info of meta comments on front-office

--- skins/bootstrap_manual_skin/_front.disp.php
+++ skins/bootstrap_manual_skin/_front.disp.php
@@ -58,7 +58,7 @@ if( ! empty( $intro_Item ) )
 	echo '<div class="evo_content_block">'; // Beginning of posts display
 	// ------------------ FEEDBACK (COMMENTS/TRACKBACKS) INCLUDED HERE ------------------
 	skin_include( '_item_feedback.inc.php', array_merge( array(
-			'before_section_title' => '<h3 class="comments_list_title">',
+			'before_section_title' => '<h3 class="evo_comment__list_title">',
 			'after_section_title'  => '</h3>',
 			'Item'                 => $intro_Item,
 			'form_title_text'      => T_('Comment form'),
