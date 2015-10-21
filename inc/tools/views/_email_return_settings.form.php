8c7908cb69b427acbc9b15f72779da2749801da7
yurabakhtin <yurybakh@gmail.com>
2015-10-21 15:21:25 +0300

    Reorganize the tabs for returned emails in back-office

--- inc/tools/views/_email_return_settings.form.php
+++ inc/tools/views/_email_return_settings.form.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file implements the UI view for the general settings.
+ * This file implements the form to edit settings for returned emails.
  *
  * This file is part of the evoCore framework - {@link http://evocore.net/}
  * See also {@link https://github.com/b2evolution/b2evolution}.
@@ -24,10 +24,6 @@ global $current_User;
  */
 global $Settings;
 
-global $baseurl, $admin_url;
-
-global $repath_test_output, $action;
-
 
 $Form = new Form( NULL, 'settings_checkchanges' );
 
@@ -35,35 +31,9 @@ $Form->begin_form( 'fform' );
 
 $Form->add_crumb( 'emailsettings' );
 $Form->hidden( 'ctrl', 'email' );
-$Form->hidden( 'tab', 'settings' );
+$Form->hidden( 'tab', get_param( 'tab' ) );
 $Form->hidden( 'tab3', get_param( 'tab3' ) );
 $Form->hidden( 'action', 'settings' );
-
-if( $current_User->check_perm( 'emails', 'edit' ) )
-{
-	$Form->begin_fieldset( T_('Test saved settings').get_manual_link( 'return-path-configuration' ) );
-
-		$url = '?ctrl=email&amp;tab=settings&amp;tab3=returned&amp;'.url_crumb('emailsettings').'&amp;action=';
-		$Form->info_field( T_('Perform tests'),
-					'<a href="'.$url.'test_1">['.T_('server connection').']</a>&nbsp;&nbsp;'.
-					'<a href="'.$url.'test_2">['.T_('get one returned email').']</a>&nbsp;&nbsp;'.
-					'<a href="'.$url.'test_3">['.T_('Paste an error message/returned email').']</a>' );
-
-		if( $action == 'test_3' )
-		{ // Display a textarea to fill a sample error message
-			$Form->textarea( 'test_error_message', param( 'test_error_message', 'raw', '' ), 15, T_('Test error message'), '', 50 );
-			$Form->buttons( array( array( 'submit', 'actionArray[test_3]', T_('Test'), 'SaveButton' ) ) );
-		}
-
-		if( !empty( $repath_test_output ) )
-		{
-			echo '<div style="margin-top:25px"></div>';
-			// Display scrollable div
-			echo '<div style="padding: 6px; margin:5px; border: 1px solid #CCC; overflow:scroll; height: 350px">'.$repath_test_output.'</div>';
-		}
-
-	$Form->end_fieldset();
-}
 
 $Form->begin_fieldset( T_('Settings to decode the returned emails').get_manual_link('return-path-configuration') );
 
@@ -120,8 +90,6 @@ $Form->begin_fieldset( T_('Settings to decode the returned emails').get_manual_l
 				T_('The first letter means one of the following:<br />S: Spam suspicion<br />P: Permament error<br />T: Temporary error<br />C: Configuration error<br />U: Unknown error (default)<br />The string after the space is a case-insensitive error text.'), 50 );
 
 $Form->end_fieldset();
-
-
 
 if( $current_User->check_perm( 'emails', 'edit' ) )
 {
