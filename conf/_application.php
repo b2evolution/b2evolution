0db09ae06e424667868d0862029f740064ed2f39
yurabakhtin <yurybakh@gmail.com>
2015-11-20 08:52:08 +0300

    Don't use a color for user names in the header site menu

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.6.6-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2015-11-17';
+$app_date = '2015-11-20';
 
 /**
  * Long version string for checking differences
