b1b4477a0debba1f3dced1db00f66d5aca2c52e2
yurabakhtin <yurybakh@gmail.com>
2015-11-30 10:32:56 +0300

    Align top all cells of messages table

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.6.6-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2015-11-20';
+$app_date = '2015-11-30';
 
 /**
  * Long version string for checking differences
