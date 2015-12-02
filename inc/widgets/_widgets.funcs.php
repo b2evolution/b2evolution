aa4ddc5709119f3c6d96b7269d5cfdb0a17e62dc
yurabakhtin <yurybakh@gmail.com>
2015-12-02 15:41:18 +0300

    Widget "Seen by"

--- inc/widgets/_widgets.funcs.php
+++ inc/widgets/_widgets.funcs.php
@@ -162,6 +162,8 @@ function insert_basic_widgets( $blog_id, $initial_install = false, $kind = '' )
 	{ // Small Print
 		add_basic_widget( $blog_id, 'Item Single', 'coll_small_print', 'core', 40, array( 'format' => ( $blog_id == $blog_a_ID ? 'standard' : 'revision' ) ) );
 	}
+	// Seen by
+	add_basic_widget( $blog_id, 'Item Single', 'coll_seen_by', 'core', 50 );
 
 
 	/* Page Top */
--- inc/widgets/views/_widget_list_available.view.php
+++ inc/widgets/views/_widget_list_available.view.php
@@ -94,6 +94,7 @@ $core_componentwidget_defs = array(
 			'coll_small_print',
 			'coll_item_tags',
 			'coll_about_author',
+			'coll_seen_by',
 		),
 	'collection' => array(
 			'coll_logo',
