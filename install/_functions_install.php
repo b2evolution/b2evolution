ea4d1c092aec6ddb414082a940777a49dbde4109
Erwin Rommel H. Satingin <winskie@gmail.com>
2016-01-12 20:27:10 +0800

    Reverted to evo_flush before creating default email campaigns

--- install/_functions_install.php
+++ install/_functions_install.php
@@ -220,8 +220,8 @@ function install_newdb()
 		create_demo_contents();
 	}
 
-	create_default_email_campaigns();
 	evo_flush();
+	create_default_email_campaigns();
 
 	// Update the progress bar status
 	update_install_progress_bar();
