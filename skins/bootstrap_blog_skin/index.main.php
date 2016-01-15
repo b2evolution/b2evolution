a81d395b388762514de52f3d135d0d211a3725d8
Francois Planque <github@fplanque.net>
2016-01-15 23:25:55 +0100

    New skin params
    to decide whether or not to display url link and parent link in disp=single

--- skins/bootstrap_blog_skin/index.main.php
+++ skins/bootstrap_blog_skin/index.main.php
@@ -208,6 +208,9 @@ siteskin_include( '_site_body_header.inc.php' );
 						'prev_text'             => '<i class="fa fa-angle-double-left"></i>',
 						'next_text'             => '<i class="fa fa-angle-double-right"></i>',
 					),
+					// disp=single:
+					'url_link_position'     => 'top',
+					'parent_link_position'  => 'top',
 					// Form params for the forms below: login, register, lostpassword, activateinfo and msgform
 					'skin_form_before'      => '<div class="panel panel-default skin-form">'
 																				.'<div class="panel-heading">'
