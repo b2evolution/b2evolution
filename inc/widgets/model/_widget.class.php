e8c5c22721a2a0c3f3ba6c654b744b8c75cf7791
Artashes Papikyan <artash.dev@gmail.com>
2016-01-27 15:17:39 +0400

    Fix bug: Declaration of ComponentWidget::dbupdate() should be compatible with DataObject::dbupdate( = true)

--- inc/widgets/model/_widget.class.php
+++ inc/widgets/model/_widget.class.php
@@ -861,7 +861,7 @@ class ComponentWidget extends DataObject
 	/**
 	 * Update the DB based on previously recorded changes
 	 */
-	function dbupdate()
+	function dbupdate( $php7_compatibility = '' )
 	{
 		global $DB;
 
