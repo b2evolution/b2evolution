9a05465169b0ace2c3234fc039299f73282baff4
yurabakhtin <yurybakh@gmail.com>
2016-02-09 12:05:58 +0300

    Enhance tool of sample users creating

--- inc/tools/views/_create_collections.form.php
+++ inc/tools/views/_create_collections.form.php
@@ -32,13 +32,15 @@ $Form->begin_form( 'fform',  T_('Create sample collections for testing') );
 	$Form->checklist( array(
 			array( 'perm_management[]', 'simple', T_('Simple permissions'), is_null( $perm_management ) || in_array( 'simple', $perm_management ) ),
 			array( 'perm_management[]', 'advanced', T_('Advanced permissions'), is_null( $perm_management ) || in_array( 'advanced', $perm_management ) ),
-		), 'perm_management', T_('Permission management'), true, false, array( 'note' => T_('Note: For each collection it creates, the tool will randomly select between the allowed (checked) options above') ) );
+		), 'perm_management', T_('Permission management'), true, false,
+		array( 'note' => T_('Note: For each collection it creates, the tool will randomly select between the allowed (checked) options above') ) );
 
 	$Form->checklist( array(
 			array( 'allow_access[]', 'public', T_('Everyone (Public Blog)'), is_null( $allow_access ) || in_array( 'public', $allow_access ) ),
 			array( 'allow_access[]', 'users', T_('Logged in users'), is_null( $allow_access ) || in_array( 'users', $allow_access ) ),
 			array( 'allow_access[]', 'members', T_('Owner / Member only'), is_null( $allow_access ) || in_array( 'members', $allow_access ) ),
-		), 'allow_access', T_('Allow access to'), true, false, array( 'note' => T_('Note: For each collection it creates, the tool will randomly select between the allowed (checked) options above') ) );
+		), 'allow_access', T_('Allow access to'), true, false,
+		array( 'note' => T_('Note: For each collection it creates, the tool will randomly select between the allowed (checked) options above') ) );
 
 $Form->end_form( array( array( 'submit', 'submit', T_('Create'), 'SaveButton' ) ) );
 
