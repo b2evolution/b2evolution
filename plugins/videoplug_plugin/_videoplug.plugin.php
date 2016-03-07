565ca6f3ad7e6b9a51f1530fff9f1e258d893152
yurabakhtin <yurybakh@gmail.com>
2016-03-07 07:37:16 +0300

    Update video plugin youtube regexp

--- plugins/videoplug_plugin/_videoplug.plugin.php
+++ plugins/videoplug_plugin/_videoplug.plugin.php
@@ -203,7 +203,7 @@ class videoplug_plugin extends Plugin
 						case 'youtube':
 							// Allow HD video code with ?hd=1 at the end
 							regexp_ID = /^[a-z0-9_?=-]+$/i;
-							regexp_URL = /^(.+\?v=)?([a-z0-9_?=-]+)$/i;
+							regexp_URL = /^.+(video\/|\/watch\?v=|embed\/|\/)([a-z0-9_?=-]+)$/i;
 							break;
 
 						case 'dailymotion':
