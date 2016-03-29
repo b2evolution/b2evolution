9315f89c31359f75a6152a99cce4e9f6368e3204
yurabakhtin <yurybakh@gmail.com>
2016-03-29 15:38:18 +0300

    Fix style bug on back-office item edit form

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.7.0-alpha';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2016-03-21';
+$app_date = '2016-03-29';
 
 /**
  * Long version string for checking differences
