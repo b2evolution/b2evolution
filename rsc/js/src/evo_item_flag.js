0af7117815cde98a86ee5ff98ed131cad11fcc40
yurabakhtin <yurybakh@gmail.com>
2016-04-14 07:29:12 +0300

    Improve flag colors in progress status

--- rsc/js/src/evo_item_flag.js
+++ rsc/js/src/evo_item_flag.js
@@ -8,8 +8,13 @@ jQuery( document ).on( 'click', 'a.evo_post_flag_btn', function()
 	var item_ID = parseInt( flag_link.data( 'id' ) );
 
 	if( item_ID > 0 )
-	{	// Request to flag if item ID is defined:
+	{	// If item ID is defined...
+		// Set status to know the request is in progress now:
 		flag_link.data( 'status', 'inprogress' );
+		// Add class to display the request is in progress now:
+		jQuery( 'span', jQuery( this ) ).addClass( 'fa-x--hover' );
+
+		// Call request to flag:
 		evo_rest_api_request( 'collections/' + flag_link.data( 'coll' ) + '/items/' + item_ID + '/flag',
 		function( data )
 		{	// Toggle icon to new flag status:
@@ -23,6 +28,7 @@ jQuery( document ).on( 'click', 'a.evo_post_flag_btn', function()
 				flag_link.find( 'span:last' ).show();
 				flag_link.find( 'span:first' ).hide();
 			}
+
 			// Remove hover style in order to keep current updated status:
 			jQuery( 'span', flag_link ).removeClass( 'fa-x--hover' );
 			// Pause progress status to avoid mouseover event right after updating:
