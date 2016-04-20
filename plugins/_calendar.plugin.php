b0b7beb67afb33fbcc690ea0f525db001d98ed85
yurabakhtin <yurybakh@gmail.com>
2016-04-20 09:02:55 +0300

    Set correct widget cache keys for plugin "Calendar"

--- plugins/_calendar.plugin.php
+++ plugins/_calendar.plugin.php
@@ -164,6 +164,26 @@ class calendar_plugin extends Plugin
 
 
 	/**
+	 * Get keys for block/widget caching
+	 *
+	 * Maybe be overriden by some widgets, depending on what THEY depend on..
+	 *
+	 * @param integer Widget ID
+	 * @return array of keys this widget depends on
+	 */
+	function get_widget_cache_keys( $widget_ID = 0 )
+	{
+		global $Blog;
+
+		return array(
+				'wi_ID'        => $widget_ID, // Have the widget settings changed ?
+				'set_coll_ID'  => $Blog->ID, // Have the settings of the blog changed ? (ex: new skin)
+				'cont_coll_ID' => empty( $this->disp_params['blog_ID'] ) ? $Blog->ID : $this->disp_params['blog_ID'], // Has the content of the displayed blog changed ?
+			);
+	}
+
+
+	/**
 	 * Event handler: SkinTag (widget)
 	 *
 	 * @param array Associative array of parameters. Valid keys are:
