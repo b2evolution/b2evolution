17e96c596528ed7966a9d096e9f7a85d1f5921a4
yurabakhtin <yurybakh@gmail.com>
2016-05-11 07:25:15 +0300

    Center the play button of plugin "HTML 5 VideoJS Player"

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.7.2-beta';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2016-05-08';
+$app_date = '2016-05-11';
 
 /**
  * Long version string for checking differences
