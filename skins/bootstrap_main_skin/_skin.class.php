8bdd9a9c99c617881b32cd028f1dfdd614abdaa0
Milos Zivojinovic <miloszivojinovic93@gmail.com>
2016-05-12 23:08:50 +0200

    bootstrap_main_Skin - customization options fix

--- skins/bootstrap_main_skin/_skin.class.php
+++ skins/bootstrap_main_skin/_skin.class.php
@@ -361,7 +361,7 @@ class bootstrap_main_Skin extends Skin
 
 			if( $color = $this->get_setting( 'front_text_color' ) )
 			{ // Custom text color:
-				$custom_css .= 'body.pictured .front_main_content, body.pictured .front_main_content h1 small { color: '.$color." }\n";
+				$custom_css .= 'body.pictured .front_main_content, body.pictured .front_main_content h1 small, .evo_container__header, .evo_container__page_top { color: '.$color." }\n";
 			}
 
 			$link_color = $this->get_setting( 'front_link_color' );
