c22c0b034ecf0364a35566f5f63a1ba7b8f47697
Milos Zivojinovic <miloszivojinovic93@gmail.com>
2016-05-14 00:35:36 +0200

    bootstrap_main_Skin - UIL widget teaser dots style

--- skins/bootstrap_main_skin/_skin.class.php
+++ skins/bootstrap_main_skin/_skin.class.php
@@ -368,7 +368,9 @@ class bootstrap_main_Skin extends Skin
 			$icon_color = $this->get_setting( 'front_icon_color' );
 			if( $link_color )
 			{ // Custom link color:
-				$custom_css .= 'body.pictured .main_page_wrapper .front_main_area a { color: '.$link_color." }\n";
+				$custom_css .= 'body.pictured .main_page_wrapper .front_main_area a,
+				body.pictured .main_page_wrapper .front_main_area div.widget_core_coll_item_list.evo_noexcerpt.evo_withteaser ul li div.item_content > a,
+				body.pictured .main_page_wrapper .front_main_area div.widget_core_coll_post_list.evo_noexcerpt.evo_withteaser ul li div.item_content > a { color: '.$link_color." }\n";
 			}
 			if( $link_color && $icon_color )
 			{ // Custom icon color:
