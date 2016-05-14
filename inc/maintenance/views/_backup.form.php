5735089c27994aea1fecf1fa3c25b84ab384b1ff
Francois Planque <github@fplanque.net>
2016-05-15 01:19:14 +0200

    more backup ignores

--- inc/maintenance/views/_backup.form.php
+++ inc/maintenance/views/_backup.form.php
@@ -83,7 +83,7 @@ if( count( $backup_exclude_checkboxes ) )
 	$Form->checklist( $backup_exclude_checkboxes, 'exclude_bk', T_('Exclude folders') );
 }
 
-$Form->checkbox( 'ignore_bk_config', $current_Backup->ignore_config, T_('Ignore files and folders'), sprintf( T_('Ignore files and folders listed in %s'), '<code>conf/backup_ignore.conf</code>' ) );
+$Form->checkbox( 'ignore_bk_config', $current_Backup->ignore_config, 'backup_ignore.conf', sprintf( T_('Ignore files and folders listed in %s'), '<code>conf/backup_ignore.conf</code>' ) );
 
 $Form->end_fieldset();
 
