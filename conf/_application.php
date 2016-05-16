bbca4d667d4d9e35c56bfa881bd7753e52f2b9c3
yurabakhtin <yurybakh@gmail.com>
2016-05-16 08:36:09 +0300

    Fix color of grey background for nav level 3 in back-office

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.7.2-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2016-05-15';
+$app_date = '2016-05-16';
 
 /**
  * Long version string for checking differences
