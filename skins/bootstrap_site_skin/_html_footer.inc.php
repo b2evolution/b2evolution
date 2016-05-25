e03afca9b975c8a29286fd87ed068613cdc68093
yurabakhtin <yurybakh@gmail.com>
2016-05-25 14:07:09 +0300

    New site skin "Bootstrap site"

---
+++ skins/bootstrap_site_skin/_html_footer.inc.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This is the HTML footer include template.
+ *
+ * This will be included ONLY for sitewide pages (like a global 404).
+ * When a blog is displayed, it is /skins/_html_header.inc.php that will be used
+ *
+ * @package skins
+ * @subpackage bootstrap_site_skin
+ */
+if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
+?>
+<!-- End of skin_wrapper -->
+
+<!-- /div -->
+
+</body>
+</html>
