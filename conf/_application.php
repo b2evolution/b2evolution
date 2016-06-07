46e168b6250fbcbfea718692dd714fa08d442826
yurabakhtin <yurybakh@gmail.com>
2016-06-07 09:29:09 +0300

    Remove css spinner class ".loadimg" from loaded images

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.7.4-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2016-06-05';
+$app_date = '2016-06-07';
 
 /**
  * Long version string for checking differences
