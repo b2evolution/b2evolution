afef68f2b63e1eb2802561a55111a434f00ea4e9
yurabakhtin <yurybakh@gmail.com>
2016-06-23 15:29:49 +0300

    Fix missed style of placeholder to sort table of attachments by javascript

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.7.4-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2016-06-14';
+$app_date = '2016-06-23';
 
 /**
  * Long version string for checking differences
