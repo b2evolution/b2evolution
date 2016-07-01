e5b20c986b88312e08a6d17aa04078f1f54f4a41
yurabakhtin <yurybakh@gmail.com>
2016-07-01 07:36:59 +0300

    Fix sorting of users list by secondary groups count

--- inc/users/model/_user.funcs.php
+++ inc/users/model/_user.funcs.php
@@ -5430,6 +5430,7 @@ function users_results( & $UserList, $params = array() )
 		$UserList->cols[] = array(
 				'th' => T_('Collections'),
 				'order' => 'nb_blogs',
+				'default_dir' => 'D',
 				'th_class' => 'shrinkwrap small',
 				'td_class' => 'center small',
 				'td' => '~conditional( (#nb_blogs# > 0), \'<a href="admin.php?ctrl=user&amp;user_tab=activity&amp;user_ID=$user_ID$" title="'.format_to_output( T_('View personal blogs'), 'htmlattr' ).'">$nb_blogs$</a>\', \'&nbsp;\' )~',
@@ -5539,7 +5540,8 @@ function users_results( & $UserList, $params = array() )
 				'th' => T_('Sec.<br />Groups'),
 				'th_class' => 'shrinkwrap small',
 				'td_class' => 'shrinkwrap small',
-				'order' => 'grp_name',
+				'order' => 'secondary_groups_count',
+				'default_dir' => 'D',
 				'td' => '%user_td_sec_groups( #user_ID#, #secondary_groups_count# )%',
 			);
 	}
