956ca0ddfb42bee4d7838e7eea023fd98688c1c4
Francois Planque <github@fplanque.net>
2016-08-08 22:18:34 +0200

    date bump

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.7.5-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2016-08-05';
+$app_date = '2016-08-08';
 
 /**
  * Long version string for checking differences
