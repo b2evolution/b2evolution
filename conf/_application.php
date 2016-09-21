4a7a7eafd83fc64d5bb105e10589523715ebe722
yurabakhtin <yurybakh@gmail.com>
2016-09-21 08:37:38 +0300

    Fix colorbox initialization depending on skin settings

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.7.7-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2016-09-10';
+$app_date = '2016-09-21';
 
 /**
  * Long version string for checking differences
