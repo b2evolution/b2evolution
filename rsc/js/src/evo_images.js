7ecfcbd3dadcd165f1c5baafbc37c9ebb71fb3e5
yurabakhtin <yurybakh@gmail.com>
2016-09-29 07:18:10 +0300

    Don't keep empty attribute "class" after image loading

--- rsc/js/src/evo_images.js
+++ rsc/js/src/evo_images.js
@@ -11,12 +11,20 @@ jQuery( document ).ready( function()
 		if( jQuery( this ).prop( 'complete' ) )
 		{	// If image is loaded from cache:
 			jQuery( this ).removeClass( 'loadimg' );
+			if( jQuery( this ).attr( 'class' ) == '' )
+			{	// Remove an empty attribute "class":
+				jQuery( this ).removeAttr( 'class' );
+			}
 		}
 		else
 		{	// After image has been loaded:
 			jQuery( this ).on( 'load', function()
 			{
 				jQuery( this ).removeClass( 'loadimg' );
+				if( jQuery( this ).attr( 'class' ) == '' )
+				{	// Remove an empty attribute "class":
+					jQuery( this ).removeAttr( 'class' );
+				}
 			} );
 		}
 	} );
