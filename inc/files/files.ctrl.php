d85327c03b9c019868d5586a67912c99b0721dd0
Erwin Rommel H. Satingin <winskie@gmail.com>
2016-11-08 14:33:40 +0800

    Changed message group title for messages related to creating single post from multiple files

--- inc/files/files.ctrl.php
+++ inc/files/files.ctrl.php
@@ -1102,7 +1102,7 @@ switch( $action )
 						// Let's make the link!
 						$LinkOwner->add_link( $l_File->ID, ( $order == 1 ? 'teaser' : 'aftermore' ), $order++ );
 
-						$Messages->add_to_group( sprintf( T_('&laquo;%s&raquo; has been attached.'), $l_File->dget('name') ), 'success', T_('Attaching files:') );
+						$Messages->add_to_group( sprintf( T_('&laquo;%s&raquo; has been attached.'), $l_File->dget('name') ), 'success', T_('Creating post:') );
 
 					} while( $l_File = & $selected_Filelist->get_next() );
 
