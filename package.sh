4375f710e63a606ba9ca7b92b1b38ccb2abdc911
Francois Planque <github@fplanque.net>
2016-11-09 19:00:34 +0100

    Make packaging script a little better

--- package.sh
+++ package.sh
@@ -15,5 +15,6 @@ echo Removing myself now
 rm -f cleanup.sh
 echo Stepping out
 cd ..
-echo Compressing
-zip -qr9 b2evolution.zip b2evolution 
+echo Compressing...
+currentbasename=${PWD##*/} 	# Assign current basename to variable
+zip -qr9 ${currentbasename}.zip b2evolution 
