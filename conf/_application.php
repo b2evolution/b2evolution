8c6f6ff43ab89181f19502938399490aae0bf0b0
yurabakhtin <yurybakh@gmail.com>
2016-11-11 15:35:19 +0300

    Fix user crop window styles on v5 skins

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.7.8-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2016-10-30';
+$app_date = '2016-11-11';
 
 /**
  * Long version string for checking differences
