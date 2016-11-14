cfa8071e3dbdc0260c0f7497870242c7ca1d7f81
yurabakhtin <yurybakh@gmail.com>
2016-11-14 18:40:21 +0300

    Don't load sprite_icons.inc.less on bootstrap skins

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.8.1-beta';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2016-11-11';
+$app_date = '2016-11-14';
 
 /**
  * Long version string for checking differences
