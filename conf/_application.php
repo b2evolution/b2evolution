18a517f9220e3e4fd6224fc285967b30b3339cab
yurabakhtin <yurybakh@gmail.com>
2016-11-15 09:12:48 +0300

    Fix space between user avatars in threads table

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.7.8-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2016-11-11';
+$app_date = '2016-11-15';
 
 /**
  * Long version string for checking differences
