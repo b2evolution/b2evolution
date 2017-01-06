256b591a85bc630134b3f9e811965ad8c6a83f8e
Erwin Rommel H. Satingin <winskie@gmail.com>
2017-01-06 13:36:01 +0800

    Reverted previous fix to bubble tip issue and applied different patch to address the same problem

--- rsc/js/bootstrap/usernames.js
+++ rsc/js/bootstrap/usernames.js
@@ -35,12 +35,6 @@ jQuery( document ).ready(function()
 	/** Init popover for User Avatar **/
 
 	var link_number = 1;
-
-	// Add small container for bubbletip cache, tucked far away but NOT HIDDEN
-	var bubbletipContainer = jQuery( '<div></div>' );
-	bubbletipContainer.height( 1 ).width( 1 ).offset({ top: -1000, left: -1000 }).css({ overflow: 'hidden', position: 'absolute' });
-	jQuery( 'body' ).append( bubbletipContainer );
-
 	jQuery( document ).on( 'mouseover', '[rel^=bubbletip_]', function()
 	{ // Prepare event mouseover for an element with popover effect
 		var link = jQuery( this );
@@ -70,12 +64,12 @@ jQuery( document ).ready(function()
 				placement: 'top',
 				html: true,
 				delay: { 'hide': 400 },
-				template: '<div class="popover"><div class="arrow"></div><div class="popover-content"></div></div>'
+				template: '<div class="popover user_bubbletip"><div class="arrow"></div><div class="popover-content"></div></div>'
 			};
 			if( jQuery( '#' + div_cache_ID ).length == 0 )
 			{ // Create a div for cache user data
-				var cache = jQuery( '<div id="' + div_cache_ID + '"></div>' );
-				bubbletipContainer.append( cache );
+				jQuery( 'body' ).append( '<div id="' + div_cache_ID + '" style="display:none;"></div>' );
+				var cache = jQuery( '#' + div_cache_ID );
 
 				jQuery.ajax(
 				{ // Get user info
@@ -88,6 +82,13 @@ jQuery( document ).ready(function()
 						{ // Init Popover only if ajax content is received
 							result = ajax_debug_clear( result );
 							cache.html( result );
+
+							var img = cache.find( 'img' );
+							img.on( 'load', function() {
+								var popover = jQuery( '.popover.user_bubbletip' );
+								popover.css( 'top',  link.offset().top - popover.outerHeight() );
+							});
+
 							var show_on_init = true;
 							if( link.hasClass( 'hide_popover' ) )
 							{ // We use this class as flag to understand that when ajax was loading
@@ -101,13 +102,10 @@ jQuery( document ).ready(function()
 							link.popover( popover_params );
 							if( show_on_init )
 							{ // Show popover
-								window.setTimeout( function()
-								{
-									link.popover( 'show' );
-								}, 10 );
+								link.popover( 'show' );
 							}
 							// Remove this from attr 'rel' to avoid of the repeating of init popover
-							link.attr( 'rel', link.attr( 'rel' ).replace( /bubbletip_(user_|comment_)[\d\s]+/g, '' ) );
+							link.attr( 'rel', link.attr( 'rel').replace( /bubbletip_(user_|comment_)[\d\s]+/g, '' ) );
 						}
 					}
 				});
@@ -118,10 +116,9 @@ jQuery( document ).ready(function()
 				{ // Ajax content is downloaded and we can show a popover
 					// Remove a title temporary to don't display title on popover
 					var link_title = link.attr( 'title' );
-					var cache = jQuery( '#' + div_cache_ID );
 					link.removeAttr( 'title' );
 					// Init popover
-					popover_params.content = cache.html();
+					popover_params.content = jQuery( '#' + div_cache_ID ).html();
 					link.popover( popover_params );
 					link.popover( 'show' );
 					// Restore a title
