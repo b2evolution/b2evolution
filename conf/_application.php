9e4a8fa07f4f2b36d826822b8ceb6011db8f54dd
Francois Planque <github@fplanque.net>
2017-01-25 03:16:32 +0100

    special version

--- conf/_application.php
+++ conf/_application.php
@@ -15,7 +15,7 @@ $app_shortname = 'b2evo';
  * Note: This has to be compatible with {@link http://us2.php.net/en/version-compare}.
  * @global string
  */
-$app_version = '6.8.6-stable';
+$app_version = '6.8.6-dev';
 
 /**
  * Release date (ISO)
