f64ad49d44d2ce4e20c4bdc58d85921ae04b15ae
Milos Zivojinovic <miloszivojinovic93@gmail.com>
2017-02-04 20:27:01 +0100

    remove images from excerpt - bootstrap_catalog_skin

--- skins/bootstrap_catalog_skin/_item_content.inc.php
+++ skins/bootstrap_catalog_skin/_item_content.inc.php
@@ -162,36 +162,36 @@ switch( $content_mode )
 		// Compact display:
 		echo $params['content_start_excerpt'];
 
-		if( !empty($params['excerpt_image_size']) && !empty($params['excerpt_image_limit']) )
-		{
+		// if( !empty($params['excerpt_image_size']) && !empty($params['excerpt_image_limit']) )
+		// {
 			// Display images that are linked to this post:
-			$Item->images( array(
-					'before'              => $params['before_images'],
-					'before_image'        => $params['before_image'],
-					'before_image_legend' => $params['before_image_legend'],
-					'after_image_legend'  => $params['after_image_legend'],
-					'after_image'         => $params['after_image'],
-					'after'               => $params['after_images'],
-					'image_class'         => $params['excerpt_image_class'],
-					'image_size'          => $params['excerpt_image_size'],
-					'limit'               => $params['excerpt_image_limit'],
-					'image_link_to'       => $params['excerpt_image_link_to'],
-					'before_gallery'      => $params['before_gallery'],
-					'after_gallery'       => $params['after_gallery'],
-					'gallery_table_start' => $params['gallery_table_start'],
-					'gallery_table_end'   => $params['gallery_table_end'],
-					'gallery_row_start'   => $params['gallery_row_start'],
-					'gallery_row_end'     => $params['gallery_row_end'],
-					'gallery_cell_start'  => $params['gallery_cell_start'],
-					'gallery_cell_end'    => $params['gallery_cell_end'],
-					'gallery_image_size'  => $params['gallery_image_size'],
-					'gallery_image_limit' => $params['gallery_image_limit'],
-					'gallery_colls'       => $params['gallery_colls'],
-					'gallery_order'       => $params['gallery_order'],
+			// $Item->images( array(
+					// 'before'              => $params['before_images'],
+					// 'before_image'        => $params['before_image'],
+					// 'before_image_legend' => $params['before_image_legend'],
+					// 'after_image_legend'  => $params['after_image_legend'],
+					// 'after_image'         => $params['after_image'],
+					// 'after'               => $params['after_images'],
+					// 'image_class'         => $params['excerpt_image_class'],
+					// 'image_size'          => $params['excerpt_image_size'],
+					// 'limit'               => $params['excerpt_image_limit'],
+					// 'image_link_to'       => $params['excerpt_image_link_to'],
+					// 'before_gallery'      => $params['before_gallery'],
+					// 'after_gallery'       => $params['after_gallery'],
+					// 'gallery_table_start' => $params['gallery_table_start'],
+					// 'gallery_table_end'   => $params['gallery_table_end'],
+					// 'gallery_row_start'   => $params['gallery_row_start'],
+					// 'gallery_row_end'     => $params['gallery_row_end'],
+					// 'gallery_cell_start'  => $params['gallery_cell_start'],
+					// 'gallery_cell_end'    => $params['gallery_cell_end'],
+					// 'gallery_image_size'  => $params['gallery_image_size'],
+					// 'gallery_image_limit' => $params['gallery_image_limit'],
+					// 'gallery_colls'       => $params['gallery_colls'],
+					// 'gallery_order'       => $params['gallery_order'],
 					// Optionally restrict to files/images linked to specific position: 'teaser'|'teaserperm'|'teaserlink'|'aftermore'|'inline'|'cover'
-					'restrict_to_image_position' => $teaser_image_positions,
-				) );
-		}
+					// 'restrict_to_image_position' => $teaser_image_positions,
+				// ) );
+		// }
 
 		/** FOR NOW EXCERPT EXCLUDED ENTIRELY - MAKE CUSTOM OPTION FOR THIS TO APPEAR ON DISP=FRONT ==================================================================== */
 		// $Item->excerpt( array(
@@ -215,36 +215,36 @@ switch( $content_mode )
 		// Normal dislpay:  (and Full display if force_more is true)
 		echo $params['content_start_full'];
 
-		if( ! empty($params['image_size']) )
-		{
+		// if( ! empty($params['image_size']) )
+		// {
 			// Display images that are linked to this post:
-			$Item->images( array(
-					'before'              => $params['before_images'],
-					'before_image'        => $params['before_image'],
-					'before_image_legend' => $params['before_image_legend'],
-					'after_image_legend'  => $params['after_image_legend'],
-					'after_image'         => $params['after_image'],
-					'after'               => $params['after_images'],
-					'image_class'         => $params['image_class'],
-					'image_size'          => $params['image_size'],
-					'limit'               => $params['image_limit'],
-					'image_link_to'       => $params['image_link_to'],
-					'before_gallery'      => $params['before_gallery'],
-					'after_gallery'       => $params['after_gallery'],
-					'gallery_table_start' => $params['gallery_table_start'],
-					'gallery_table_end'   => $params['gallery_table_end'],
-					'gallery_row_start'   => $params['gallery_row_start'],
-					'gallery_row_end'     => $params['gallery_row_end'],
-					'gallery_cell_start'  => $params['gallery_cell_start'],
-					'gallery_cell_end'    => $params['gallery_cell_end'],
-					'gallery_image_size'  => $params['gallery_image_size'],
-					'gallery_image_limit' => $params['gallery_image_limit'],
-					'gallery_colls'       => $params['gallery_colls'],
-					'gallery_order'       => $params['gallery_order'],
+			// $Item->images( array(
+					// 'before'              => $params['before_images'],
+					// 'before_image'        => $params['before_image'],
+					// 'before_image_legend' => $params['before_image_legend'],
+					// 'after_image_legend'  => $params['after_image_legend'],
+					// 'after_image'         => $params['after_image'],
+					// 'after'               => $params['after_images'],
+					// 'image_class'         => $params['image_class'],
+					// 'image_size'          => $params['image_size'],
+					// 'limit'               => $params['image_limit'],
+					// 'image_link_to'       => $params['image_link_to'],
+					// 'before_gallery'      => $params['before_gallery'],
+					// 'after_gallery'       => $params['after_gallery'],
+					// 'gallery_table_start' => $params['gallery_table_start'],
+					// 'gallery_table_end'   => $params['gallery_table_end'],
+					// 'gallery_row_start'   => $params['gallery_row_start'],
+					// 'gallery_row_end'     => $params['gallery_row_end'],
+					// 'gallery_cell_start'  => $params['gallery_cell_start'],
+					// 'gallery_cell_end'    => $params['gallery_cell_end'],
+					// 'gallery_image_size'  => $params['gallery_image_size'],
+					// 'gallery_image_limit' => $params['gallery_image_limit'],
+					// 'gallery_colls'       => $params['gallery_colls'],
+					// 'gallery_order'       => $params['gallery_order'],
 					// Optionally restrict to files/images linked to specific position: 'teaser'|'teaserperm'|'teaserlink'|'aftermore'|'inline'|'cover'
-					'restrict_to_image_position' => $teaser_image_positions,
-				) );
-		}
+					// 'restrict_to_image_position' => $teaser_image_positions,
+				// ) );
+		// }
 
 		if( $params['content_display_full'] )
 		{	// We want to display text, not just images:
