b835e5a810eecc91f14dab7a54163277aac2e97c
Francois Planque <github@fplanque.net>
2017-02-14 22:16:31 +0100

    Version bump

--- conf/_application.php
+++ conf/_application.php
@@ -15,13 +15,13 @@ $app_shortname = 'b2evo';
  * Note: This has to be compatible with {@link http://us2.php.net/en/version-compare}.
  * @global string
  */
-$app_version = '6.8.6-stable';
+$app_version = '6.8.7-stable';
 
 /**
  * Release date (ISO)
  * @global string
  */
-$app_date = '2017-02-01';
+$app_date = '2017-02-14';
 
 /**
  * Long version string for checking differences
