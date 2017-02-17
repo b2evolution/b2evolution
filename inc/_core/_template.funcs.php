9c32f6503f5e9e062a53df117d7b51119a322516
yurabakhtin <yurybakh@gmail.com>
2017-02-17 13:58:03 +0300

    Update jquery plugin "Autocomplete for Textarea" to use several textareas on one page
    # Conflicts:
    #	conf/_application.php
    #	inc/_core/_template.funcs.php
    #	rsc/customized/jquery/textcomplete/css/jquery.textcomplete.css

--- inc/_core/_template.funcs.php
+++ inc/_core/_template.funcs.php
@@ -3318,7 +3318,6 @@ function init_autocomplete_usernames_js( $relative_to = 'rsc_url' )
 
 	require_js( '#jquery#', $relative_to );
 	require_js( 'build/textcomplete.bmin.js', $relative_to );
-	require_css( 'customized:jquery/textcomplete/css/jquery.textcomplete.css', $relative_to );
 }
 
 
