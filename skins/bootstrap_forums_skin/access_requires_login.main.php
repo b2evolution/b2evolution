bbe5fc14526dc22e026f892573c3a1a1744fbe5a
Francois Planque <github@fplanque.net>
2017-03-02 21:59:11 +0100

    Revert "Set footer widget template params on bootstrap skins"
    This reverts commit e958c236162e43e625d6a76fe7de92efa28e8869.

--- skins/bootstrap_forums_skin/access_requires_login.main.php
+++ skins/bootstrap_forums_skin/access_requires_login.main.php
@@ -180,19 +180,10 @@ if( $Skin->show_container_when_access_denied( 'menu' ) )
 		<div class="evo_container evo_container__footer">
 		<?php
 			// Display container and contents:
-			skin_container( NT_('Footer'), array(
+			skin_container( NT_("Footer"), array(
 					// The following params will be used as defaults for widgets included in this container:
-					'block_start'         => '<span class="evo_widget $wi_class$">',
-					'block_end'           => '</span> ',
-					'block_display_title' => false,
-					'list_start'          => '',
-					'list_end'            => '',
-					'item_start'          => '',
-					'item_end'            => '',
-					'item_selected_start' => '',
-					'item_selected_end'   => '',
-					'link_default_class'  => 'btn btn-default btn-sm',
-					'link_selected_class' => 'btn btn-default btn-sm active',
+					'block_start'       => '<div class="evo_widget $wi_class$">',
+					'block_end'         => '</div>',
 				) );
 			// Note: Double quotes have been used around "Footer" only for test purposes.
 		?>
