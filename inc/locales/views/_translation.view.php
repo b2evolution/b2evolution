dbffe3e51c38885e51b793f5ef28921e652687ac
Erwin Rommel H. Satingin <winskie@gmail.com>
2017-04-03 08:55:28 +0800

    Added filter to original and translated string. Added checkbox to view untranslated string only.

--- inc/locales/views/_translation.view.php
+++ inc/locales/views/_translation.view.php
@@ -17,11 +17,16 @@ if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.'
 /**
  * @var User
  */
-global $current_User;
+global $admin_url, $current_User;
 
 global $edit_locale;
 
+$original_string = param( 'original', 'string', '', true );
+$translated_string = param( 'translated', 'string', '', true );
+$untranslated_only = param( 'untranslated_only', 'boolean', 0, true );
+
 $Form = new Form();
+$Form->formclass = 'form-inline';
 
 $Form->begin_form( 'fform' );
 
@@ -29,19 +34,53 @@ $Form->add_crumb( 'translation' );
 $Form->hidden( 'ctrl', 'translation' );
 $Form->hidden( 'edit_locale', $edit_locale );
 
-// Create query
-$SQL = new SQL();
-$SQL->SELECT( 'itst_ID, iost_string, itst_standard' );
-$SQL->FROM( 'T_i18n_original_string' );
-$SQL->FROM_add( 'RIGHT OUTER JOIN T_i18n_translated_string ON iost_ID = itst_iost_ID' );
-$SQL->WHERE( 'itst_locale = '.$DB->quote( $edit_locale ) );
-$SQL->ORDER_BY( '*, iost_string' );
+if( $untranslated_only )
+{
+	// Create query
+	$SQL = new SQL();
+	$SQL->SELECT( ' NULL as itst_ID, iost_string' );
+	$SQL->FROM( 'T_i18n_original_string' );
+	$SQL->WHERE( 'NOT EXISTS ( SELECT 1 FROM T_i18n_translated_string WHERE itst_iost_ID = iost_ID AND itst_locale = '.$DB->quote( $edit_locale ).')' );
+	$SQL->ORDER_BY( 'iost_string' );
 
-// Create a count sql
-$count_SQL = new SQL();
-$count_SQL->SELECT( 'SQL_NO_CACHE COUNT( itst_ID )' );
-$count_SQL->FROM( 'T_i18n_translated_string' );
-$count_SQL->WHERE( 'itst_locale = '.$DB->quote( $edit_locale ) );
+	// Create a count sql
+	$count_SQL = new SQL();
+	$count_SQL->SELECT( 'SQL_NO_CACHE COUNT( itst_ID )' );
+	$count_SQL->FROM( 'T_i18n_translated_string' );
+	$count_SQL->WHERE( 'NOT EXISTS ( SELECT 1 FROM T_i18n_translated_string WHERE itst_iost_ID = iost_ID AND itst_locale = '.$DB->quote( $edit_locale ).')' );
+}
+else
+{
+	// Create query
+	$SQL = new SQL();
+	$SQL->SELECT( 'itst_ID, iost_string, itst_standard' );
+	$SQL->FROM( 'T_i18n_original_string' );
+	$SQL->FROM_add( 'RIGHT OUTER JOIN T_i18n_translated_string ON iost_ID = itst_iost_ID' );
+	$SQL->WHERE( 'itst_locale = '.$DB->quote( $edit_locale ) );
+	$SQL->ORDER_BY( '*, iost_string' );
+
+	// Create a count sql
+	$count_SQL = new SQL();
+	$count_SQL->SELECT( 'SQL_NO_CACHE COUNT( itst_ID )' );
+	$count_SQL->FROM( 'T_i18n_translated_string' );
+	$count_SQL->WHERE( 'itst_locale = '.$DB->quote( $edit_locale ) );
+
+	if( ! empty( $translated_string ) )
+	{
+		$SQL->add_search_field( 'itst_standard' );
+		$SQL->WHERE_kw_search( $translated_string, 'AND' );
+		$count_SQL->add_search_field( 'itst_standard' );
+		$count_SQL->WHERE_kw_search( $translated_string, 'AND' );
+	}
+}
+
+if( ! empty( $original_string ) )
+{
+	$SQL->add_search_field( 'iost_string' );
+	$SQL->WHERE_kw_search( $original_string, 'AND' );
+	$count_SQL->add_search_field( 'iost_string' );
+	$count_SQL->WHERE_kw_search( $original_string, 'AND' );
+}
 
 // Create result set:
 $Results = new Results( $SQL->get(), 'itst_', '-A'/*, NULL, $count_SQL->get()*/ );
@@ -50,24 +89,62 @@ $Results->title = sprintf( T_('Translation editor for locale "%s"'), $edit_local
 
 $Results->global_icon( T_('Add new translated string...'), 'new', regenerate_url( '', 'action=new_strings' ), T_('Add new translated string').' &raquo;', 3, 4, array( 'class' => 'action_icon btn-primary' ) );
 
+
+/**
+ * Callback to add filters on top of the result set
+ *
+ * @param Form
+ */
+function filter_translation( & $Form )
+{
+	$Form->text( 'original', get_param( 'original' ), 20, T_('Original string') );
+	if( get_param( 'untranslated_only' ) )
+	{
+		$Form->text_input( 'translated', null, 20, T_('Translated string'), '', array( 'disabled' => 'disabled' ) );
+	}
+	else
+	{
+		$Form->text_input( 'translated', get_param( 'translated' ), 20, T_('Translated string') );
+	}
+	$Form->checkbox( 'untranslated_only', get_param( 'untranslated_only' ), T_('Show only untranslated strings') );
+}
+
+$Results->filter_area = array(
+	'callback' => 'filter_translation',
+	'presets' => array(
+		'all' => array( T_('All'), $admin_url.'?ctrl=translation&edit_locale='.$edit_locale ),
+		)
+	);
+
 $Results->cols[] = array(
 		'th' => T_('Original string'),
 		'order' => 'iost_string',
 		'td' => '%htmlspecialchars( #iost_string# )%',
 	);
 
-$Results->cols[] = array(
-		'th' => T_('Translated string'),
-		'order' => 'itst_standard',
-		'td' => '%htmlspecialchars( #itst_standard# )%',
-	);
+if( ! $untranslated_only )
+{
+	$Results->cols[] = array(
+			'th' => T_('Translated string'),
+			'order' => 'itst_standard',
+			'td' => '%htmlspecialchars( #itst_standard# )%',
+		);
+}
 
 function iost_td_actions( $translated_string_ID )
 {
-	$r = action_icon( T_('Edit this string...'), 'edit',
-										regenerate_url( 'action', 'itst_ID='.$translated_string_ID.'&amp;action=edit' ) );
-	$r .= action_icon( T_('Delete this string!'), 'delete',
-										regenerate_url( 'action', 'itst_ID='.$translated_string_ID.'&amp;action=delete&amp;'.url_crumb('translation') ) );
+	if( is_null( $translated_string_ID ) )
+	{
+		$r = action_icon( T_('Translate this string...'), 'add',
+										regenerate_url( 'action', 'iost_ID='.$translated_string_ID.'&amp;action=new' ) );
+	}
+	else
+	{
+		$r = action_icon( T_('Edit this string...'), 'edit',
+											regenerate_url( 'action', 'itst_ID='.$translated_string_ID.'&amp;action=edit' ) );
+		$r .= action_icon( T_('Delete this string!'), 'delete',
+											regenerate_url( 'action', 'itst_ID='.$translated_string_ID.'&amp;action=delete&amp;'.url_crumb('translation') ) );
+	}
 
 	return $r;
 }
@@ -98,3 +175,19 @@ if( $current_User->check_perm( 'options', 'edit' ) )
 }
 
 ?>
+
+<script type="text/javascript">
+	jQuery( '#untranslated_only' ).on( 'change', function()
+		{
+			var translated_string_input = jQuery( '#translated' );
+			if( jQuery( this ).is( ':checked' ) )
+			{
+				translated_string_input.attr( 'disabled',  'disabled' );
+				translated_string_input.val( null );
+			}
+			else
+			{
+				translated_string_input.removeAttr( 'disabled' );
+			}
+		} );
+</script>
