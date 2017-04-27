ba97775e1ec30e5a35c5456555ee4ed361778508
yurabakhtin <yurybakh@gmail.com>
2017-04-27 10:00:45 +0300

    Modify form to edit item custom fields

--- install/_functions_create.php
+++ install/_functions_create.php
@@ -397,13 +397,13 @@ function create_default_data()
 	// Insert item types:
 	$DB->query( $post_types_sql );
 
-	$DB->query( 'INSERT INTO T_items__type_custom_field ( itcf_ityp_ID, itcf_label, itcf_name, itcf_type )
-			VALUES ( 3, "First numeric field", "first_numeric_field", "double" ),
-						 ( 3, "Second numeric field", "second_numeric_field", "double" ),
-						 ( 3, "First string field", "first_string_field", "varchar" ),
-						 ( 3, "Define you own labels", "define_you_own_labels", "varchar" ),
-						 ( 3, "Multiline plain text field", "multiline_plain_text_field", "text" ),
-						 ( 3, "Multiline HTML field", "multiline_html_field", "html" )' );
+	$DB->query( 'INSERT INTO T_items__type_custom_field ( itcf_ityp_ID, itcf_label, itcf_name, itcf_type, itcf_order, itcf_note )
+			VALUES ( 3, "First numeric field", "first_numeric_field", "double", 1, "Enter a number" ),
+						 ( 3, "Second numeric field", "second_numeric_field", "double", 3, "Enter a number" ),
+						 ( 3, "First string field", "first_string_field", "varchar", 2, "Enter a string" ),
+						 ( 3, "Define you own labels", "define_you_own_labels", "varchar", 4, "Define your own notes" ),
+						 ( 3, "Multiline plain text field", "multiline_plain_text_field", "text", 6, "Enter multiple lines" ),
+						 ( 3, "Multiline HTML field", "multiline_html_field", "html", 5, "Enter HTML code" )' );
 	task_end();
 
 
