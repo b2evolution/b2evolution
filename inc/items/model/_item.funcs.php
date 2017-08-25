9c278566fd3a4bb6c40a4ab0f3eaeac8966b4ba5
Francois Planque <github@fplanque.net>
2017-08-25 18:43:30 +0200

    Show seconds for easier debugging in last touched / last content update dates

--- inc/items/model/_item.funcs.php
+++ inc/items/model/_item.funcs.php
@@ -3386,7 +3386,7 @@ function echo_comment( $Comment, $redirect_to = NULL, $save_context = false, $in
 
 			// Last touched date:
 			echo ' <span class="text-nowrap">&middot; '.T_('Last touched').': '
-				.mysql2date( locale_datefmt().' @ '.locale_shorttimefmt(), $Comment->get( 'last_touched_ts' ) ).'</span>';
+				.mysql2date( locale_datefmt().' @ '.locale_timefmt(), $Comment->get( 'last_touched_ts' ) ).'</span>';
 
 			echo '</div>';
 			echo '<div style="padding-top:3px">';
