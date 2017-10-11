0071af6c45a83a9f947b069220117b88d2723a17
yurabakhtin <yurybakh@gmail.com>
2017-10-11 10:00:33 +0300

    Fix widget desinger mode for secondary area of bootstrap_main_skin

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '7.0.0-alpha';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2017-10-10';
+$app_date = '2017-10-11';
 
 /**
  * Long version string for checking differences
