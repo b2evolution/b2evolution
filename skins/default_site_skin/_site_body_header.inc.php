776236c65e1dc3093f940ed11d919a0ec81201dc
yurabakhtin <yurybakh@gmail.com>
2017-10-13 15:18:04 +0300

    Small fix for site header on small screens

--- skins/default_site_skin/_site_body_header.inc.php
+++ skins/default_site_skin/_site_body_header.inc.php
@@ -14,13 +14,13 @@ global $baseurl, $Settings;
 ?>
 
 <nav class="sitewide_header">
-
 <?php
+	// ------------------------- "Site Header" CONTAINER EMBEDDED HERE --------------------------
 	widget_container( 'site_header', array(
 			// The following params will be used as defaults for widgets included in this container:
 			'container_display_if_empty' => true, // Display container anyway even if no widget
 			'container_start'     => '<div class="evo_container $wico_class$">',
-			'container_end'       => '</div>',
+			'container_end'       => '<label for="nav-trigger"></label></div>',
 			'block_start'         => '<span class="evo_widget $wi_class$">',
 			'block_end'           => '</span>',
 			'block_display_title' => false,
@@ -33,97 +33,15 @@ global $baseurl, $Settings;
 			'link_selected_class' => 'swhead_item swhead_item_selected',
 			'link_default_class'  => 'swhead_item',
 		) );
-/*
-$notification_logo_file_ID = intval( $Settings->get( 'notification_logo_file_ID' ) );
-if( $notification_logo_file_ID > 0 &&
-    ( $FileCache = & get_FileCache() ) &&
-    ( $File = $FileCache->get_by_ID( $notification_logo_file_ID, false ) ) &&
-    $File->is_image() )
-{	// Display site logo image if the file exists in DB and it is an image:
-	$site_title = $Settings->get( 'notification_long_name' ) != '' ? ' title="'.$Settings->dget( 'notification_long_name', 'htmlattr' ).'"' : '';
-	$site_name_text = '<img src="'.$File->get_url().'" alt="'.$Settings->dget( 'notification_short_name', 'htmlattr' ).'"'.$site_title.' />';
-	$site_title_class = ' swhead_logo';
-}
-else
-{	// Display only short site name if the logo file cannot be used by some reason above:
-	$site_name_text = $Settings->get( 'notification_short_name' );
-	$site_title_class = '';
-}
+	// ----------------------------- END OF "Site Header" CONTAINER -----------------------------
 ?>
-<a href="<?php echo $baseurl; ?>" class="swhead_sitename<?php echo $site_title_class; ?>"><?php echo $site_name_text; ?></a>
+	<div class="clear"></div>
+</nav>
 
-<?php
-
-	// --------------------------------- START OF COLLECTION LIST --------------------------------
-	// Call widget directly (without container):
-	skin_widget( array(
-						// CODE for the widget:
-						'widget' => 'colls_list_public',
-						// Optional display params
-						'block_start' => '',
-						'block_end' => '',
-						'block_display_title' => false,
-						'list_start' => '',
-						'list_end' => '',
-						'item_start' => '',
-						'item_end' => '',
-						'item_selected_start' => '',
-						'item_selected_end' => '',
-						'link_selected_class' => 'swhead_item swhead_item_selected',
-						'link_default_class' => 'swhead_item ',
-				) );
-	// ---------------------------------- END OF COLLECTION LIST ---------------------------------
-
-	if( $Settings->get( 'info_blog_ID' ) > 0 )
-	{ // We have a collection for info pages:
-		// --------------------------------- START OF PAGES LIST --------------------------------
-		// Call widget directly (without container):
-		skin_widget( array(
-						// CODE for the widget:
-						'widget' => 'coll_page_list',
-						// Optional display params
-						'block_start' => '',
-						'block_end' => '',
-						'block_display_title' => false,
-						'list_start' => '',
-						'list_end' => '',
-						'item_start' => '',
-						'item_end' => '',
-						'item_selected_start' => '',
-						'item_selected_end' => '',
-						'link_selected_class' => 'swhead_item swhead_item_nav_md swhead_item_selected',
-						'link_default_class' => 'swhead_item swhead_item_nav_md ',
-						'blog_ID' => $Settings->get( 'info_blog_ID' ),
-						'item_group_by' => 'none',
-						'order_by' => 'order',		// Order (as explicitly specified)
-				) );
-		// ---------------------------------- END OF PAGES LIST ---------------------------------
-	}
-
-	// --------------------------------- START OF CONTACT LINK --------------------------------
-	// Call widget directly (without container):
-	skin_widget( array(
-						// CODE for the widget:
-						'widget' => 'basic_menu_link',
-						// Optional display params
-						'block_start' => '',
-						'block_end' => '',
-						'block_display_title' => false,
-						'list_start' => '',
-						'list_end' => '',
-						'item_start' => '',
-						'item_end' => '',
-						'item_selected_start' => '',
-						'item_selected_end' => '',
-						'link_selected_class' => 'swhead_item swhead_item_nav_md swhead_item_selected',
-						'link_default_class' => 'swhead_item swhead_item_nav_md ',
-						'link_type' => 'ownercontact',
-				) );
-	// --------------------------------- END OF CONTACT LINK --------------------------------
-?>
-
-	<div class="floatright">
-	<?php
+<input type="checkbox" id="nav-trigger" class="nav-trigger">
+<div class="sitewide_header_menu_wrapper">
+	<ul class="sitewide_header_menu">
+		<?php
 		// Optional display params for widgets below
 		$right_menu_params = array(
 				'block_start' => '',
@@ -139,72 +57,6 @@ else
 				'link_default_class' => 'swhead_item ',
 			);
 
-		if( is_logged_in() )
-		{ // Display the following menus when current user is logged in
-
-			// Profile link:
-			// Call widget directly (without container):
-			skin_widget( array_merge( $right_menu_params, array(
-				// CODE for the widget:
-				'widget' => 'profile_menu_link',
-				// Optional display params
-				'profile_picture_size' => 'crop-top-32x32',
-			) ) );
-
-			// Messaging link:
-			// Call widget directly (without container):
-			skin_widget( array_merge( $right_menu_params, array(
-				// CODE for the widget:
-				'widget' => 'msg_menu_link',
-				// Optional display params
-				'link_type' => 'messages',
-				'link_selected_class' => 'swhead_item swhead_item_nav_sm swhead_item_selected',
-				'link_default_class' => 'swhead_item swhead_item_nav_sm'
-			) ) );
-
-			// Logout link:
-			// Call widget directly (without container):
-			skin_widget( array_merge( $right_menu_params, array(
-				// CODE for the widget:
-				'widget' => 'basic_menu_link',
-				// Optional display params
-				'link_type' => 'logout',
-			) ) );
-		}
-		else
-		{ // Display the following menus when current user is NOT logged in
-
-			// Login link:
-			// Call widget directly (without container):
-			skin_widget( array_merge( $right_menu_params, array(
-				// CODE for the widget:
-				'widget' => 'basic_menu_link',
-				// Optional display params
-				'link_type' => 'login',
-				'link_default_class' => 'swhead_item_login '.$right_menu_params['link_default_class']
-			) ) );
-
-			// Register link:
-			// Call widget directly (without container):
-			skin_widget( array_merge( $right_menu_params, array(
-				// CODE for the widget:
-				'widget' => 'basic_menu_link',
-				// Optional display params
-				'link_type' => 'register',
-				'link_selected_class' => 'swhead_item_white '.$right_menu_params['link_selected_class'],
-				'link_default_class' => 'swhead_item_white '.$right_menu_params['link_default_class'],
-			) ) );
-		}
-	?>
-	</div>*/ ?>
-	<label for="nav-trigger"></label>
-	<div class="clear"></div>
-</nav>
-
-<input type="checkbox" id="nav-trigger" class="nav-trigger">
-<div class="sitewide_header_menu_wrapper">
-	<ul class="sitewide_header_menu">
-		<?php
 		// --------------------------------- START OF BLOG LIST --------------------------------
 		// Call widget directly (without container):
 		skin_widget( array(
