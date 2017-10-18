96c62c82f1ac087bc28f8058cddd14d818b3a7c4
Erwin Rommel H. Satingin <winskie@gmail.com>
2017-10-18 08:39:30 +0800

    Fixed iver_edit_user_ID values of generated sample post revisions

--- inc/tools/model/_tool.funcs.php
+++ inc/tools/model/_tool.funcs.php
@@ -323,8 +323,18 @@ function tool_create_sample_revisions( $blog_ID, $min_revisions = 1, $max_revisi
 		for( $i = 0; $i < $num_revisions; $i++ )
 		{
 			$iver_ID += $i;
+
+			if( $i === 0 )
+			{ // Original author
+				$editor_user_id = 'post_lastedit_user_ID';
+			}
+			else
+			{
+				$editor_user_id = $editor_Users[rand( 0, $editors_count - 1 )];
+			}
+
 			$sql = 'INSERT INTO T_items__version( iver_ID, iver_itm_ID, iver_edit_user_ID, iver_edit_datetime, iver_status, iver_title, iver_content )
-				SELECT "'.$iver_ID.'" AS iver_ID, post_ID, '.rand( 0, $editors_count - 1 ).', post_datemodified, post_status, CONCAT( post_title, " - revision '.( $i + 1 ).'" ), post_content
+				SELECT "'.$iver_ID.'" AS iver_ID, post_ID, '.$editor_user_id.', post_datemodified, post_status, CONCAT( post_title, " - revision '.( $i + 1 ).'" ), post_content
 					FROM T_items__item
 				WHERE post_ID = '.$Item->ID;
 
