d15cb83cc07078b31f473de5d22a2fc4a32b098d
yurabakhtin <yurybakh@gmail.com>
2017-10-24 19:04:09 +0300

    Add blue label for widgets icons on list of adding new widget

--- inc/widgets/views/_widget_list_available.view.php
+++ inc/widgets/views/_widget_list_available.view.php
@@ -185,7 +185,7 @@ foreach( $widget_groups as $widget_group_code => $widget_group_title )
 
 			echo '<li>';
 			echo '<a href="'.regenerate_url( '', 'action=create&amp;type=core&amp;code='.$ComponentWidget->code.$mode_url_param.'&amp;'.url_crumb( 'widget' ) ).'" title="'.T_('Add this widget to the container').'">';
-			echo '<span class="fa fa-'.$ComponentWidget->icon.'"></span> <strong>'.$ComponentWidget->get_name().'</strong>';
+			echo '<span class="badge badge-info">'.$ComponentWidget->get_icon().'</span> <strong>'.$ComponentWidget->get_name().'</strong>';
 			echo '</a> <span class="notes">'.$ComponentWidget->get_desc().'</span> '.$ComponentWidget->get_help_link( 'manual', false );
 			echo '</li>';
 		}
@@ -198,7 +198,7 @@ foreach( $widget_groups as $widget_group_code => $widget_group_title )
 		{
 			echo '<li>';
 			echo '<a href="'.regenerate_url( '', 'action=create&amp;type=plugin&amp;code='.$Plugin->code.$mode_url_param.'&amp;'.url_crumb( 'widget' ) ).'" title="'.T_('Add this widget to the container').'">';
-			echo '<span class="fa fa-'.$Plugin->widget_icon.'"></span> <strong>'.$Plugin->name.'</strong>';
+			echo '<span class="badge badge-info"><span class="fa fa-'.$Plugin->widget_icon.'"></span></span> <strong>'.$Plugin->name.'</strong>';
 			echo '</a> <span class="notes">'.$Plugin->short_desc.'</span> '.$Plugin->get_help_link( '$widget_url', 'manual', false );
 			echo '</li>';
 		}
