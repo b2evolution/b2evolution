0023c09b76abe41f67f5e328c3dd54c2df5c61f5
yurabakhtin <yurybakh@gmail.com>
2017-11-30 15:03:11 +0300

    Changes for plugin param option 'allow_empty'

--- inc/plugins/_plugin.funcs.php
+++ inc/plugins/_plugin.funcs.php
@@ -977,8 +977,11 @@ function autoform_set_param_from_request( $parname, $parmeta, & $Obj, $set_type,
 
 	if( isset( $parmeta['type'] ) && $parmeta['type'] == 'integer' )
 	{	// Convert to correct integer value:
-		$l_value = intval( $l_value );
-		if( $l_value == 0 && ! empty( $parmeta['allow_empty'] ) &&
+		if( $l_value !== '' )
+		{	// Don't convert empty string '' to integer 0:
+			$l_value = intval( $l_value );
+		}
+		if( empty( $l_value ) && ! empty( $parmeta['allow_empty'] ) &&
 				isset( $parmeta['valid_range'], $parmeta['valid_range']['min'] ) && $parmeta['valid_range']['min'] > 0 )
 		{	// Convert 0 to empty value for integer field if it allows empty values:
 			$l_value = NULL;
@@ -1153,6 +1156,14 @@ function autoform_validate_param_value( $param_name, $value, $meta )
 
 		switch( $meta['type'] )
 		{
+			case 'text':
+				if( isset( $meta['allow_empty'] ) && ! $meta['allow_empty'] && $value === '' )
+				{	// Display error if the text field is required to be not empty:
+					param_error( $param_name, sprintf( T_('The field &laquo;%s&raquo; cannot be empty.'), $meta['label'] ), T_('This field cannot be empty.') );
+					return false;
+				}
+				break;
+
 			case 'integer':
 				if( ! preg_match( '~^[-+]?\d+$~', $value ) )
 				{
