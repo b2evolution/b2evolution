c07b9c439bccdad3fa509a9add8085ce94801927
midnight-studios <jacques.erich.joubert@gmail.com>
2017-12-01 08:29:23 +0200

    Reverted: Get default value for Nested plugin fields

--- inc/plugins/_plugin.funcs.php
+++ inc/plugins/_plugin.funcs.php
@@ -458,40 +458,6 @@ function autoform_display_field( $parname, $parmeta, & $Form, $set_type, $Obj, $
 						else
 						{	// Use default value if it is defined:
 							$l_value = isset( $l_set_entry['defaultvalue'] ) ? $l_set_entry['defaultvalue'] : NULL;
-							/*
-							*	Use default value if it is defined:
-							*	Updated here: https://github.com/b2evolution/b2evolution/commit/30b6063c3383b6b20c75fd130681a3cca7469195
-							*	but not loading default values for items for type 'input_group' when nested
-							*	this is because key['defaultvalue'] is found deeper in array ie:
-							*
-							*	
-							*	array(3) {
-							*	  ["label"]=>
-							*	  string(3) "label name"
-							*	  ["type"]=>
-							*	  string(11) "input_group" 	<--- we will have a sub set
-							*	  ["inputs"]=>  		<--- sub set array
-							*	  array(1) {
-							*	    ["_color"]=>
-							*	    array(3) {
-							*	      ["label"]=>
-							*	      string(13) "Color Label"
-							*	      ["defaultvalue"]=>
-							*	      string(7) "#fed136" 	<----- This is our default value
-							*	      ["type"]=>
-							*	      string(5) "color"
-							*	    }
-							*	  }
-							*	}
-							*	
-							*/
-							if( ! empty( $l_set_entry['inputs'] ) && is_array( $l_set_entry['inputs'] ) )
-							{
-								foreach( $l_set_entry['inputs'] as $l_set_name => $l_set_entry )
-								{
-									$l_value = isset( $l_set_entry['defaultvalue'] ) ? $l_set_entry['defaultvalue'] : NULL;
-								}
-							}
 						}
 						// RECURSE:
 						autoform_display_field( $parname.'['.$k_nb.']['.$l_set_name.']', $l_set_entry, $Form, $set_type, $Obj, $set_target, $l_value );
