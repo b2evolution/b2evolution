5bd09241b5374456ead1220cdd8b7003fbe76fc4
Francois Planque <github@fplanque.net>
2017-12-08 05:29:47 +0100

    Version bump to 6.10.0-beta

--- conf/_application.php
+++ conf/_application.php
@@ -15,7 +15,7 @@ $app_shortname = 'b2evo';
  * Note: This has to be compatible with {@link http://us2.php.net/en/version-compare}.
  * @global string
  */
-$app_version = '6.9.5-stable';
+$app_version = '6.10.0-beta';
 
 /**
  * Release date (ISO)
--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 
 # b2evolution CMS
 
-This is b2evolution CMS version **6.9.5**.
+This is b2evolution CMS version **6.10.0-beta**.
 
 ## A complete engine for your website !
 
