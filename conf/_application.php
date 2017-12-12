4fe513c6fe1131548d5012886f6c3e1a5507c8f2
yurabakhtin <yurybakh@gmail.com>
2017-12-12 08:15:57 +0300

    Fix version

--- conf/_application.php
+++ conf/_application.php
@@ -15,7 +15,7 @@ $app_shortname = 'b2evo';
  * Note: This has to be compatible with {@link http://us2.php.net/en/version-compare}.
  * @global string
  */
-$app_version = '6.10.0-beta';
+$app_version = '7.0.0-alpha';
 
 /**
  * Release date (ISO)
