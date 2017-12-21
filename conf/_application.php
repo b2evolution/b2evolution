0a8795c992461d8c611099ad8ba45814bb2eba18
yurabakhtin <yurybakh@gmail.com>
2017-12-21 10:26:20 +0300

    Style fixes in bootstrap_forums_skin

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.9.5-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2017-12-13';
+$app_date = '2017-12-21';
 
 /**
  * Long version string for checking differences
