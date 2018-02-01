585a6d7a6b9b4aeb61040391fd71b809bad42843
Erwin Rommel H. Satingin <winskie@gmail.com>
2018-02-01 13:02:32 +0800

    Fixed incorrect generated email tracking code when URL contains secret_content

--- inc/tools/views/_email_sent_details.view.php
+++ inc/tools/views/_email_sent_details.view.php
@@ -52,7 +52,7 @@ if( !empty( $mail_contents ) )
 	if( !empty( $mail_contents['text'] ) )
 	{ // Display Plain Text content
 		$plain_text_content = preg_replace( '~\$secret_content_start\$.*\$secret_content_end\$~', '***secret-content-removed***', $mail_contents['text']['content'] );
-		$plain_text_content = preg_replace( '~\$secret_email_key_start\$.*\$secret_email_key_end\$~', '***secret-content-removed***', $plain_text_content );
+		$plain_text_content = preg_replace( '~\$email_key_start\$(.*)\$email_key_end\$~', '***prevent-tracking-through-log***$1', $plain_text_content );
 		$plain_text_content = str_replace( array( '$message_body_text_start$', '$message_body_text_end$' ), '', $plain_text_content );
 
 		$Form->info( T_('Text content'), $mail_contents['text']['type']
@@ -63,7 +63,7 @@ if( !empty( $mail_contents ) )
 	{ // Display HTML content
 
 		$html_content = preg_replace( '~\$secret_content_start\$.*\$secret_content_end\$~', '***secret-content-removed***', $mail_contents['html']['content'] );
-		$html_content = preg_replace( '~\$secret_email_key_start\$.*\$secret_email_key_end\$~', '***secret-content-removed***', $html_content );
+		$html_content = preg_replace( '~\$email_key_start\$(.*)\$email_key_end\$~', '***prevent-tracking-through-log***$1', $html_content );
 		$html_content = str_replace( array( '$message_body_html_start$', '$message_body_html_end$' ), '', $html_content );
 
 		if( ! empty( $mail_contents['html']['head_style'] ) )
@@ -77,7 +77,7 @@ if( !empty( $mail_contents ) )
 	}
 }
 $emlog_message = preg_replace( '~\$secret_content_start\$.*\$secret_content_end\$~', '***secret-content-removed***', $MailLog->emlog_message );
-$emlog_message = preg_replace( '~\$secret_email_key_start\$.*\$secret_email_key_end\$~', '***secret-content-removed***', $emlog_message );
+$emlog_message = preg_replace( '~\$email_key_start\$(.*)\$email_key_end\$~', '***prevent-tracking-through-log***$1', $emlog_message );
 $emlog_message = str_replace( array( '$message_body_text_start$', '$message_body_text_end$', '$message_body_html_start$', '$message_body_html_end$' ), '', $emlog_message );
 $Form->info( T_('Raw email source'), '<pre class="email_log_scroll"><span>'.htmlspecialchars( $emlog_message ).'</span></pre>' );
 
