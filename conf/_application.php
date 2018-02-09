6285041b64ffab6fb3341de176ab06201110e19b
Francois Planque <github@fplanque.net>
2018-02-09 17:29:24 +0100

    date bump

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.10.0-beta';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2018-02-08';
+$app_date = '2018-02-09';
 
 /**
  * Long version string for checking differences
