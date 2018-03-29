961dbe28154337fcd8c1874cc9d4e5adf5ffa6db
yurabakhtin <yurybakh@gmail.com>
2018-03-29 11:43:19 +0300

    Fix double margin space between content block and login form on widget container "Login Required"

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.10.1-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2018-03-26';
+$app_date = '2018-03-29';
 
 /**
  * Long version string for checking differences
