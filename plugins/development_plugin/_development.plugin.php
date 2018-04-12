7867d65e4823925b0cd59c4b84c89ad4cc11c461
midnight-studios <jacques.erich.joubert@gmail.com>
2018-04-12 12:35:40 +0200

    Readme File and Test Plugin Added

---
+++ plugins/development_plugin/_development.plugin.php
@@ -0,0 +1,536 @@
+<?php
+/**
+ * -----------------------------------------------------------------------------------------
+ * This file provides a skeleton to create a new {@link http://b2evolution.net/ b2evolution}
+ * plugin quickly.
+ * See also:
+ *  - {@link http://b2evolution.net/man/creating-plugin}
+ *  - {@link http://doc.b2evolution.net/stable/plugins/Plugin.html}
+ * (Delete this first paragraph, of course)
+ * -----------------------------------------------------------------------------------------
+ *
+ * This file implements the Foo Plugin for {@link http://b2evolution.net/}.
+ *
+ * @license GNU GPL v2 - {@link http://b2evolution.net/about/gnu-gpl-license}
+ *
+ * @copyright (c)2010 by Your NAME - {@link http://example.com/}.
+ *
+ * @package plugins
+ *
+ * @author Your NAME
+ */
+if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
+
+
+/**
+ * Foo Plugin
+ *
+ * Your description
+ *
+ * @package plugins
+ */
+class development_plugin extends Plugin
+{
+	/**
+	 * Variables below MUST be overriden by plugin implementations,
+	 * either in the subclass declaration or in the subclass constructor.
+	 */
+	/**
+	 * Human readable plugin name.
+	 */
+	var $name = 'Plugin Name';
+	/**
+	 * Code, if this is a renderer or pingback plugin.
+	 */
+	var $code = 'devplugin';
+	var $priority = 50;
+	var $version = '0.1-dev';
+	var $author = 'http://example.com/';
+	var $help_url = '';
+
+	/**
+	 * Group of the plugin, e.g. "widget", "rendering", "antispam"
+	 */
+	var $group = 'Test';
+
+
+	/**
+	 * Init: This gets called after a plugin has been registered/instantiated.
+	 */
+	function PluginInit( & $params )
+	{
+		$this->short_desc = $this->T_('Short description');
+		$this->long_desc = $this->T_('Longer description. You may also remove this.');
+	}
+	
+	/**
+	 * Define the GLOBAL settings of the plugin here. These can then be edited in the backoffice in System > Plugins.
+	 *
+	 * @see Plugin::GetDefaultSettings()
+	 * @param array Associative array of parameters (since v1.9).
+	 *    'for_editing': true, if the settings get queried for editing;
+	 *                   false, if they get queried for instantiating {@link Plugin::$Settings}.
+	 * @return array see {@link Plugin::GetDefaultSettings()}.
+	 * The array to be returned should define the names of the settings as keys (max length is 30 chars)
+	 * and assign an array with the following keys to them (only 'label' is required):
+	 */
+	function GetDefaultSettings( & $params )
+	{
+		$r = $this->Form_Parts();
+			
+		return $r;
+	}
+	
+	/**
+	 * Define the PER-USER settings of the plugin here. These can then be edited by each user.
+	 *
+	 * You can access them in the plugin through the member object
+	 * {@link $UserSettings}, e.g.:
+	 * <code>$this->UserSettings->get( 'my_param' );</code>
+	 *
+	 * This method behaves exactly like {@link Plugin::GetDefaultSettings()},
+	 * except that it defines user specific settings instead of global settings.
+	 *
+	 * @todo 3.0 fp> 1) This is not an event: RENAME to lowercase (in b2evo 3.0)
+	 * @todo 3.0 fp> 2) This defines more than Default values ::  confusing name
+	 * @todo name tentative get_user_param_definitions()
+	 *
+	 * @see Plugin::GetDefaultUserSettings()
+	 * @param array Associative array of parameters.
+	 *    'for_editing': true, if the settings get queried for editing;
+	 *                   false, if they get queried for instantiating {@link Plugin::$UserSettings}.
+	 * @return array See {@link Plugin::GetDefaultSettings()}.
+	 */
+	function GetDefaultUserSettings( & $params )
+	{
+		$r = $this->Form_Parts();
+			
+		return $r;
+	}
+	
+	/**
+	 * Define here default collection/blog settings that are to be made available in the backoffice.
+	 *
+	 * @param array Associative array of parameters.
+	 * @return array See {@link Plugin::get_coll_setting_definitions()}.
+	 */
+	function get_coll_setting_definitions( & $params )
+	{
+		$default_params = array_merge( $params,
+			array(
+				'default_comment_rendering' => 'stealth',
+				'default_post_rendering' => 'opt-out'
+			)
+		);
+
+		$r = array_merge( parent::get_coll_setting_definitions( $default_params ), $this->Form_Parts() );
+			
+		return $r;
+	}
+		
+	/**
+	 * Define here default message settings that are to be made available in the backoffice.
+	 *
+	 * @param array Associative array of parameters.
+	 * @return array See {@link Plugin::GetDefaultSettings()}.
+	 */
+	function get_msg_setting_definitions( & $params )
+	{
+		$default_params = array_merge( $params, array( 'default_msg_rendering' => 'stealth' ) );
+
+		$r = array_merge( parent::get_msg_setting_definitions( $default_params ), $this->Form_Parts() );
+			
+		return $r;
+	}
+	
+	/**
+	 * Define here default email settings that are to be made available in the backoffice.
+	 *
+	 * @param array Associative array of parameters.
+	 * @return array See {@link Plugin::GetDefaultSettings()}.
+	 */
+	function get_email_setting_definitions( & $params )
+	{
+		$default_params = array_merge( $params, array( 'default_email_rendering' => 'stealth' ) );
+
+		$r = array_merge( parent::get_email_setting_definitions( $default_params ), $this->Form_Parts());
+
+		return $r;
+	}
+	
+	/**
+	 * Param definitions when added as a widget.
+	 *
+	 * Plugins used as widget need to implement the SkinTag hook.
+	 *
+	 * @return array
+	 */
+	function get_widget_param_definitions( $params )
+	{
+		$r = array_merge( $this->Form_Parts(), parent::get_widget_param_definitions( $params ) );
+			
+		return $r;	
+		
+	}
+	
+	/**
+	 * Param definitions when added as a widget.
+	 *
+	 * Plugins used as widget need to implement the SkinTag hook.
+	 *
+	 * @return array
+	 */
+	function Form_Parts()
+	{
+		global $app_version;
+		
+		return array(
+
+
+	'info_1' => array(
+						'label' => T_('Following'),
+						'info' => 'Type: \'select_input\'<br>\'max_number 	=> 4 \'<br>\'<font color=red><b>use_fieldset 	=> true</b></font> \'<br>\'fold 	=> true \'<br>\'entries 	=>  array("Vegtables", "Fruits") <br> This will display a <font color=red><b>list</b></font>\'',
+						'type' => 'info',
+					),
+
+					'input_item_1' => array(
+					'label' 		=> T_('Select Input'),
+					'note' 			=> T_('Your note'),
+					'multiple' 		=> true, 
+					'max_number' 	=> 4,
+					'use_fieldset' 	=> true, 
+					'fold' 			=> true, 
+					'type' 			=> 'select_input',
+					'entries' 		=> array(
+
+									'_vagtables' => array(
+										'label' 		=> T_('Vegtables'),
+										'type' 			=> 'input_group',
+										'inputs' 		=> array(
+
+
+														'_value' => array(
+																			'label' => T_('Value'),
+																			'defaultvalue' => '',
+																			'type' => 'text',
+																			//'allow_empty' => false,
+																		),
+														'_label' => array(
+																			'label' => T_('Label'),
+																			'defaultvalue' => '',
+																			'type' => 'text',
+																			//'allow_empty' => false,
+																		),
+
+								)),
+									'_fruits' => array(
+										'label' 		=> T_('Fruits'),
+										'type' 			=> 'input_group',
+										'inputs' 		=> array(
+
+
+														'_value' => array(
+																			'label' => T_('Value'),
+																			'defaultvalue' => '',
+																			'type' => 'text',
+																			//'allow_empty' => false,
+																		),
+														'_label' => array(
+																			'label' => T_('Label'),
+																			'defaultvalue' => '',
+																			'type' => 'text',
+																			//'allow_empty' => false,
+																		),
+
+								)),
+
+
+
+
+			)),
+
+
+	'info_2' => array(
+						'label' => T_('Following'),
+						'info' => 'Type: \'select_input\'<br>\'max_number 	=> 4 \'<br>\'<font color=red><b>use_fieldset 	=> false</b></font> \'<br>\'fold 	=> no affect \'<br>\'entries 	=>  array("Vegtables", "Fruits") <br> This will display a <font color=red><b>list</b></font>\'',
+						'type' => 'info',
+					),
+
+					'input_item_2' => array(
+					'label' 		=> T_('Select Input'),
+					'note' 			=> T_('Your note'),
+					'multiple' 		=> true, 
+					'max_number' 	=> 4,
+					'use_fieldset' 	=> false, 
+					'fold' 			=> true, 
+					'type' 			=> 'select_input',
+					'entries' 		=> array(
+
+									'_color' => array(
+										'label' 		=> T_('Color'),
+										'type' 			=> 'input_group',
+										'inputs' 		=> array(
+
+
+														'_value' => array(
+																			'label' => T_('Color'),
+																			'defaultvalue' => '#444444',
+																			'type' => 'color',
+																			//'allow_empty' => false,
+																		),
+														'_label' => array(
+																			'label' => T_('Color'),
+																			'defaultvalue' => '#444444',
+																			'type' => 'color',
+																			//'allow_empty' => false,
+																		),
+
+								)),
+									'_vagtables' => array(
+										'label' 		=> T_('Vegtables'),
+										'type' 			=> 'input_group',
+										'inputs' 		=> array(
+
+
+														'_value' => array(
+																			'label' => T_('Value'),
+																			'defaultvalue' => '',
+																			'type' => 'text',
+																			//'allow_empty' => false,
+																		),
+														'_label' => array(
+																			'label' => T_('Label'),
+																			'defaultvalue' => '',
+																			'type' => 'text',
+																			//'allow_empty' => false,
+																		),
+
+								)),
+									'_fruits' => array(
+										'label' 		=> T_('Fruits'),
+										'type' 			=> 'input_group',
+										'inputs' 		=> array(
+
+
+														'_value' => array(
+																			'label' => T_('Value'),
+																			'defaultvalue' => '',
+																			'type' => 'text',
+																			//'allow_empty' => false,
+																		),
+														'_label' => array(
+																			'label' => T_('Label'),
+																			'defaultvalue' => '',
+																			'type' => 'text',
+																			//'allow_empty' => false,
+																		),
+
+								)),
+
+
+
+
+			)),
+
+
+	'info_3' => array(
+						'label' => T_('Following'),
+						'info' => 'Type: \'select_input\'<br>\'max_number 	=> 4 \'<br>\'use_fieldset 	=> false \'<br>\'fold 	=> no affect \'<br>\'entries 	=>  array("Vegtables") <br> This will display a  <font color=red><b>button</b></font>\'',
+						'type' => 'info',
+					),
+
+					'input_item_3' => array(
+					'label' 		=> T_('Select Input'),
+					'note' 			=> T_('Your note'),
+					'multiple' 		=> true, 
+					'max_number' 	=> 4,
+					'use_fieldset' 	=> false, 
+					'fold' 			=> true, 
+					'type' 			=> 'select_input',
+					'entries' 		=> array(
+
+									'_vagtables' => array(
+										'label' 		=> T_('Vegtables'),
+										'type' 			=> 'input_group',
+										'inputs' 		=> array(
+
+
+														'_value' => array(
+																			'label' => T_('Value'),
+																			'defaultvalue' => '',
+																			'type' => 'text',
+																			//'allow_empty' => false,
+																		),
+														'_label' => array(
+																			'label' => T_('Label'),
+																			'defaultvalue' => '',
+																			'type' => 'text',
+																			//'allow_empty' => false,
+																		),
+
+								)),
+
+
+
+
+
+			)),
+
+				'info_4' => array(
+						'label' => T_('Following'),
+						'info' => 'Type: \'array:array:string\'<br>\'<font color=red><b>max_number 	=> 3</b></font> \'<br>\'fold 	=> true \'',
+						'type' => 'info',
+					),
+
+
+							'plugin_sets'	=> array(
+												'label'			=> T_('Dynamic Fields'),
+												'note'			=> T_('Click to add another field item'),
+												// limited by array:array:string therefore html_textarea won't work
+												'type'			=> version_compare( $app_version, '6.6.5', '>' ) ? 'array:array:string' : 'array',
+												'max_number'		=> 3,
+												'fold' => true,
+												'entries'		=> array(
+
+
+	'info_1' => array(
+						'label' => T_('Following'),
+						'info' => 'Type: \'select_input\'<br>\'max_number 	=> 4 \'<br>\'<font color=red><b>use_fieldset 	=> true</b></font> \'<br>\'fold 	=> true \'<br>\'entries 	=>  array("Vegtables", "Fruits") <br> This will display a <font color=red><b>list</b></font>\'',
+						'type' => 'info',
+					),
+
+					'input_item_1' => array(
+					'label' 		=> T_('Select Input'),
+					'note' 			=> T_('Your note'),
+					'multiple' 		=> true, 
+					'max_number' 	=> 4,
+					'use_fieldset' 	=> true, 
+					'fold' 			=> true, 
+					'type' 			=> 'select_input',
+					'entries' 		=> array(
+
+									'_color' => array(
+										'label' 		=> T_('Color'),
+										'type' 			=> 'input_group',
+										'inputs' 		=> array(
+
+
+														'_value' => array(
+																			'label' => T_('Color'),
+																			'defaultvalue' => '#444444',
+																			'type' => 'color',
+																			//'allow_empty' => false,
+																		),
+														'_label' => array(
+																			'label' => T_('Color'),
+																			'defaultvalue' => '#444444',
+																			'type' => 'color',
+																			//'allow_empty' => false,
+																		),
+
+								)),
+									'_vagtables' => array(
+										'label' 		=> T_('Vegtables'),
+										'type' 			=> 'input_group',
+										'inputs' 		=> array(
+
+
+														'_value' => array(
+																			'label' => T_('Value'),
+																			'defaultvalue' => '',
+																			'type' => 'text',
+																			//'allow_empty' => false,
+																		),
+														'_label' => array(
+																			'label' => T_('Label'),
+																			'defaultvalue' => '',
+																			'type' => 'text',
+																			//'allow_empty' => false,
+																		),
+
+								)),
+									'_fruits' => array(
+										'label' 		=> T_('Fruits'),
+										'type' 			=> 'input_group',
+										'inputs' 		=> array(
+
+
+														'_value' => array(
+																			'label' => T_('Value'),
+																			'defaultvalue' => '',
+																			'type' => 'text',
+																			//'allow_empty' => false,
+																		),
+														'_label' => array(
+																			'label' => T_('Label'),
+																			'defaultvalue' => '',
+																			'type' => 'text',
+																			//'allow_empty' => false,
+																		),
+
+								)),
+
+
+
+
+			)),
+
+
+
+	'info_2' => array(
+						'label' => T_('Following'),
+						'info' => 'Type: \'select_input\'<br>\'max_number 	=> 4 \'<br>\'use_fieldset 	=> false \'<br>\'fold 	=> no affect \'<br>\'entries 	=>  array("Vegtables") <br> This will display a  <font color=red><b>button</b></font>\'',
+						'type' => 'info',
+					),
+
+					'input_item_3' => array(
+					'label' 		=> T_('Select Input'),
+					'note' 			=> T_('Your note'),
+					'multiple' 		=> true, 
+					'max_number' 	=> 4,
+					'use_fieldset' 	=> false, 
+					'fold' 			=> true, 
+					'type' 			=> 'select_input',
+					'entries' 		=> array(
+
+									'_vagtables' => array(
+										'label' 		=> T_('Vegtables'),
+										'type' 			=> 'input_group',
+										'inputs' 		=> array(
+
+
+														'_value' => array(
+																			'label' => T_('Value'),
+																			'defaultvalue' => '',
+																			'type' => 'text',
+																			//'allow_empty' => false,
+																		),
+														'_label' => array(
+																			'label' => T_('Label'),
+																			'defaultvalue' => '',
+																			'type' => 'text',
+																			//'allow_empty' => false,
+																		),
+								)),
+			)),
+	))
+
+				);
+
+	}
+	
+	/**
+	 * Called when a plugin gets called by its {@link $code}.
+	 *
+	 * If you provide this event, b2evolution will assume your plugin
+	 * provides a widget and list it in the "Available widgets" list.
+	 *
+	 * @see Plugins::SkinTag()
+	 * @param array The array passed to {@link Plugins::call_by_code()}.
+	 */
+	function SkinTag( & $params )
+	{
+	}
+	
+}
+?>
--- readme.md
+++ readme.md
@@ -2,7 +2,27 @@
 
 # b2evolution CMS
 
-This is b2evolution CMS version **6.10.1-stable**.
+This is b2evolution CMS Develop version **6.10.x**.
+This version should not be installed on a production server.
+
+## Changes
+
+This branch contains support for a feature not available in the Official Branch:
+
+Fixes for dynamic types: array:array:string
+Support for new dynamic type: select_input
+Support for nested dynamic types
+
+# Configurations:
+
+type			= 'select_input'
+max_number		= 4
+use_fieldset 	= true | false 
+fold			= true | false
+
+# Supports:
+
+'select' | 'integer' | 'html_input' | 'html_textarea' | 'textarea' | 'text' | 'checkbox' | 'checklist' | 'radio' | 'fileselect' | 'password' | 'info' | 'color' | 'input_group'
 
 ## A complete engine for your website !
 
@@ -11,48 +31,3 @@ b2evolution includes everything you need to run and maintain a modern website.
 Plus, it's optimized for low maintenance with easy upgrades and effective antispam. Full Bootstrap & RWD support.
 
 More info: http://b2evolution.net
-
-## Requirements
-
-Basically, all you need is a standard [web hosting plan](http://b2evolution.net/web-hosting/top-quality-best-webhosting.php).
-
-More specifically, your web server should support PHP 5.4+, MySQL 5.1+ & Apache 2+ (which is very common). More info about these requirements [here](http://b2evolution.net/man/installation-upgrade/system_requirements).
-
-## Downloading
-
-### With Bower
-
-If you're familiar with bower, just type: `bower install b2evolution`
-
-### Manual Download
-
-You can download releases either from GitHub or from b2evolution.net :
-
-- https://github.com/b2evolution/b2evolution/releases
-- http://b2evolution.net/downloads/
-
-## Installation: Amazing 3-minute install ;)
-
-Upload everything to your web server and call the installation script that you will find at `/install/index.php` on your website. Then you just need to enter your database connection details and the installer will take care of everything for you.
-
-Now, you might ask for more details here... Totally legitimate! Please check out our [Getting Stated - Installation Guide](http://b2evolution.net/man/getting-started).
-
-Hint: It is possible to install b2evolution in less than 3 minutes. Probably not the first time though. (And the same is true for anyone else claiming a 5 minute install process.)
-
-## Upgrading
-
-### Automatic upgrade
-
-b2evolution includes an automatic upgrade feature which you can use to automatically download the lastest stable version and perform the upgrade operations.
-
-### Manual upgrade
-
-You can download any newer version (including beta releases), overwrite the files of your current installation (after backup) and then run the install script.
-
-The installation script will detect that the b2evolution database is already installed (any version) and offer to upgrade it to the current version.
-
-There are [several other upgrade options](http://b2evolution.net/man/upgrading).
-
-## GitHub
-
-All bug fixes and all new code are made available through GitHub before being packaged as releases. If you are interested in cutting-edge versions, we recommend you [follow us on GitHub](https://github.com/b2evolution/b2evolution).
