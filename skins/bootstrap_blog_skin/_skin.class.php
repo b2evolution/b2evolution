eb67f0203bb2c71452822da1216f0e701f6574fc
Erwin Rommel H. Satingin <winskie@gmail.com>
2018-05-30 16:49:09 +0800

    Initial commit for affix messages feature

--- skins/bootstrap_blog_skin/_skin.class.php
+++ skins/bootstrap_blog_skin/_skin.class.php
@@ -175,6 +175,14 @@ class bootstrap_blog_Skin extends Skin
 							)
 						)
 					),
+
+					'message_affix_offset' => array(
+						'label' => T_('Messages affix offset'),
+						'note' => 'px. ' . T_('Set message top offset value.'),
+						'defaultvalue' => '',
+						'type' => 'integer',
+						'allow_empty' => true,
+					),
 				'section_layout_end' => array(
 					'layout' => 'end_fieldset',
 				),
@@ -543,6 +551,7 @@ class bootstrap_blog_Skin extends Skin
 -->
 		</style>';
 			add_headline( $custom_css );
+			init_affix_messages_js( $this->get_setting( 'message_affix_offset' ) );
 		}
 	}
 
