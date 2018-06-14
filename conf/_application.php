0a5485ba2c4b6f3ad7efa39b783663dd4ebf4b9d
yurabakhtin <yurybakh@gmail.com>
2018-06-14 10:23:08 +0300

    Fix author type label alignment on bootstrap_forums_skin

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.10.2-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2018-06-13';
+$app_date = '2018-06-14';
 
 /**
  * Long version string for checking differences
