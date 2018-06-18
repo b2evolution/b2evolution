0cb334e9bc4d7183315829271d07bd82e5068f83
yurabakhtin <yurybakh@gmail.com>
2018-06-18 10:42:03 +0300

    Changes for email notification about error and timeout scheduled tasks

--- skins_email/scheduled_task_error_report.txt.php
+++ skins_email/scheduled_task_error_report.txt.php
@@ -14,19 +14,28 @@ if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.'
 emailskin_include( '_email_header.inc.txt.php', $params );
 // ------------------------------- END OF EMAIL HEADER --------------------------------
 
-global $admin_url, $baseurl;
+global $admin_url;
 
 // Default params:
 $params = array_merge( array(
-		'tasks' => array(),
+		'timeout_tasks' => array(),
+		'error_task'    => NULL,
 	), $params );
 
-echo T_('The following scheduled tasks have ended with error:')."\n";
-if( is_array( $params['tasks'] ) && count( $params['tasks'] ) )
-{
-	foreach( $params['tasks'] as $task_ID => $task )
+if( $params['error_task'] !== NULL )
+{	// Display an error task:
+	echo T_('The following scheduled task has ended with an error:')."\n";
+	echo $params['error_task']['name'].' (#'.$params['error_task']['ID'].' - '.$admin_url.'?ctrl=crontab&action=view&cjob_ID='.$params['error_task']['ID'].'):'."\n";
+	echo ltrim( $params['error_task']['message'], "\n" );
+	echo "\n\n";
+}
+
+if( is_array( $params['timeout_tasks'] ) && count( $params['timeout_tasks'] ) )
+{	// Display timeout tasks:
+	echo T_('The following scheduled tasks have timed out:')."\n";
+	foreach( $params['timeout_tasks'] as $task_ID => $task )
 	{
-		echo '- '.$task['name'].' (#'.$task_ID.'):'."\n".T_( $task['message'] )."\n";
+		echo '- '.$task['name'].' (#'.$task_ID.' - '.$admin_url.'?ctrl=crontab&action=view&cjob_ID='.$task_ID.'):'."\n".T_( $task['message'] )."\n";
 	}
 }
 echo "\n";
