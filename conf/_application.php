c3e66a17eeea8b9e4ab049239ba44636fb444d8f
Francois Planque <github@fplanque.net>
2018-07-02 21:11:20 +0200

    date bump

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '7.0.0-alpha';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2018-06-29';
+$app_date = '2018-07-02';
 
 /**
  * Long version string for checking differences
