20adff7cdf8cde884ab5f99226698d21fd452a24
yurabakhtin <yurybakh@gmail.com>
2018-07-25 12:47:47 +0300

    date bump

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.10.3-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2018-07-23';
+$app_date = '2018-07-25';
 
 /**
  * Long version string for checking differences
