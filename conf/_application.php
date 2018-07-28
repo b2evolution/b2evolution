259243e4e16e2227f86b0c2ffcc15acf3811120c
yurabakhtin <yurybakh@gmail.com>
2018-07-28 10:37:07 +0300

    Duplicate post type custom fields

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.10.3-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2018-07-27';
+$app_date = '2018-07-28';
 
 /**
  * Long version string for checking differences
