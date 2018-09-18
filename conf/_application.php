458a94a598a5496417eaa559941e2cea852fa4d6
yurabakhtin <yurybakh@gmail.com>
2018-09-18 19:50:50 +0300

    Fix widget customizing for blocks out window borders

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '7.0.0-alpha';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2018-09-17';
+$app_date = '2018-09-18';
 
 /**
  * Long version string for checking differences
