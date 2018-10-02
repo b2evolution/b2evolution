37c40bb6924cae214f82f6a32cae877719506c2b
yurabakhtin <yurybakh@gmail.com>
2018-10-02 15:04:47 +0300

    Changes for page param of rest api items list request

--- inc/rest/_restapi.class.php
+++ inc/rest/_restapi.class.php
@@ -604,7 +604,10 @@ class RestApi
 
 		// Get param to limit number posts per page:
 		$api_per_page = param( 'per_page', 'integer', 10 );
-		$page = param( 'page', 'integer', 1 );
+
+		// Get param to select current page:
+		// (NOTE: if this param is not set then param 'paged' is used as filter of ItemList)
+		$page = param( 'page', 'integer', NULL );
 
 		// Get param to know what post fields should be sent in response:
 		$api_details = param( 'details', 'string', NULL );
@@ -632,7 +635,12 @@ class RestApi
 
 		if( $ItemList2->filters['types'] == $ItemList2->default_filters['types'] )
 		{	// Allow all post types by default for this request:
-			$ItemList2->set_filters( array( 'itemtype_usage' => NULL, 'page' => $page ), true, true );
+			$ItemList2->set_filters( array( 'itemtype_usage' => NULL ), true, true );
+		}
+
+		if( $page !== NULL )
+		{	// Set page from request:
+			$ItemList2->set_filters( array( 'page' => $page ), true, true );
 		}
 
 		if( ! empty( $force_filters ) )
