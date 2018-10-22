679074383a15ec0c08e8f764742d79a6d391f824
yurabakhtin <yurybakh@gmail.com>
2018-10-22 11:29:18 +0300

    Fix redirect cross-posted items

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.10.4-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2018-10-18';
+$app_date = '2018-10-22';
 
 /**
  * Long version string for checking differences
