abdc99d3b2c3f9e074e76d5cfd53fdda561deb4a
yurabakhtin <yurybakh@gmail.com>
2018-11-10 11:48:27 +0300

    Fix getting of top domain by subdomain name without unknown status

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.10.4-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2018-11-09';
+$app_date = '2018-11-10';
 
 /**
  * Long version string for checking differences
