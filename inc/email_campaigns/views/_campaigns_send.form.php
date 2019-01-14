61c0fe294588e547b6630799e044c90f4a3ba9bf
yurabakhtin <yurybakh@gmail.com>
2019-01-14 19:07:32 +0300

    Display send error counter on email campaign form

--- inc/email_campaigns/views/_campaigns_send.form.php
+++ inc/email_campaigns/views/_campaigns_send.form.php
@@ -91,6 +91,7 @@ $Form->begin_fieldset( T_('Campaign recipients').get_manual_link( 'campaign-reci
 		) );
 	$Form->info( T_('Already received'), $edited_EmailCampaign->get_recipients_count( 'receive', true ), '('.T_('Accounts which have already been sent this campaign').')' );
 	$Form->info( T_('Manually skipped'), $edited_EmailCampaign->get_recipients_count( 'skipped', true ), '('.T_('Accounts which will be skipped from receiving this campaign').')' );
+	$Form->info( T_('Send error'), $edited_EmailCampaign->get_recipients_count( 'error', true ), '('.T_('Accounts which had errors on receiving this campaign').')' );
 	$Form->info( T_('Ready to send'), $edited_EmailCampaign->get_recipients_count( 'wait', true ), '('.T_('Accounts which meet all criteria to receive this campaign').')' );
 
 	if( $edited_EmailCampaign->get_recipients_count( 'wait' ) > 0 )
