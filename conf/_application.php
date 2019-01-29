491d943054f15783d462cf323a8b7536f7b743a9
yurabakhtin <yurybakh@gmail.com>
2019-01-29 09:02:09 +0300

    Fix comments on front page of bootstrap manual skin

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.10.6-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2019-01-24';
+$app_date = '2019-01-29';
 
 /**
  * Long version string for checking differences
