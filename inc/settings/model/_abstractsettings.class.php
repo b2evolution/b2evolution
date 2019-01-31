50fb06e9feed517b3cf14c0a39bc3658d0e0076e
yurabakhtin <yurybakh@gmail.com>
2019-01-31 15:30:26 +0300

    Fix caching for plugin user settings

--- inc/settings/model/_abstractsettings.class.php
+++ inc/settings/model/_abstractsettings.class.php
@@ -162,17 +162,12 @@ class AbstractSettings
 
 		if( $this->cache_by_col_keys && isset($arg1) )
 		{ // The number of column keys to cache by is > 0
-			$testCache = $this->cache;
-			$args = array( $arg1, $arg2, $arg3 );
-
 			for( $i = 0; $i < $this->cache_by_col_keys; $i++ )
 			{
-				$whereList[] = $this->col_key_names[$i]." = '".$args[$i]."'";
+				$whereList[] = $this->col_key_names[$i].' = '.$DB->quote( ${'arg'.($i+1)} );
 
-				if( ! is_array( $testCache )
-						|| is_null($args[$i])
-						|| ! isset( $testCache[$args[$i]] )
-						|| ! ($testCache = & $testCache[$args[$i]]) )
+				if( ( $this->cache_by_col_keys == 1 && $i == 0 && ! isset( $this->cache[ $arg1 ] ) ) ||
+				    ( $this->cache_by_col_keys == 2 && $i == 1 && ! isset( $this->cache[ $arg1 ][ $arg2 ] ) ) )
 				{
 					break;
 				}
