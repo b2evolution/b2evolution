ec959b3ed498047e324a801293be154a790d7313
yurabakhtin <yurybakh@gmail.com>
2019-02-11 08:40:59 +0300

    Fix inline tags in demo contents

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.10.7-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2019-02-05';
+$app_date = '2019-02-11';
 
 /**
  * Long version string for checking differences
