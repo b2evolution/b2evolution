711665dd1813188b24b392c063286186c570159c
yurabakhtin <yurybakh@gmail.com>
2019-02-12 19:32:24 +0300

    Use other filtered post contents mode by default

--- skins/bootstrap_manual_skin/_item_content.inc.php
+++ skins/bootstrap_manual_skin/_item_content.inc.php
@@ -130,24 +130,18 @@ if( $content_mode == 'auto' )
 			$content_mode = $Blog->get_setting('archive_content');
 			break;
 
-		case 'posts-filtered':
-		case 'search':
-			$content_mode = $Blog->get_setting('filtered_content');
-			break;
-
 		case 'single':
 		case 'page':
 			$content_mode = 'full';
 			break;
 
-		case 'flagged':
-			$content_mode = 'excerpt';
-			break;
-
 		case 'posts-default':  // home page 1
 		case 'posts-next':     // next page 2, 3, etc
-		default:
 			$content_mode = $Blog->get_setting('main_content');
+			break;
+
+		default: // posts-filtered, search, flagged and etc.
+			$content_mode = $Blog->get_setting('filtered_content');
 	}
 }
 
