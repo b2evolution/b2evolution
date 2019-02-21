95bcdfad09189fcdf7ed11ab854f31793d0dc30d
yurabakhtin <yurybakh@gmail.com>
2019-02-21 16:51:13 +0300

    Snippet toolbar plugin

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.10.7-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2019-02-20';
+$app_date = '2019-02-21';
 
 /**
  * Long version string for checking differences
