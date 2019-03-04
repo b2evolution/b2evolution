8cc45f2b4598636091199280956df27832a3e0db
yurabakhtin <yurybakh@gmail.com>
2019-03-04 09:26:04 +0300

    Changes for additional js files of TinyMCE plugin

--- plugins/tinymce_plugin/_tinymce.plugin.php
+++ plugins/tinymce_plugin/_tinymce.plugin.php
@@ -700,8 +700,7 @@ class tinymce_plugin extends Plugin
 					// Anyway, not using AJAX to fetch the file makes it more cachable anyway.
 					require_js( '#tinymce#', 'blog', false, true );
 					require_js( '#tinymce_jquery#', 'blog', false, true );
-					require_js( '#shortcodes#', 'blog', false, true );
-					require_js( '#evo_view#', 'blog', false, true );
+					require_js( $this->get_plugin_url().'js/evo_view_shortcodes.bmin.js', 'blog', false, true );
 					?>
 
 					<script>
