9cd3a9fb3798f77a3d05cf04e64b9b599d5d6358
yurabakhtin <yurybakh@gmail.com>
2019-03-13 11:31:11 +0300

    Fix duplicating of email campaign

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.11.0-beta';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2019-03-12';
+$app_date = '2019-03-13';
 
 /**
  * Long version string for checking differences
