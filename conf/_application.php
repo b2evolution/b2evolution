ea0fb00b25233b60214a0398f6441b5c8204ca47
yurabakhtin <yurybakh@gmail.com>
2019-03-20 09:38:21 +0300

    Fix render inline files by plugins

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.10.7-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2019-03-14';
+$app_date = '2019-03-20';
 
 /**
  * Long version string for checking differences
