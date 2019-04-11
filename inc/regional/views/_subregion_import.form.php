120fce6ca77acebc09d935c23fdd8c64aade6ca0
yurabakhtin <yurybakh@gmail.com>
2019-04-11 19:37:51 +0300

    Import regions and sub-regions from CSV files

--- inc/regional/views/_subregion_import.form.php
+++ inc/regional/views/_subregion_import.form.php
@@ -5,7 +5,7 @@
  *
  * @license GNU GPL v2 - {@link http://b2evolution.net/about/gnu-gpl-license}
  *
- * @copyright (c)2009-2016 by Francois Planque - {@link http://fplanque.com/}
+ * @copyright (c)2009-2019 by Francois Planque - {@link http://fplanque.com/}
  * Parts of this file are copyright (c)2009 by The Evo Factory - {@link http://www.evofactory.com/}.
  *
  * @package evocore
@@ -15,20 +15,20 @@ if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.'
 
 load_class( 'regional/model/_country.class.php', 'Country' );
 
-$Form = new Form( NULL, 'city_checkchanges', 'post', 'compact', 'multipart/form-data' );
+$Form = new Form( NULL, 'subregion_checkchanges', 'post', 'compact', 'multipart/form-data' );
 
 $Form->global_icon( T_('Cancel importing!'), 'close', regenerate_url( 'action' ) );
 
-$Form->begin_form( 'fform', T_('Import cities').get_manual_link( 'cities-import' ) );
+$Form->begin_form( 'fform', T_('Import sub-regions').get_manual_link( 'subregions-import' ) );
 
 	echo T_('Select a country and upload a CSV file with the following columns:');
 	echo '<div style="padding:10px 0 10px 40px">';
-	echo '1. '.T_('Post code').'<br />';
-	echo '2. '.T_('City name').'<br />';
-	echo '3. '.T_('Optional: sub-region code');
+	echo '1. '.T_('Region code').'<br />';
+	echo '2. '.T_('Sub-region code').'<br />';
+	echo '3. '.T_('Sub-region name').'<br />';
 	echo '</div>';
 
-	$Form->add_crumb( 'city' );
+	$Form->add_crumb( 'subregion' );
 	$Form->hiddens_by_key( get_memorized( 'action' ) ); // (this allows to come back to the right list order & page)
 
 	$CountryCache = & get_CountryCache();
