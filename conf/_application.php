71d50b1b21cda1baa66d57fbafbc506067e61ecd
yurabakhtin <yurybakh@gmail.com>
2019-04-12 09:53:09 +0300

    Fix full tables list on backup form

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.11.0-beta';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2019-04-11';
+$app_date = '2019-04-12';
 
 /**
  * Long version string for checking differences
--- inc/maintenance/model/_maintenance.funcs.php
+++ inc/maintenance/model/_maintenance.funcs.php
@@ -875,9 +875,27 @@ function get_affected_tables( $table )
 	}
 	elseif( $table == '*' )
 	{
+		// Get tables what should be excluded from full tables list:
+		global $backup_tables;
+		$exclude_tables = array();
+		foreach( $backup_tables as $backup_data )
+		{
+			if( isset( $backup_data['included'] ) &&
+			    ! $backup_data['included'] &&
+			    is_array( $backup_data['table'] ) )
+			{
+				$exclude_tables = array_merge( $exclude_tables, aliases_to_tables( $backup_data['table'] ) );
+			}
+		}
+
 		$tables = array();
 		foreach( $DB->get_results( 'SHOW TABLES', ARRAY_N ) as $row )
+		{
+			if( ! in_array( $row[0], $exclude_tables ) )
+			{
 				$tables[] = $row[0];
+			}
+		}
 
 		$affected_tables .= implode( ', ', $tables );
 	}
