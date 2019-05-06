87d58b7706438ad0c9cc7c3af1b5548bd2a8beb2
yurabakhtin <yurybakh@gmail.com>
2019-05-06 14:44:59 +0300

    Fix intro content mode
    Conflicts:
    	skins/green_bootstrap_theme/forum/_item_content.inc.php

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.11.2-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2019-05-05';
+$app_date = '2019-05-06';
 
 /**
  * Long version string for checking differences
