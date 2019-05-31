aabd012a89876df3e75f6957fdbef836cfad707e
yurabakhtin <yurybakh@gmail.com>
2019-05-31 10:23:49 +0300

    Fix style of action icon to restore skin setting to default value

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '7.0.1-beta';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2019-05-30';
+$app_date = '2019-05-31';
 
 /**
  * Long version string for checking differences
