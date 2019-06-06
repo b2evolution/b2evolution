364596b07d9379992ea50e1679966cf546e5a7e3
yurabakhtin <yurybakh@gmail.com>
2019-06-06 19:38:54 +0300

    Fix for parsedown plugin

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.11.2-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2019-06-05';
+$app_date = '2019-06-06';
 
 /**
  * Long version string for checking differences
