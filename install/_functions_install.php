1a564b0f4728ef83140e8a99ce61afa10cb8e33f
yurabakhtin <yurybakh@gmail.com>
2019-06-12 16:17:03 +0300

    New plugin "Auto Anchors"

--- install/_functions_install.php
+++ install/_functions_install.php
@@ -791,6 +791,11 @@ function install_basic_plugins( $old_db_version = 0 )
 	{
 		install_plugin( 'webmention_plugin' );
 	}
+
+	if( $old_db_version < 13210 )
+	{
+		install_plugin( 'auto_anchors_plugin' );
+	}
 }
 
 
