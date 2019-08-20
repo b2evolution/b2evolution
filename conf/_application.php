0e67999c5cec502a48704ea196366b601f405350
Francois Planque <github@fplanque.net>
2019-08-20 23:29:49 +0200

    fix app date

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.11.3-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2019-08-19';
+$app_date = '2019-08-20';
 
 /**
  * Long version string for checking differences
