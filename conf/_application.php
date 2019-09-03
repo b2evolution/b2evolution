847519943222bd699f8a583b65161226a5a45d3f
yurabakhtin <yurybakh@gmail.com>
2019-09-03 11:26:03 +0300

    Make nice scroll to anchor headers for plugin "Table of Contents"

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.11.3-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2019-09-02';
+$app_date = '2019-09-03';
 
 /**
  * Long version string for checking differences
