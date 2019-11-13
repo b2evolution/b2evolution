55f0f59f7f07ea518e1d9bea5985fada4854affb
yurabakhtin <yurybakh@gmail.com>
2019-11-13 15:51:35 +0300

    Remove plugin "Who's online Widget"

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.11.4-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2019-11-12';
+$app_date = '2019-11-13';
 
 /**
  * Long version string for checking differences
