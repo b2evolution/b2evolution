378db8ddbfa2e39c28c084474a1ba968a6819c66
yurabakhtin <yurybakh@gmail.com>
2019-12-11 08:54:54 +0300

    Fix plugin "Video Plug"

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.11.4-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2019-12-07';
+$app_date = '2019-12-11';
 
 /**
  * Long version string for checking differences
