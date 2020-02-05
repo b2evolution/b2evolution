a5e6a58ad3d599183ede91ebcf944220680b78c1
yurabakhtin <yurybakh@gmail.com>
2020-02-05 11:39:54 +0300

    Fix string

--- inc/widgets/widgets/_item_info_line.widget.php
+++ inc/widgets/widgets/_item_info_line.widget.php
@@ -294,7 +294,7 @@ class item_info_line_Widget extends ComponentWidget
 			switch( $this->disp_params['display_date'] )
 			{
 				case 'issue_date':
-					$before_issue_time = empty( $before_author ) ? '' : $T_('on').' ';
+					$before_issue_time = empty( $before_author ) ? '' : T_('on').' ';
 					$template .= ' $issue_time$';
 					break;
 
