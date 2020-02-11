34beaa98c5eff6db472554f138309fb8c434b90d
Francois Planque <github@fplanque.net>
2020-02-11 20:38:49 +0100

    app date

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.11.5-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2020-02-04';
+$app_date = '2020-02-11';
 
 /**
  * Long version string for checking differences
