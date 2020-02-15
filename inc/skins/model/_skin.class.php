5f55e3ede08540594a50090a251a84ca848d8e2b
Francois Planque <github@fplanque.net>
2020-02-15 22:22:04 +0100

    Ui cleanup

--- inc/skins/model/_skin.class.php
+++ inc/skins/model/_skin.class.php
@@ -1825,11 +1825,11 @@ var downloadInterval = setInterval( function()
 							'page_url' => '', // All generated links will refer to the current page
 							'before' => '<div class="results panel panel-default">',
 							'content_start' => '<div id="$prefix$ajax_content">',
-							'header_start' => '<div class="evo_panel evo_panel__top_pager">',
-								'header_text' => '<div class="center"><ul class="pagination">'
+							'header_start' => '<div class="results_header clearfix">',
+								'header_text' => '<div class="evo_pager"><div class="results_summary">$nb_results$ Results $reset_filters_button$</div><ul class="pagination">'
 										.'$prev$$first$$list_prev$$list$$list_next$$last$$next$'
 									.'</ul></div>',
-								'header_text_single' => '',
+								'header_text_single' => '<div class="results_summary">$nb_results$ Results $reset_filters_button$</div>',
 							'header_end' => '</div>',
 							'head_title' => '<div class="panel-heading fieldset_title"><span class="pull-right panel_heading_action_icons">$global_icons$</span><h3 class="panel-title">$title$</h3></div>'."\n",
 							'global_icons_class' => 'btn btn-default btn-sm',
@@ -1886,7 +1886,7 @@ var downloadInterval = setInterval( function()
 									'total_col_end' => "</td>\n",
 								'total_line_end' => "</tr>\n\n",
 							'list_end' => "</table></div>\n\n",
-							'footer_start' => '',
+							'footer_start' => '<div class="results_footer">',
 							'footer_text' => '<div class="center"><ul class="pagination">'
 									.'$prev$$first$$list_prev$$list$$list_next$$last$$next$'
 								.'</ul></div><div class="center">$page_size$</div>'
@@ -1910,7 +1910,7 @@ var downloadInterval = setInterval( function()
 								'scroll_list_range' => 5,
 							'footer_end' => "\n\n",
 							'no_results_start' => '<div class="panel-footer">'."\n",
-							'no_results_end'   => '$no_results$</div>'."\n\n",
+							'no_results_end'   => '$no_results$ $reset_filters_button$</div>'."\n\n",
 							'content_end' => '</div>',
 							'after' => '</div>',
 							'sort_type' => 'basic'
