ca61607c8848bd0436b83120d633b9ab30313e54
yurabakhtin <yurybakh@gmail.com>
2020-02-28 19:38:47 +0300

    Fix sidebars js position on scroll in bootstrap_manual_skin

--- skins/bootstrap_manual_skin/left_navigation.js
+++ skins/bootstrap_manual_skin/left_navigation.js
@@ -4,24 +4,39 @@
 
 jQuery( document ).ready( function()
 {
-	// Check all 3 sidebars are NOT in 3 columns (broken layout):
-	// (We need to work only with correct layout when 3 sidebars are either in single column or in 2 columns)
 	var sidebar_selectors = '#evo_container__sidebar, #evo_container__sidebar_2, #evo_container__sidebar_single';
-	var sidebar_left_positions = [];
-	jQuery( sidebar_selectors ).each( function()
-	{
-		var left = jQuery( this ).offset().left;
-		if( sidebar_left_positions.indexOf( left ) )
-		{	// Put in array only new different left position:
-			sidebar_left_positions.push( left );
+
+	function fix_sidebars_positions()
+	{	// Check all 3 sidebars are NOT in 3 columns (broken layout):
+		// (We need to work only with correct layout when 3 sidebars are either in single column or in 2 columns)
+		if( jQuery( sidebar_selectors ).length < 3 )
+		{	// Skip case with 2 and less sidebars because no issue:
+			return;
 		}
-	} );
-	if( sidebar_left_positions.length > 2 )
-	{	// WRONG CASE when 3 sidebars are contained in 3 different columns:
-		// Correct layout is when 3 sidebars are either in single column or in 2 columns:
-		console.log( 'Cannot affix sidebars, because more than 2 sidebars are found. Please correct layout first.' );
-		return;
+
+		var sidebar_left_positions = [];
+		jQuery( sidebar_selectors ).each( function()
+		{
+			var left = jQuery( this ).offset().left;
+			if( sidebar_left_positions.indexOf( left ) )
+			{	// Put in array only new different left position:
+				sidebar_left_positions.push( left );
+			}
+		} );
+		if( sidebar_left_positions.length > 2 )
+		{	// WRONG CASE when 3 sidebars are contained in 3 different columns:
+			// Correct layout is when 3 sidebars are either in single column or in 2 columns:
+			// Make height of the left sidebar same as right sidebar in order to fix the bootstrap CSS issue:
+			jQuery( '#evo_container__sidebar' ).parent().css( 'height', jQuery( '#evo_container__sidebar_single' ).parent().outerHeight( true ) + 100 );
+		}
+		else if( sidebar_left_positions.length == 1 )
+		{	// Use auto height when all 3 sidebars in single column:
+			jQuery( '#evo_container__sidebar' ).parent().css( 'height', 'auto' );
+		}
 	}
+	// Fix position of sidebar on load and resize window:
+	fix_sidebars_positions();
+	jQuery( window ).bind( 'resize', fix_sidebars_positions );
 
 	// Detect touch device in order to disable the fixed position for sidebars at all:
 	var has_touch_event;
@@ -49,9 +64,11 @@ jQuery( document ).ready( function()
 			//   A) 3 sidebars in single left column
 			//   B) 1 sidebar in left column, 2 sidebars in right column
 			var column_top_point = $sidebar.offset().top;
-			// Store in this var real top position where sidebar is started, it may be a position under other sidebar for case B:
+			var column_height = $sidebar.outerHeight( true );
+			// Top position(for position:fixed) where sidebar is started, it may be a position under other sidebar for case B:
 			var sidebar_top_start_point = top_start_point;
-			var sidebar_height = $sidebar.outerHeight();
+			// Top position of shifting sidebar inside column relating on top sidebars:
+			var sidebar_top_shift_in_column = 0
 			jQuery( sidebar_selectors ).each( function()
 			{	// Go through all sidebars in order to detect in each column current sidebar is located:
 				if( $sidebar.offset().left == jQuery( this ).offset().left )
@@ -62,10 +79,12 @@ jQuery( document ).ready( function()
 					}
 					if( jQuery( this ).attr( 'id' ) != $sidebar.attr( 'id' ) )
 					{	// This another sidebar but from the same column:
-						sidebar_height += jQuery( this ).outerHeight();
+						var sidebar_height = jQuery( this ).parent().outerHeight( true );
+						column_height += sidebar_height;
 						if( $sidebar.offset().top > jQuery( this ).offset().top )
 						{	// If current sidebar is located under another sidebar in the same column (case B)
-							sidebar_top_start_point += jQuery( this ).outerHeight();
+							sidebar_top_shift_in_column += sidebar_height;
+							sidebar_top_start_point += sidebar_height;
 						}
 					}
 				}
@@ -75,12 +94,12 @@ jQuery( document ).ready( function()
 			$sidebar.css( 'width', $sidebar.parent().width() );
 
 			if( ! $sidebar.hasClass( 'fixed' ) &&
-					sidebar_height < jQuery( window ).height() &&
+					column_height < jQuery( window ).height() &&
 					jQuery( window ).scrollTop() > $sidebar.offset().top - sidebar_top_start_point )
 			{	// Fill the space with fake block with same size where we had real sidebar
 				// AND Make sidebar as fixed if we scroll down:
 				$sidebar.before( $sidebarSpacer );
-				$sidebar.addClass( 'fixed' ).css( 'top', sidebar_top_start_point + 'px' );
+				$sidebar.addClass( 'fixed' );
 			}
 			else if( $sidebar.hasClass( 'fixed' ) &&
 					jQuery( window ).scrollTop() < $sidebarSpacer.offset().top - sidebar_top_start_point )
@@ -93,10 +112,10 @@ jQuery( document ).ready( function()
 			if( $sidebar.hasClass( 'fixed' ) )
 			{	// Check and fix an overlapping of footer with sidebar:
 				$sidebar.css( 'top', sidebar_top_start_point + 'px' );
-				var diff = parseInt( column_top_point + sidebar_height - jQuery( '#evo_site_footer' ).offset().top );
+				var diff = $sidebar.offset().top + column_height - sidebar_top_shift_in_column - jQuery( '#evo_site_footer' ).offset().top;
 				if( diff >= 0 )
 				{	// Don't allow to put sidebar over footer:
-					$sidebar.css( 'top', parseInt( sidebar_top_start_point - diff - 5 ) + 'px' );
+					$sidebar.css( 'top', ( sidebar_top_start_point - diff - 5 ) + 'px' );
 				}
 			}
 		}
@@ -117,13 +136,13 @@ jQuery( document ).ready( function()
 	jQuery( sidebar_selectors ).each( function()
 	{
 		var $sidebar = jQuery( this );
-		if( $sidebar.outerHeight( true ) + footer_height < jQuery( window ).height() )
+		if( $sidebar.outerHeight( true ) + footer_height < jQuery( document ).height() )
 		{	// It make sense to make sidebar with fixed position only when height of sidebar + footer is less than window height
 			// Create fake empty block instead of real sidebar in order to fill the space what was used by real sidebar before we made sidebar to "position:fixed",
 			// if we don't create such block at the same position and size as sidebar then layout will be broken, because some elements will be shifted to the place where sidebar was before fixed position.
 			var $sidebarSpacer = jQuery( '<div />', {
 					"class" : "evo_container__sidebar fixed_spacer",
-					"height": $sidebar.outerHeight( true )
+					"height": $sidebar.parent().outerHeight( true )
 				} );
 			jQuery( window ).bind( 'scroll resize', function ()
 			{	// Set proper position on scroll and resize window:
