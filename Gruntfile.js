6daac22f3ac95645311569daad6d9818d1a2cea0
Erwin Rommel H. Satingin <winskie@gmail.com>
2020-04-23 13:28:58 +0800

    Implemented deferred loading of Markdown toolbar plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -291,6 +291,7 @@ module.exports = function(grunt) {
 					'rsc/js/src/evo_init_plugin_tinymce.js',
 					'rsc/js/src/evo_init_plugin_shortlinks.js',
 					'rsc/js/src/evo_init_plugin_inlines.js',
+					'rsc/js/src/evo_init_plugin_markdown.js',
 					'rsc/js/src/evo_init_plugin_polls.js',
 					'rsc/js/src/evo_init_editable_column.js',
 					'rsc/js/src/evo_init_regional.js',
