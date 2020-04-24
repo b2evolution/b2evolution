8c2652aee410eeef7ba9bb1df3847997d26d6740
yurabakhtin <yurybakh@gmail.com>
2020-04-24 17:50:40 +0300

    Fix quick uploader

--- conf/_application.php
+++ conf/_application.php
@@ -21,7 +21,7 @@ $app_version = '6.11.6-stable';
  * Release date (ISO)
  * @global string
  */
-$app_date = '2020-04-23';
+$app_date = '2020-04-24';
 
 /**
  * Long version string for checking differences
